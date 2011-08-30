85332dbd1b9e1f05fa801bb6751b82a0ff6b3610
Matt Newville <newville@cars.uchicago.edu>
2011-08-30 09:55:15 -0500

    typo

--- lib/inputText.py
+++ lib/inputText.py
@@ -254,7 +254,7 @@ class InputText:
                         self.endkeys = ('end',  'end%s'%self.current,
                                         '&end', '&end%s'%self.current)
 
-            elif not text.endswith(')') and self.__isCommand(thiskey, eword2):
+            elif not text.endswith(')') and self.__isCommand(thiskey, word2):
                 # handle 'command format', including 'print'
                 text = '%s(%s)' % (thiskey, text[len(thiskey):].strip())
 
