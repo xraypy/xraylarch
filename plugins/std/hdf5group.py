51f63f33e1a841c369c3ae90226908da7e7be538
Matt Newville <newville@cars.uchicago.edu>
2011-11-16 01:02:08 -0600

    improved hdf5 reader

--- plugins/std/hdf5group.py
+++ plugins/std/hdf5group.py
@@ -1,26 +1,40 @@
 import h5py
-
-MODNAME = '_io'
+import numpy
+from larch.closure import Closure
 
 def h5group(fname, larch=None):
+    """simple mapping of hdf5 file to larch groups"""
+
     fh = h5py.File(fname, 'r')
-    components = []
-    new_group = larch.symtable.create_group
-    fh.visit(components.append)
-    topgroup = new_group()
-    groupmap = {'root': topgroup}
-    
-    for cname in components:
-        val = fh.get(cname)
+    group = larch.symtable.create_group
+    def add_component(key, val, top):
+        parents = key.split('/')
+        current = parents.pop()
+        for word in parents:
+            if not hasattr(top, word):
+                setattr(top, word, group())
+            top = getattr(top, word)
+        tname = top.__name__
         if isinstance(val, h5py.Group):
-            print 'Group: ', cname
-            groupmap[cname] = new_group()
-            setattr(topgroup, cname, groupmap[cname])
+            setattr(top, current,  group(name=tname+'/'+current))
+            if len(val.attrs) > 0:
+                setattr(top, 'attrs', dict(val.attrs))
         else:
-            print 'MM ', cname # setattr(group, cname, fh.get(comp))
-    return topgroup
+            dat = fh.get(key)
+            try:
+                if len(dat) > 1 and dat.dtype.type == numpy.string_:
+                    dat = list(dat)
+            except:
+                pass
+            setattr(top, current, dat)
+            if len(val.attrs) > 0:
+                setattr(top, current+'_attrs', dict(val.attrs))
+    top = group(name=fname)
+    setattr(top, 'h5_file', fh)
+    fh.visititems(Closure(func=add_component, top=top))
+    return top
 
 def registerLarchPlugin():
-    return (MODNAME, {'h5group': h5group})
+    return ('_io', {'h5group': h5group})
 
 
