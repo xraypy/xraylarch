64537236f16774272cdb6af907434a5e50ff2fdb
Matt Newville <newville@cars.uchicago.edu>
2011-12-10 11:26:19 -0600

    xafs.pre_edge plugin supports 1st pass at Victoreen fits

--- plugins/xafs/pre_edge.py
+++ plugins/xafs/pre_edge.py
@@ -26,10 +26,12 @@ def find_e0(energy, mu, group=None, larch=None):
         setattr(group, 'e0', energy[idmu_max+1])
     return energy[idmu_max+1]
 
-def pre_edge(energy, mu, group=None, larch=None, e0=None,
-             step=None, nnorm=3, form='linear',
-             pre1=-200, pre2=-50, norm1=100, norm2=400):
+def pre_edge(energy, mu, group=None, e0=None, step=None,
+             nnorm=3, form='victoreen', pre1=None, pre2=-50,
+             norm1=100, norm2=None, larch=None, **kws):
+    """pre edge, normalization for XAFS
 
+    """
     if larch is None:
         raise Warning("cannot remove pre_edge -- larch broken?")
 
@@ -40,14 +42,20 @@ def pre_edge(energy, mu, group=None, larch=None, e0=None,
     p2 = max(np.where(energy <= e0+10.0)[0])
     ie0 = np.where(energy-e0 == min(abs(energy[p1:p2] - e0)))[0][0]
 
+    if pre1 is None:  pre1  = min(energy) - e0
+    if norm2 is None: norm2 = max(energy) - e0
+
     p1 = min(np.where(energy >= pre1+e0)[0])
     p2 = max(np.where(energy <= pre2+e0)[0])
     if p2-p1 < 2:
         p2 = min(len(energy), p1 + 2)
 
-    if form == 'linear':
-        coefs = polyfit(energy[p1:p2], mu[p1:p2], 1)
-        pre_edge = coefs[0] * energy + coefs[1]
+    nvict = 0
+    if form == 'victoreen':
+        nvict = 2
+    omu  = mu*energy**nvict
+    coefs = polyfit(energy[p1:p2], omu[p1:p2], 1)
+    pre_edge = (coefs[0] * energy + coefs[1]) * energy**(-nvict)
 
     # normalization
     p1 = min(np.where(energy >= norm1+e0)[0])
@@ -55,21 +63,19 @@ def pre_edge(energy, mu, group=None, larch=None, e0=None,
     if p2-p1 < 2:
         p2 = min(len(energy), p1 + 2)
 
-    coefs = polyfit(energy[p1:p2], mu[p1:p2], nnorm)
+    coefs = polyfit(energy[p1:p2], omu[p1:p2], nnorm)
     post_edge = 0
     for n, c in enumerate(reversed(list(coefs))):
-        post_edge += c * energy**n
+        post_edge += c * energy**(n-nvict)
 
     edge_step = post_edge[ie0] - pre_edge[ie0]
     norm  = (mu - pre_edge)/edge_step
-
     if larch.symtable.isgroup(group):
         setattr(group, 'e0', e0)
         setattr(group, 'pre_edge', pre_edge)
         setattr(group, 'post_edge', post_edge)
         setattr(group, 'edge_step', edge_step)
         setattr(group, 'norm', norm)
-
     return edge_step, e0
 
 def registerLarchPlugin():
