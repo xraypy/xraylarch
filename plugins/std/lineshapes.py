0ac1b0ceb69cd7e3c83938f3b0271e0cd6e17126
Matt Newville <newville@cars.uchicago.edu>
2012-01-29 15:17:53 -0600

    improved standard lineshape defs, no builtin amplitudes

--- plugins/std/lineshapes.py
+++ plugins/std/lineshapes.py
@@ -8,25 +8,33 @@ from scipy.special import gamma, beta
 
 log2 = log(2)
 
-def gaussian(x, amp, cen, sigma, larch=None):
-    "1 dimensional gaussian"
-    return (sqrt(log2/pi) / sigma) * amp * exp(-log2 * (1.0*x-cen) **2 / sigma**2)
+def gaussian(x, cen=0, sigma=1, larch=None):
+    """1 dimensional gaussian:
+    gaussian(x, cen, sigma)
+    """
+    return (sqrt(log2/pi) / sigma) * exp(-log2 * (1.0*x-cen) **2 / sigma**2)
 
-def lorentzian(x, amp, cen, sigma, larch=None):
-    "1 dimensional lorentzian"
-    return (amp  / (1 + ((1.0*x-cen)/sigma)**2) ) / (pi*sigma)
+def lorentzian(x, cen=0, sigma=1, larch=None):
+    """1 dimensional lorentzian
+    lorenztian(x, cen, sigma)
+    """
+    return (1.  / (1 + ((1.0*x-cen)/sigma)**2) ) / (pi*sigma)
 
-def pvoigt(x, amp, cen, sigma, frac, larch=None):
-    "1 dimensional pseudo-voight: amp*(1-frac)*gaussion + amp*frac*lorentzian"
-    return amp * (gaussiann(x, (1-frac), cen, sigma) +
-                  lorentzian(x, frac,    cen, sigma))
+def pvoigt(x, cen=0, sigma=1, frac=0.5, larch=None):
+    """1 dimensional pseudo-voight:
+    pvoigt(x, cen, sigma, frac)
+       = (1-frac)*gaussion(x,cen,sigma) + frac*lorentzian(x,cen, sigma)
+    """
+    return ((1-frac)*gaussiann(x, cen=cen, sigma=sigma) +
+                frac*lorentzian(x, cen=cen, sigma=sigma))
 
-def pearson7(x, amp, cen, sigma, expon, larch=None):
-    """"pearson7 lineshape, according to NIST StRD
-    though it seems wikpedia gives a different formula... """
-    scale = amp * gamma(expon) * sqrt((2**(1/expon) -1)) / (gamma(expon-0.5)) / (sigma*sqrt(pi))
+def pearson7(x, cen=0, sigma=1, expon=0.5, larch=None):
+    """pearson7 lineshape, according to NIST StRD
+    though it seems wikpedia gives a different formula...
+    pearson7(x, cen, sigma, expon)
+    """
+    scale = gamma(expon) * sqrt((2**(1/expon) -1)) / (gamma(expon-0.5)) / (sigma*sqrt(pi))
     return scale / (1 + ( ((1.0*x-cen)/sigma)**2) * (2**(1/expon) -1) )**expon
-
 
 def registerLarchPlugin():
     return ('_math', {'gaussian': gaussian,
