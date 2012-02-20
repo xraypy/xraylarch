4a33676ca95812565223959354ae4f27606f50ea
Matt Newville <newville@cars.uchicago.edu>
2012-02-20 12:08:44 -0600

    added std mathutils modules moved some builtins to this

--- lib/builtins.py
+++ lib/builtins.py
@@ -158,17 +158,6 @@ numpy_renames = {'ln':'log', 'asin':'arcsin', 'acos':'arccos',
                  'acosh':'arccosh', 'asinh':'arcsinh'}
 ##
 ## More builtin commands, to set up the larch language:
-def _deriv(arr, larch=None, **kws):
-    """take numerical derivitive of an array:"""
-    if not isinstance(arr, np.ndarray):
-        raise Warning("cannot take derivative of non-numeric array")
-    n = len(arr)
-    out  = np.zeros(n)
-    out[0] = arr[1] - arr[0]
-    out[n-1] = arr[n-1] - arr[n-2]
-    out[1:n-2] = [(arr[i+1] - arr[i-1])/2.0 for i in range(1, n-2)]
-    return out
-
 
 def _group(larch=None, **kws):
     """create a group"""
@@ -411,7 +400,6 @@ local_funcs = {'group':_group,
                'which': _which,
                'pause': _pause,
                'sleep': _sleep,
-               'deriv': _deriv,
                'reload':_reload,
                'run': _run,
                'help': _help,
