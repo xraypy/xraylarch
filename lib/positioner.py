06b1f7addf62b065be11a4224dd67d9b30e30cab
Matt Newville <newville@cars.uchicago.edu>
2012-03-30 16:17:10 -0500

    begin step scan app

---
+++ lib/positioner.py
@@ -0,0 +1,60 @@
+"""Positioner for Step Scan
+"""
+
+from epics import PV
+
+class Positioner(object):
+    """a positioner for a scan
+This **ONLY** sets an ordinate value for scan, it does *NOT*
+do a readback on this position -- add a ScanDetector for that!
+    """
+    def __init__(self, pvname, label=None, array=None, **kws):
+        self._pv = PV(pvname)
+        self._pv.get()
+        self._pv.get_ctrlvars()
+
+        self.label = label
+        self.array = array
+        self.extra_pvs = []
+
+    def __onComplete(self, pvname=None, **kws):
+        self.done = True
+
+    def move_to_start(self, wait=False):
+        """ move to starting position"""
+        self.move_to_pos(0, wait=wait)
+
+    def verify_array(self):
+        """return True if array is within the """
+        if array is None:
+            return True
+        if ((self._pv.upper_ctrl_limit is not None and
+             self._pv.upper_ctrl_limit < max(array)) or
+            (self._pv.lower_ctrl_limit is not None and
+             self._pv.lower_ctrl_limit > min(array))):
+            return False
+        return True
+
+    def move_to_pos(self, i, wait=False, timeout=600):
+        """move to i-th position in positioner array"""
+        if self.array is None or not self._pv.connected:
+            return
+        self.done = False
+        self._pv.put(self.array[i], callback=self.__onComplete)
+        if wait:
+            t0 = time.time()
+            while not self.done and time.time()-t0 < timeout:
+                time.sleep(1.e-4)
+
+    def pre_scan(self):
+        "method to run prior to scan: override for real action"
+        pass
+
+    def post_scan(self):
+        "method to run after to scan: override for real action"
+        pass
+
+    def at_break(self):
+        "method to run at break points: override for real action"
+        pass
+
