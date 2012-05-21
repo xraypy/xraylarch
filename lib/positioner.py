d12fb8792c8ef447b461b7d7ee6cdef4b80402ac
Matt Newville <newville@cars.uchicago.edu>
2012-05-21 10:08:05 -0500

    allow positioner to take a PV object or PV name

--- lib/positioner.py
+++ lib/positioner.py
@@ -10,27 +10,30 @@ This **ONLY** sets an ordinate value for scan, it does *NOT*
 do a readback on this position -- add a ScanDetector for that!
     """
     def __init__(self, pvname, label=None, array=None, **kws):
-        self.pv = PV(pvname)
-        self.pv.get()
-        self.pv.get_ctrlvars()
+        if isinstance(pvname, PV):
+            self.pv = pvname
+        else:
+            self.pv = PV(pvname)
+        self.pv.connect()
+        if self.pv.connected:
+            self.pv.get_ctrlvars()
 
         self.label = label
-        if label is None:
+        if label is None and self.pv.connected:
             desc = pvname
             if '.' in pvname:
                 idot = pvname.index('.')
                 descpv = pvname[:idot] + '.DESC'
             else:
-                descpv = pvname + '.DESC'                
+                descpv = pvname + '.DESC'
             try:
                 desc = caget(descpv)
             except:
                 pass
             self.label = desc
-                        
+
         if array is None: array  = []
         self.array = array
-        
         self.extra_pvs = []
 
     def __repr__(self):
@@ -74,7 +77,7 @@ do a readback on this position -- add a ScanDetector for that!
     def move_to(self, value, wait=False, timeout=600):
         """move to a value, optionally waiting"""
         self.pv.put(value, wait=wait, timeout=timeout)
-        
+
 
     def move_to_pos(self, i, wait=False, timeout=600):
         """move to i-th position in positioner array"""
