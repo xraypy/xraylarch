b26f7c71ca2081046459fa212fb21858c8200098
Matt Newville <newville@cars.uchicago.edu>
2012-05-21 10:09:35 -0500

    more (incompletegit diff lib/xafs_scan.py) enhancements to xafs_scan class

--- lib/xafs_scan.py
+++ lib/xafs_scan.py
@@ -26,20 +26,32 @@ class XAFS_Scan(StepScan):
         self.dwelltimes   = []
         self.dtime = dtime
         StepScan.__init__(self, **kws)
-        
-        if energy_pv is not None:
-            self.add_positioner(Positioner(energy_pv, label='Energy (drive)'))
-            
-        if read_pv is not None:
-            self.add_counter(Counter(read_pv, label='Energy (read)'))
 
-            
+        self.energy_pos = None
+        self.set_energy_pv(energy_pv, read_pv=read_pv)
+
+    def set_energy_pv(energy_pv, read_pv=None):
+        if energy_pv is not None:
+            self.energy_pos = Positioner(energy_pv, label='Energy'))
+            self.positioners = []
+            self.add_positioner(self.energy_pos)
+        if read_pv is not None:
+            self.add_counter(read_pv, label='Energy (read)')
+
+    def read_config_file(self, configfile):
+        """read configuration from INI file"""
+        pass
+
+    def save_config_file(self, configfile):
+        """write configuration from INI file"""
+        pass
+
     def add_region(self, start, stop, step=None, npts=None,
-                   relative=True, use_k=False, e0=None, 
+                   relative=True, use_k=False, e0=None,
                    dtime=None, dtime_final=None, dtime_wt=1):
         """add a region to an EXAFS scan.
         Note that scans must be added in order of increasing energy
-        
+
         """
         if e0 is None:
             e0 = self.e0
@@ -47,7 +59,7 @@ class XAFS_Scan(StepScan):
             dtime = self.dtime
         self.e0 = e0
         self.dtime = dtime
-            
+
         if npts is None and step is None:
             print 'add_region needs start, stop, and either step on npts'
             return
@@ -82,14 +94,13 @@ class XAFS_Scan(StepScan):
         self.energies = []
         self.dwelltimes = []
 
-
     def load_scan(self):
-        print 'load'
-        print 'Positioners: ', self.positioners
-        for p in self.positioners:
-            if p.label == 'Energy':
-                p.array = self.energies
-        print 'Detectors: ', self.detectors
+        print 'load scan'
+        if self.energy_pos is None:
+            print 'Need Energy Positioner!'
+            return
+        self.energy_pos.array = self.energies
+
         # set detector dwelltime array
         for d in self.detectors:
             if d.dwelltime_pv is not None:
@@ -98,9 +109,19 @@ class XAFS_Scan(StepScan):
                     if p.pv.pvname == d.dwelltime_pv.pvname:
                         thispos = p
                 if thispos is None:
-                    thispos = Positioner(d.dwelltime_pv,
+                    thispos = Positioner(d.dwelltime_pv.pvname,
                                          label = "%s time" % d.label)
+                    self.add_positioner(thispos)
                 thispos.array = self.dwelltimes
+        print 'Positioners: ', self.positioners
+        print 'Detectors: ', self.detectors
+        self.set_dwelltime(self.dwelltimes[0])
+        if self.energy_pre_scan not in self.pre_scan_methods:
+            self.pre_scan_methods.append(self.energy_pre_scan)
 
-                
-        
+    def energy_pre_scan(self):
+        """special pre_scan command for XAFS scans
+        put here to be overridden -- may need setting mono modes.
+        """
+        self.energy_pos.move_to_start()
+
