9dc649be27654d0ef7b304acc3250324aa16675f
Matt Newville <newville@cars.uchicago.edu>
2012-06-08 07:11:11 -0500

    improved several tests

--- tests/basictest_1.py
+++ tests/basictest_1.py
@@ -23,21 +23,14 @@ for   i in range(5):
 
 print '== Groups: Name       # members    SearchGroup?'
 
-allgroups = s._subgroups()
-fmt = '     %s     %5i      yes'
-for i in s._sys.searchGroups:
-    print fmt % ((i+' '*15)[:15], len(s.get_symbol(i)))
-    allgroups.remove(i)
-
+allgroups = ['_main'] + s._subgroups()
+fmt = '     %s     %5i      %s'
 for i in allgroups:
-    sym = s.get_symbol(i)
-    # print i, larch.symboltable.isgroup(sym)
-    if larch.symboltable.isgroup(sym):
-        print fmt % ((i+' '*15)[:15], len(sym._members()))
-    else:
-        print '     %s         no' % ((i+' '*15)[:15])
+    print fmt % ((i+' '*15)[:15], len(dir(s.get_symbol(i))),
+                 {True:'yes', False:'no'}[i in s._sys.searchGroups])
+
 
 print s.show_group('g3')
 
 print '== Passed all tests for Symbol Test 01 '
-#s.showTable()
+
