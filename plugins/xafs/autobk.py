544bcce3edda92227c232de38748e0e44eae75ce
Matt Newville <newville@cars.uchicago.edu>
2012-06-14 13:41:31 -0500

    autobk -- better initial guess for spline, added debugging

--- plugins/xafs/autobk.py
+++ plugins/xafs/autobk.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 
-import numpy as np
 import sys, os
+
+import numpy as np
+from scipy.interpolate import splrep, splev, UnivariateSpline
+
 import larch
+from larch import Group, Parameter
 from larch.larchlib import plugin_path
 
 # put the 'std' and 'xafs' (this!) plugin directories into sys.path
 sys.path.insert(0, plugin_path('std'))
 sys.path.insert(0, plugin_path('xafs'))
+sys.path.insert(0, plugin_path('fitter'))
 
 # now we can reliably import other std and xafs modules...
 from mathutils import _index_nearest, realimag
@@ -16,9 +21,10 @@ from xafsutils import ETOK
 from xafsft import ftwindow, xafsft_fast
 from pre_edge import find_e0
 
-from scipy.interpolate import splrep, splev, UnivariateSpline
+from minimizer import Minimizer
 
-from lmfit import Parameters, Minimizer
+FMT_COEF = 'c%2.2i'
+# from lmfit import Parameters, Minimizer
 
 def spline_eval(kraw, mu, knots, coefs, order, kout):
     """eval bkg(kraw) and chi(k) for knots, coefs, order"""
@@ -26,15 +32,17 @@ def spline_eval(kraw, mu, knots, coefs, order, kout):
     chi = UnivariateSpline(kraw, mu-bkg, s=0)(kout)
     return bkg, chi
 
-def __resid(pars, knots=None, order=3, irbkg=1, nfft=2048,
+def __resid(pars, ncoefs=1, knots=None, order=3, irbkg=1, nfft=2048,
             kraw=None, mu=None, kout=None, ftwin=1, **kws):
-    coefs = [p.value for p in pars.values()]
+
+    coefs = [getattr(pars, FMT_COEF % i) for i in range(ncoefs)]
     bkg, chi = spline_eval(kraw, mu, knots, coefs, order, kout)
     return realimag(xafsft_fast(chi*ftwin, nfft=nfft)[:irbkg])
 
 def autobk(energy, mu, rbkg=1, nknots=None, group=None, e0=None,
            kmin=0, kmax=None, kw=1, dk=0, win=None, vary_e0=True,
-           chi_std=None, nfft=2048, kstep=0.05, _larch=None):
+           chi_std=None, nfft=2048, kstep=0.05, debug=False,
+           _larch=None):
     """Use Autobk algorithm to remove XAFS background
     Options are:
       rbkg -- distance out to which the chi(R) is minimized
@@ -63,31 +71,42 @@ def autobk(energy, mu, rbkg=1, nknots=None, group=None, e0=None,
     nspline = max(4, min(60, 2*int(rbkg*(kmax-kmin)/np.pi) + 1))
     spl_y  = np.zeros(nspline)
     spl_k  = np.zeros(nspline)
+    spl_e  = np.zeros(nspline)
     for i in range(nspline):
         q = kmin + i*(kmax-kmin)/(nspline - 1)
         ik = _index_nearest(kraw, q)
+        
         i1 = min(len(kraw)-1, ik + 5)
         i2 = max(0, ik - 5)
         spl_k[i] = kraw[ik]
-        spl_y[i] = (2*mu[ik] + mu[i1] + mu[i2] ) / 4.0
+        spl_e[i] = energy[ik+ie0]
+        spl_y[i] = (2*mu[ik+ie0] + mu[i1+ie0] + mu[i2+ie0] ) / 4.0
 
     # get spline represention: knots, coefs, order=3
     # coefs will be varied in fit.
     knots, coefs, order = splrep(spl_k, spl_y)
 
     # set fit parameters from initial coefficients
-    fparams = Parameters()
-    for i, v in enumerate(coefs):
-        fparams.add("c%i" % i, value=v, vary=i<len(spl_y))
+    ncoefs = len(coefs)
+    params = Group()
+    for i in range(ncoefs):
+        name = FMT_COEF % i
+        p = Parameter(coefs[i], name=name, vary=i<len(spl_y))
+        p._getval()
+        setattr(params, name, p)
 
-    fitkws = dict(knots=knots, order=order, kraw=kraw, mu=mu[ie0:],
+    initbkg, initchi = spline_eval(kraw, mu[ie0:], knots, coefs, order, kout)
+              
+    fitkws = dict(ncoefs=len(coefs),
+                  knots=knots, order=order, kraw=kraw, mu=mu[ie0:],
                   irbkg=irbkg, kout=kout, ftwin=ftwin, nfft=nfft)
     # do fit
-    fit = Minimizer(__resid, fparams, fcn_kws=fitkws)
+    fit = Minimizer(__resid, params, fcn_kws=fitkws, _larch=_larch)
     fit.leastsq()
 
     # write final results
-    coefs = [p.value for p in fparams.values()]
+    coefs = [getattr(params, FMT_COEF % i) for i in range(ncoefs)]
+
     bkg, chi = spline_eval(kraw, mu[ie0:], knots, coefs, order, kout)
     obkg  = np.zeros(len(mu))
     obkg[:ie0] = mu[:ie0]
@@ -97,6 +116,17 @@ def autobk(energy, mu, rbkg=1, nknots=None, group=None, e0=None,
         group.chie = mu-obkg
         group.k    = kout
         group.chi  = chi
+        if debug:
+            group.spline_params = params
+            ix_bkg = np.zeros(len(mu))
+            ix_bkg[:ie0] = mu[:ie0]
+            ix_bkg[ie0:] = initbkg            
+            group.init_bkg = ix_bkg
+            group.init_chi = initchi
+            group.spline_e = spl_e
+            group.spline_y = np.array([coefs[i] for i in range(nspline)])
+            group.spline_yinit = spl_y
+            
 
 def registerLarchPlugin():
     return ('_xafs', {'autobk': autobk})
