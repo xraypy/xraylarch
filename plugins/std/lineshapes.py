c02caa019a7d9120a16f2e810e77ee6ab034800d
Matt Newville <newville@cars.uchicago.edu>
2012-07-07 22:23:47 -0500

    add erf() as easily available lineshape

--- plugins/std/lineshapes.py
+++ plugins/std/lineshapes.py
@@ -4,12 +4,12 @@ Some common lineshapes and distribution functions
 """
 from numpy import pi, log, exp, sqrt
 
-from scipy.special import gamma, beta
+from scipy.special import gamma, beta, erf
 from larch import param_value
 
 log2 = log(2)
 
-def gaussian(x, cen=0, sigma=1, _larch=None):
+def gaussian(x, cen=0, sigma=1):
     """1 dimensional gaussian:
     gaussian(x, cen, sigma)
     """
@@ -17,7 +17,7 @@ def gaussian(x, cen=0, sigma=1, _larch=None):
     sigma = param_value(sigma)
     return (sqrt(log2/pi)/sigma) * exp(-log2 * (1.0*x-cen) **2 / sigma**2)
 
-def lorentzian(x, cen=0, sigma=1, _larch=None):
+def lorentzian(x, cen=0, sigma=1):
     """1 dimensional lorentzian
     lorenztian(x, cen, sigma)
     """
@@ -25,7 +25,7 @@ def lorentzian(x, cen=0, sigma=1, _larch=None):
     sigma = param_value(sigma)
     return (1.  / (1 + ((1.0*x-cen)/sigma)**2) ) / (pi*sigma)
 
-def pvoigt(x, cen=0, sigma=1, frac=0.5, _larch=None):
+def pvoigt(x, cen=0, sigma=1, frac=0.5):
     """1 dimensional pseudo-voight:
     pvoigt(x, cen, sigma, frac)
        = (1-frac)*gaussion(x,cen,sigma) + frac*lorentzian(x,cen, sigma)
@@ -36,7 +36,7 @@ def pvoigt(x, cen=0, sigma=1, frac=0.5, _larch=None):
     return ((1-frac)*gaussian(x, cen=cen, sigma=sigma) +
                 frac*lorentzian(x, cen=cen, sigma=sigma))
 
-def pearson7(x, cen=0, sigma=1, expon=0.5, _larch=None):
+def pearson7(x, cen=0, sigma=1, expon=0.5):
     """pearson7 lineshape, according to NIST StRD
     though it seems wikpedia gives a different formula...
     pearson7(x, cen, sigma, expon)
@@ -47,8 +47,13 @@ def pearson7(x, cen=0, sigma=1, expon=0.5, _larch=None):
     scale = gamma(expon) * sqrt((2**(1/expon) -1)) / (gamma(expon-0.5)) / (sigma*sqrt(pi))
     return scale / (1 + ( ((1.0*x-cen)/sigma)**2) * (2**(1/expon) -1) )**expon
 
+def _erf(x):
+    """error function.  = 2/sqrt(pi)*integral(exp(-t**2), t=[0, z])"""
+    return erf(x)
+
 def registerLarchPlugin():
     return ('_math', {'gaussian': gaussian,
                       'lorentzian': lorentzian,
                       'pvoigt': pvoigt,
-                      'pearson7': pearson7})
+                      'pearson7': pearson7,
+                      'erf': _erf})
