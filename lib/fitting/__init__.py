abd50acf7b25ee6e2da1b42d6292f8b3d6986a4a
Matt Newville <newville@cars.uchicago.edu>
2012-08-05 13:15:00 -0500

    Working on major refactor to move minimization (was plugins/fitter) and xafs analysis code (was plugins/xafs) from larch plugins folder to main installed larch library.  This will allow more "core functionality" to be available directly from python, without needing the larch interpreter to access this functionality.   In addition, plugins from plugins/std may also be moved to installed larch libraries.
    As a general rule, these moved plugins should not add dependencies beyond
    numpy and scipy.

---
+++ lib/fitting/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from parameter import Parameter, isParameter, param_value
+from minimizer import minimize, fit_report
+
+
+def param(*args, **kws):
+    "create a fitting Parameter as a Variable"
+    if len(args) > 0 and isinstance(args[0], (str, unicode)):
+        expr = args[0]
+        args = args[1:]
+        kws.update({'expr': expr})
+    out = Parameter(*args, **kws)
+    if 'name' not in kws:
+        return out
+
+def guess(value,  **kws):
+    """create a fitting Parameter as a Variable.
+    A minimum or maximum value for the variable value can be given:
+       x = guess(10, min=0)
+       y = guess(1.2, min=1, max=2)
+    """
+    kws.update({'vary':True})
+    return param(value, **kws)
+
