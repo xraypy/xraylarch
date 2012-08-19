3923509b16e3766e2ddd160b5b33c9911d11e20c
Matt Newville <newville@cars.uchicago.edu>
2012-08-19 08:40:45 -0500

    added more lineshapes

--- plugins/std/lineshapes.py
+++ plugins/std/lineshapes.py
@@ -4,10 +4,12 @@ Some common lineshapes and distribution functions
 """
 from numpy import pi, log, exp, sqrt
 
-from scipy.special import gamma, beta, erf
+from scipy.special import gamma, gammaln, beta, betaln, erf, erfc, wofz
 from larch import param_value
 
 log2 = log(2)
+s2pi = sqrt(2*pi)
+spi  = sqrt(pi)
 
 def gaussian(x, cen=0, sigma=1):
     """1 dimensional gaussian:
@@ -15,7 +17,7 @@ def gaussian(x, cen=0, sigma=1):
     """
     cen = param_value(cen)
     sigma = param_value(sigma)
-    return (1./(sqrt(2*pi)*sigma)) * exp(-(1.0*x-cen) **2 / (2*sigma)**2)
+    return (1./(2*spi*sigma)) * exp(-(1.0*x-cen) **2 / (2*sigma)**2)
 
 def lorentzian(x, cen=0, sigma=1):
     """1 dimensional lorentzian
@@ -25,8 +27,18 @@ def lorentzian(x, cen=0, sigma=1):
     sigma = param_value(sigma)
     return (1.  / (1 + ((1.0*x-cen)/sigma)**2) ) / (pi*sigma)
 
+def voigt(x, cen=0, sigma=1, gamma=None):
+    """1 dimensional voigt function.
+
+    see http://en.wikipedia.org/wiki/Voigt_profile
+    """
+    if gamma is None:
+        gamma = sigma
+    z = (x-cen + 1j*gamma)/ (sigma*sqrt(2))
+    return wofz(z).real / (sigma*s2pi)
+
 def pvoigt(x, cen=0, sigma=1, frac=0.5):
-    """1 dimensional pseudo-voight:
+    """1 dimensional pseudo-voigt:
     pvoigt(x, cen, sigma, frac)
        = (1-frac)*gaussion(x,cen,sigma) + frac*lorentzian(x,cen, sigma)
     """
@@ -51,9 +63,29 @@ def _erf(x):
     """error function.  = 2/sqrt(pi)*integral(exp(-t**2), t=[0, z])"""
     return erf(x)
 
+def _erfc(x):
+    """complented error function.  = 1 - erf(x)"""
+    return erfc(x)
+
+def _wofz(x):
+    """fadeeva function for complex argument. = exp(-x**2)*erfc(-i*x)"""
+    return wofz(x)
+
+def _gamma(x):
+    """gamma function"""
+    return gamma(x)
+
+def _gammaln(x):
+    """log of absolute value of gamma function"""
+    return gammaln(x)
+
 def registerLarchPlugin():
     return ('_math', {'gaussian': gaussian,
                       'lorentzian': lorentzian,
+                      'voigt': voigt,
                       'pvoigt': pvoigt,
                       'pearson7': pearson7,
-                      'erf': _erf})
+                      'gammaln': _gammaln,
+                      'erf': _erf,
+                      'erfc': _erfc,
+                      'wofz': _wofz})
