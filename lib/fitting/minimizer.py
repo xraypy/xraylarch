d846d47d9927b102a751e2103553c911b64dc00b
Matt Newville <newville@cars.uchicago.edu>
2012-09-20 14:15:13 -0500

    minimizer: add prepar_fit() to Minimize.__init__ and setting of _sys.paramGroup to fit_report, both to better ensure that _sys.paramGroup is set

--- lib/fitting/minimizer.py
+++ lib/fitting/minimizer.py
@@ -94,6 +94,7 @@ or set  leastsq_kws['maxfev']  to increase this maximum."""
 
         self.jacfcn = jacfcn
         self.__prepared = False
+        self.prepare_fit()
 
     def __update_params(self, fvars):
         """
@@ -405,6 +406,8 @@ def fit_report(group, show_correl=True, min_correl=0.1, _larch=None, **kws):
     if not _larch.symtable.isgroup(group):
         print('must pass Group to fit_report()')
         return
+    _larch.symtable._sys.paramGroup = group
+    
     topline = '===================== FIT RESULTS ====================='
     header = '[[%s]] %s'
     varformat  = '   %12s = %s (init= % f)'
