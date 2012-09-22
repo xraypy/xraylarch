0903300375a96a675e8abe9bbc213d6299874b7d
Matt Newville <newville@cars.uchicago.edu>
2012-09-22 02:46:23 -0500

    xafs.autobk: clamps implemented, uncertainties in mu0 and chi calculated if possible

--- plugins/xafs/autobk.py
+++ plugins/xafs/autobk.py
@@ -19,7 +19,16 @@ from xafsutils import ETOK
 from xafsft import ftwindow, xafsft_fast
 from pre_edge import find_e0, pre_edge
 
-# from minimizer import Minimizer
+# check for uncertainties package
+HAS_UNCERTAIN = False
+try:
+    import uncertainties
+    from uncertainties import correlated_values
+    from uncertainties.unumpy import uarray
+    HAS_UNCERTAIN = True
+except ImportError:
+    pass
+
 
 FMT_COEF = 'c%2.2i'
 
@@ -30,18 +39,27 @@ def spline_eval(kraw, mu, knots, coefs, order, kout):
     return bkg, chi
 
 def __resid(pars, ncoefs=1, knots=None, order=3, irbkg=1, nfft=2048,
-            kraw=None, mu=None, kout=None, ftwin=1, chi_std=None, **kws):
+            kraw=None, mu=None, kout=None, ftwin=1, chi_std=None,
+            nclamp=2, clamp_lo=1, clamp_hi=1, **kws):
 
     coefs = [getattr(pars, FMT_COEF % i) for i in range(ncoefs)]
     bkg, chi = spline_eval(kraw, mu, knots, coefs, order, kout)
     if chi_std is not None:
         chi = chi - chi_std
-    return realimag(xafsft_fast(chi*ftwin, nfft=nfft)[:irbkg])
+    sum2 = (chi*chi).sum() * nclamp / 10.0
+    out = realimag(xafsft_fast(chi*ftwin, nfft=nfft)[:irbkg])
+
+    if clamp_lo > 0 and nclamp > 0:
+        out = np.concatenate((out, clamp_lo*chi[:nclamp]/sum2))
+    if clamp_hi > 0 and nclamp > 0:
+        out = np.concatenate((out, clamp_hi*chi[-nclamp:]/sum2))
+    return out
 
 def autobk(energy, mu, group=None, rbkg=1, nknots=None, e0=None,
            edge_step=None, kmin=0, kmax=None, kweight=1, dk=0,
            win='hanning', k_std=None, chi_std=None, nfft=2048, kstep=0.05,
-           pre_edge_kws=None, debug=False, _larch=None, **kws):
+           pre_edge_kws=None, debug=False, _larch=None, nclamp=2,
+           clamp_lo=1, clamp_hi=1, **kws):
 
     """Use Autobk algorithm to remove XAFS background
     Options are:
@@ -93,7 +111,7 @@ def autobk(energy, mu, group=None, rbkg=1, nknots=None, e0=None,
                                      window=win, dx=dk)
 
     # calc k-value and initial guess for y-values of spline params
-    nspline = max(4, min(60, 2*int(rbkg*(kmax-kmin)/np.pi) + 1))
+    nspline = max(4, min(128, 2*int(rbkg*(kmax-kmin)/np.pi) + 1))
     spl_y  = np.zeros(nspline)
     spl_k  = np.zeros(nspline)
     spl_e  = np.zeros(nspline)
@@ -124,7 +142,8 @@ def autobk(energy, mu, group=None, rbkg=1, nknots=None, e0=None,
 
     fitkws = dict(ncoefs=len(coefs), chi_std=chi_std,
                   knots=knots, order=order, kraw=kraw, mu=mu[ie0:],
-                  irbkg=irbkg, kout=kout, ftwin=ftwin, nfft=nfft)
+                  irbkg=irbkg, kout=kout, ftwin=ftwin, nfft=nfft,
+                  nclamp=nclamp, clamp_lo=clamp_lo, clamp_hi=clamp_hi)
     # do fit
     fit = Minimizer(__resid, params, fcn_kws=fitkws, _larch=_larch, toler=1.e-4)
     fit.leastsq()
@@ -141,17 +160,47 @@ def autobk(energy, mu, group=None, rbkg=1, nknots=None, e0=None,
         group.chie = (mu-obkg)/edge_step
         group.k    = kout
         group.chi  = chi/edge_step
-        if debug:
-            group.spline_params = params
-            ix_bkg = np.zeros(len(mu))
-            ix_bkg[:ie0] = mu[:ie0]
-            ix_bkg[ie0:] = initbkg
-            group.init_bkg = ix_bkg
-            group.init_chi = initchi/edge_step
-            group.spline_e = spl_e
-            group.spline_y = np.array([coefs[i] for i in range(nspline)])
-            group.spline_yinit = spl_y
+        gdet = group.autobk_details = Group()
+        gdet.spline_params = params
+        ix_bkg = np.zeros(len(mu))
+        ix_bkg[:ie0] = mu[:ie0]
+        ix_bkg[ie0:] = initbkg
+        gdet.init_bkg = ix_bkg
+        gdet.init_chi = initchi/edge_step
+        gdet.spline_e = spl_e
+        gdet.spline_y = np.array([coefs[i] for i in range(nspline)])
+        gdet.spline_yinit = spl_y
+        if HAS_UNCERTAIN:
+            # now, calculate delta_mu0 and delta_chi
+            vbest, vstd = [], []
+            for n in fit.var_names:
+                par = getattr(params, n)
+                vbest.append(par.value)
+                vstd.append(par.stderr)
+            uvars = correlated_values(vbest, params.covar)
+            # uncertainty in bkg (aka mu0)
+            # note that much of this is working around
+            # limitations in the uncertainty package that make it
+            #  1. take an argument list (not array)
+            #  2. work on returned scalars (but not arrays)
+            #  3. not handle kw args and *args well (so use
+            #     of global "index" is important here)
+            def my_dsplev(*args):
+                coefs = np.array(args)
+                return splev(kraw, [knots, coefs, order])[index]
+            fdbkg = uncertainties.wrap(my_dsplev)
+            dmu0  = [fdbkg(*uvars).std_dev() for index in range(len(bkg))]
+            gdet.dbkg = np.zeros(len(mu))
+            gdet.dbkg[ie0:] = np.array(dmu0)
 
+            # uncertainty in chi (see notes above)
+            def my_dchi(*args):
+                coefs = np.array(args)
+                b, c = spline_eval(kraw, mu[ie0:], knots, coefs, order, kout)
+                return c[index]
+            fdchi = uncertainties.wrap(my_dchi)
+            dci   = [fdchi(*uvars).std_dev() for index in range(len(kout))]
+            gdet.dchi  = np.array(dci)/edge_step
 
 def registerLarchPlugin():
     return ('_xafs', {'autobk': autobk})
