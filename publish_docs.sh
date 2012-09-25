da822ce794b5d6b122df359366c3a77709b1b15a
Matt Newville <newville@cars.uchicago.edu>
2012-09-25 09:20:40 -0500

    updated publish_docs.sh

--- publish_docs.sh
+++ publish_docs.sh
@@ -1,43 +1,59 @@
 installdir='/www/apache/htdocs/software/python/lmfit'
 docbuild='doc/_build'
 name='larch'
+
+
+finish() {
+   echo "# git checkout master"
+   git checkout master
+   if  [ $? -ne 0 ]  ; then
+      echo '# git checkout master failed.'
+      exit
+   fi
+}
+
 cd doc
 echo '# Making docs'
 make all
 cd _build/html
-tar czf ../../../docs.tgz *
-
+tar czf ../../../_docs.tgz *
 cd ../../../
 #
 echo "# Switching to gh-pages branch"
 git checkout gh-pages
+git pull
 
 if  [ $? -ne 0 ]  ; then
-  echo ' failed.'
-  exit
+  echo '# git pull on gh-pages branch failed.'
+  finish
 fi
 
-tar xzf docs.tgz 
-
+tar xzf _docs.tgz 
 echo "# commit changes to gh-pages branch"
-git add *.html */*.html  *.js objects.inv searchindex.js larch.pdf _sources/ _static/ _images/ _images/math
+git add *.html */*.html larch.pdf
+git add *.js objects.inv searchindex.js 
+git add  _sources/ _static/ _images/ _images/math
 git commit -am "updated docs"
 
 if  [ $? -ne 0 ]  ; then
-  echo ' failed.'
-  exit
+  echo '# git commit of all doc changes failed.'
+  finish
 fi
 
 echo "# Pushing docs to github"
-git push
+##git push
+##if  [ $? -ne 0 ]  ; then
+##  echo '# git push failed.'
+##  finish
+##fi
 
 echo "# switch back to master branch"
 git checkout master
-
 if  [ $? -ne 0 ]  ; then
-  echo ' failed.'
-  exit
+  echo '# checkout master failed.'
+  finish
 fi
+
 
 # # install locally
 # echo "# Installing docs to CARS web pages"
