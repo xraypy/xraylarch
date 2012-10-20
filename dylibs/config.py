b9539ff4048c3edb261519937f9badf240c160af
Matt Newville <newville@cars.uchicago.edu>
2012-10-19 22:42:10 -0500

    moved dlls to new location, soted by platform/arch

---
+++ dylibs/config.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+#  this script returns the directory into which dlls
+#  should be placed for the present architecture.
+#  current options are:
+#     win32/     Windows, 32-bit
+#     win64/     Windows, 64-bit
+#     linux32/   Linux, 32-bit
+#     linux64/   Linux, 64-bit
+#     darwin/    Mac OS X, universal dynamic libraries
+
+import  os, sys
+from platform import uname, architecture
+system, node, release, version, mach, processor = uname()
+arch = architecture()[0]
+
+dlldir = None
+makefile = None
+if os.name == 'nt':
+    dlldir = 'win32'
+    makefile = 'Mk.win32'
+    if arch.startswith('64'):
+        dlldir = 'win64'
+else:
+    if system.lower().startswith('linu'):
+        dlldir = 'linux32'
+        makefile = 'Mk.linux'
+        if arch.startswith('64'):    dlldir = 'linux64'
+    elif system.lower().startswith('darw'):
+        dlldir = 'darwin'
+        makefile = 'Mk.macosx'
+
+if dlldir is not None:
+    os.system('cp %s Mk.config' % makefile)
+    fout = open('Mk.install', 'w')
+    fout.write("INSTALLDIR=../../dlls/%s/" % dlldir)
+    fout.write("")
+    fout.close()
+
--- lib/version.py
+++ lib/version.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
-__date__    = '17-Sept-2012'
-__version__ = '0.9.14'
+__date__    = '19-Oct-2012'
+__version__ = '0.9.15'
 
