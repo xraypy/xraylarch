bc7b35099150947f46ccb4a04aa15e4252265227
Matt Newville <newville@cars.uchicago.edu>
2012-12-09 08:58:49 -0600

    several improvements to xmlrpc server

--- lib/xmlrpc_server.py
+++ lib/xmlrpc_server.py
@@ -16,10 +16,13 @@ except ImportError:
     HAS_WX = False
 
 class LarchServer(SimpleXMLRPCServer):
-    def __init__(self, host='127.0.0.1', port=5465, with_wx=True, **kws):
+    def __init__(self, host='127.0.0.1', port=5465, with_wx=True,
+                 local_echo=True, **kws):
         self.keep_alive = True
         self.port = port
         self.with_wx = HAS_WX and with_wx
+        self.local_echo = local_echo
+        self.out_buffer = []
         SimpleXMLRPCServer.__init__(self, (host, port),
                                     logRequests=False, allow_none=True, **kws)
         self.initialized = False
@@ -31,6 +34,23 @@ class LarchServer(SimpleXMLRPCServer):
         self.register_function(self.larch_exec,    'larch')
         self.register_function(self.wx_update,     'wx_update')
         self.register_function(self.get_data,      'get_data')
+        self.register_function(self.get_messages,  'get_messages')
+        self.register_function(self.len_messages,  'len_messages')
+
+    def write(self, text):
+        self.out_buffer.append(text)
+        if self.local_echo:
+            print text
+
+    def get_messages(self):
+        if self.local_echo:
+            print '== clear output buffer (%i)' % len(self.out_buffer)
+        out = '\n'.join(self.out_buffer)
+        self.out_buffer = []
+        return out
+
+    def len_messages(self):
+        return len(self.out_buffer)
 
     def run(self):
         """run server until keep_alive is set to False"""
@@ -70,8 +90,10 @@ class LarchServer(SimpleXMLRPCServer):
         return 1
 
     def initialize_larch(self):
-        self.larch  = Interpreter()
-        self.input  = InputText(prompt='', _larch=self.larch)
+        self.larch  = Interpreter(writer=self)
+        self.input  = InputText(prompt='', _larch=self.larch,
+                                interactive=False)
+
         self.larch.run_init_scripts()
         self.wxapp = None
         self.wx_evtloop = None
