1132e3e10cc42cb4590d57f28870750a2da7562f
Matt Newville <newville@cars.uchicago.edu>
2013-01-27 12:29:56 -0600

    more additions to server.py -- still not even close to done

--- lib/server.py
+++ lib/server.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 
 import time
+import threading
+
 from .scandb import ScanDB
 
-dbname = 'N1.sdb'
-
-
 class ScanServer():
-    def __init__(self, dbname='N1.sdb', **kws):
-        self.db = ScanDB(dbname, **kws)
-        
+    def __init__(self, dbname=None, **kws):
+        self.db = None
         self.abort = False
+        self.command_in_progress = False
+        if dbname is not None:
+            self.connect(dbname, **kws)
 
-    def sleep(self, t=0.025):
+    def connect(self, dbname, **kws):
+        """connect to Scan Database"""
+        self.db = ScanDB(dbname, **kws)
+
+    def sleep(self, t=0.05):
         try:
             time.sleep(t)
         except KeyboardInterrupt:
@@ -23,31 +28,68 @@ class ScanServer():
 
     def execute_command(self, req):
         print 'EXECUTE Command ', req
-        time.sleep(0.5)
-        self.scandb.set_command_status(req.id, 'finished')
-        
+        print 'req.command = ', req.command
+        print 'req.arguments = ', req.arguments
+        print 'req.status_id, req.status = ', req.status_id, req.status
+        print 'req.scandefs = ', req.scandefs
+        print 'req.request_time = ', req.request_time
+        print 'req.start_time = ', req.start_time
+        print 'req.modify_time = ', req.modify_time
+        print 'req.output_value = ', req.output_value
+        print 'req.output_file = ', req.output_file
+
+        cmd_thread = Thread(target=self.do_command,
+                            kwargs=dict(req=req),
+                            name='cmd_thread')
+
+        self.db.set_command_status(req.id, 'starting')
+        self.command_in_progress = True
+        cmd_thread.start()
+        cmd_thread.join()
+        self.db.set_command_status(req.id, 'finished')
+        self.command_in_progress = False
+
+    def do_command(self, req):
+        print 'IN do_command ', req
+        time.sleep(3.0)
+        print 'command done!'
+
+    def look_for_interrupt_requests(self):
+        """set interrupt requests:
+        abort / pause / resume
+        it is expected that
+        """
+        get = self.db.get_info
+        self.abort_request = get('request_command_abort') == '1'
+        self.pause_request = get('request_command_pause') == '1'
+        self.resume_request = get('request_command_resume') == '1'
+
     def mainloop(self):
         print "starting server"
 
         while True:
             self.sleep()
+            print 'tick'
             if self.abort:   break
-            reqs = self.scandb.get_commands('requested')
-            if len(reqs) == 0:
-                self.sleep(t=0.05)
+            reqs = self.db.get_commands('requested')
+            if self.command_in_progress:
+                self.look_for_interrupt_requests()
+                self.sleep(t=0.10)
+            if len(reqs) == 0 or self.command_in_progress:
+                self.sleep(t=0.10)
             else:
                 nextreq = reqs.pop()
                 print 'Will do next request: ', nextreq
                 self.execute_command(nextreq)
-                
+
         # mainloop end
         self.finish()
-        
-        
-    
-    
+
+
+
+
 if __name__  == '__main__':
-    s = ScanServer(dbname='N1.sdb')
+    s = ScanServer(dbname='A.sdb')
     s.mainloop()
-    
-    
+
+
