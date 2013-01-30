e794839376fd9f2a1cc4e3faa289b9a2d6774643
Matt Newville <newville@cars.uchicago.edu>
2013-01-29 22:20:47 -0600

    updated (slightly) xmlrpc examples

--- examples/rpc/example_client.py
+++ examples/rpc/example_client.py
@@ -5,30 +5,26 @@ import time
 import json
 from larch.utils.jsonutils import json_decode
 s = xmlrpclib.ServerProxy('http://127.0.0.1:4966')
-print ' Methods: ', s.system.listMethods()
-print s.system.methodHelp('dir')
 
+print 'Avaialable Methods from XML-RPC server: ', s.system.listMethods()
+print s.system.methodHelp('dir')
 
 s.larch('m = 222.3')
 
 s.larch('g = group(x=linspace(0, 10, 11))')
 s.larch('g.z = cos(g.x)')
+
+# show and print will be done in server process of course!!!
 s.larch('show(g)')
 s.larch('print g.z[3:10]')
-# s.larch('newplot(g.x, g.z)')
-# s.wx_interact(10.2)
-# 
-# print ' All done! '
-# 
-s.larch('show(g)')
-# 
+
 gx  = json_decode(s.get_data('g.z'))
 print 'm = ', s.get_data('m')
 print 'x = ', s.get_data('x')
- 
-print 'gx = ',  gx, type(gx), gx.dtype
-# 
 
-s.exit()
+print 'gx = ',  gx, type(gx), gx.dtype
+
+# could tell server to exit!
+# s.exit()
 
 
--- examples/rpc/example_client2.py
+++ examples/rpc/example_client2.py
@@ -1,14 +1,20 @@
 
+import os
 import xmlrpclib
 lserve = xmlrpclib.ServerProxy('http://localhost:4966')
 
 # print lserve.system.listMethods()
-lserve.chdir('/Users/newville/Codes/xraylarch/examples/feffit')
+
+parent, thisdir = os.path.split(os.path.abspath(os.curdir))
+
+lserve.chdir(os.path.join(parent, 'feffit'))
+
 lserve.larch("run('doc_feffit1.lar')")
+
 print '## Messages: '
 print lserve.get_messages()
 
-# allow interaction with plots drwan by server, for a while: 
+# allow interaction with plots drwan by server, for a while:
 i = 0
 while i < 400:
     i = i + 1
