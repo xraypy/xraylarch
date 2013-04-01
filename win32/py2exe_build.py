ceca5b91f5ec4eca89566ec64b5a045d7dfd0993
Matt Newville <newville@cars.uchicago.edu>
2013-04-01 14:26:45 -0500

    including GSE MapViewer in py2exe apps

--- win32/py2exe_build.py
+++ win32/py2exe_build.py
@@ -17,30 +17,39 @@ import numpy
 import scipy
 import matplotlib
 import h5py
-import epics
 import Image
 import sqlalchemy
 import wx
 import ctypes
 import ctypes.util
-ldll =  ctypes.windll.LoadLibrary
 
+import scipy.io.netcdf
+from scipy.io.netcdf import netcdf_file
+
+loadlib =  ctypes.windll.LoadLibrary
+
+# larch library bits...
 from larch.larchlib import get_dll
-
 cllib = get_dll('cldata')
 
+# matplotlib, wxmplot
 matplotlib.use('WXAgg')
+mpl_data_files = matplotlib.get_py2exe_datafiles()
 import wxmplot
 
-ca = epics.ca.initialize_libca()
-mpl_data_files = matplotlib.get_py2exe_datafiles()
+# epics
+try:
+    import epics
+    ca = epics.ca.initialize_libca()
+except ImportError:
+    pass
 
-extra_files = ['inno_setup.iss', 'COPYING', 'README.txt', 'larch.ico']
+
+extra_files = ['inno_setup.iss', '../COPYING', '../README.txt', 'GSEMap.ico']
 scipy_dlls = ['lib/site-packages/scipy/optimize/minpack2.pyd',
               'lib/site-packages/scipy/interpolate/dftipack.pyd',
               'lib/site-packages/scipy/integrate/_quadpack.pyd',
               'lib/site-packages/numpy/fft/fftpack_lite.pyd']
-
 
 dlldir = os.path.join(sys.prefix, 'DLLs')
 for n in os.listdir(dlldir):
@@ -49,34 +58,36 @@ for n in os.listdir(dlldir):
 for n in scipy_dlls:
     extra_files.append(os.path.join(sys.prefix, n))
 
-windows_apps = [{'script': 'larch_gui', 'icon_resources': [(0, 'larch.ico')]}]
-console_apps = [{'script': 'larch',     'icon_resources': [(0, 'larch.ico')]}]
+windows_apps = [{'script': '../bin/larch_gui',     'icon_resources': [(0, 'larch.ico')]},
+                {'script': '../bin/GSE_MapViewer', 'icon_resources': [(0, 'GSEMap.ico')]},
+                ]
+console_apps = [{'script': '../bin/larch',         'icon_resources': [(0, 'larch.ico')]}]
 
-# pu2exe.org for options
 py2exe_opts = {'optimize':1,
-                'bundle_files':2,
-               'includes': ['epics', 'ctypes', 'ctypes.util', 'wx', 'ConfigParser',
-                            'numpy', 'scipy', 'matplotlib', 'h5py',
-                            'Image', 'MySQLdb', 'sqlite3', 'sqlalchemy',
-                            'xdrlib', 'zlib', 'struct', 'datetime', 'xml',
-                            're', 'warnings', 'json', 'wxmplot',
-                            ],
-               'packages': ['MySQLdb', 'sqlite3', 'sqlalchemy.dialects.mysql',
-                            'sqlalchemy.dialects.sqlite',
-                            'sqlalchemy.orm', 'sqlalchemy.pool',
-                            'epics.ca', 'wx.lib', 'wx.lib.newevent',
-                            'h5py', 'scipy.optimize', 'scipy.signal',
+               'bundle_files':2,
+               'includes': ['Image', 'ctypes', 'numpy', 'scipy', 'scipy.optimize',
+                            'wx', 'wx._core', 'wx.py', 'wxversion',
+                            'wx.lib', 'wx.lib.*', 'wx.lib.masked', 'wx.lib.mixins',
+                            'wx.lib.colourselect', 'wx.lib.newevent',
+                            'wx.lib.agw', 'wx.lib.agw.flatnotebook',
+                            'h5py', 'h5py._objects', 'h5py.defs', 'h5py.utils', 'h5py._proxy',
+                            'matplotlib', 'wxmplot',
+                            'ConfigParser', 'fpformat',
+                            'sqlalchemy', 'sqlalchemy.orm', 'sqlalchemy.pool',
+                            'sqlite3', 'sqlalchemy.dialects.sqlite',
+                            'xdrlib',  'epics', 'epics.devices'],
+              
+               'packages': ['h5py', 'scipy.optimize', 'scipy.signal', 'scipy.io',
                             'numpy.random', 'xml.etree', 'xml.etree.cElementTree'], 
-
                'excludes': ['Tkinter', '_tkinter', 'Tkconstants', 'tcl',
                             '_imagingtk', 'PIL._imagingtk', 'ImageTk',
                             'PIL.ImageTk', 'FixTk''_gtkagg', '_tkagg',
-                            'qt', 'PyQt4Gui', 'Carbon', 'email'],
+                            'matplotlib.tests', 'qt', 'PyQt4Gui', 'IPython'],
+
                'dll_excludes': ['libgdk-win32-2.0-0.dll',
                                 'libgobject-2.0-0.dll', 'libzmq.dll']
                }
 
-# include matplotlib datafiles
 setup(name = "Larch",
       windows = windows_apps,
       console = console_apps,
