ab13476778ed7936e15206b784f963618f760a7c
Matt Newville <newville@cars.uchicago.edu>
2013-09-15 16:03:20 -0500

    ASCII datafile / extrapvs now more XDI-like

--- lib/datafile.py
+++ lib/datafile.py
@@ -23,13 +23,14 @@ import os
 import time
 import numpy as np
 
-from .file_utils import new_filename, get_timestamp
+from .file_utils import new_filename, get_timestamp, fix_filename
 from .ordereddict import OrderedDict
 
 COM1 = '#'
-COM2 = '##'
-SEP  = '||'   # separater between value, pvname in header
-FILETOP = '##Epics StepScan File'
+COM2 = '/'*3 + '  Users Comments  ' + '/'*3
+COM3 = '-'*len(COM2)
+SEP  = ' || '   # separater between value, pvname in header
+FILETOP = '#XDI/1.1    Epics StepScan File'
 
 class StepScanData(object):
     """
@@ -214,17 +215,17 @@ class ScanFile(object):
 class ASCIIScanFile(ScanFile):
     """basis ASCII Column File, line-ending delimited,
     using '#' for comment lines
+    and a format derived from XDI
     """
     num_format = "% 15f"
     version = '2.0'
-    def __init__(self, name=None, scan=None,
-                 comments=None,
+    def __init__(self, name=None, scan=None, comments=None,
                  auto_increment=True):
         ScanFile.__init__(self, name=name, scan=scan)
         if name is None:
             self.filename = 'test.dat'
         self.auto_increment = auto_increment
-        self.comments = comments
+        self.comments = commentsxo
 
     def write_lines(self, buff):
         "write array of text lines"
@@ -236,81 +237,77 @@ class ASCIIScanFile(ScanFile):
 
     def write_extrapvs(self):
         "write extra PVS"
-        out = ['%sExtraPVs Start:' % COM2]
+        out = ['%s ExtraPVs.Start: here' % COM1]
         for desc, pvname, val in self.scan.read_extra_pvs():
             if not isinstance(val, (str, unicode)):
                 val = repr(val)
+            # require a '.' in the description!!
+            if '.' not in desc:
+                isp = desc.find(' ')
+                if isp > 0:
+                    desc = "%s.%s" % (desc[:isp], desc[isp+1:])
+                else:
+                    desc = '%s.Value' % desc
+                desc = desc.fix_filename(desc)
             out.append("%s %s: %s %s %s" % (COM1, desc, val, SEP, pvname))
 
-        out.append('%sExtraPVs End' % COM2)
+        out.append('%s ExtraPVs.End: here' % COM1)
         self.write_lines(out)
 
-    def write_timestamp(self, label='Time'):
+    def write_timestamp(self, label='Now'):
         "write timestamp"
-        self.write_lines(["%s%s: %s" % (COM2, label, get_timestamp())])
+        self.write_lines(["%s Scan.%s: %s" % (COM1, label,
+                                              get_timestamp())])
 
     def write_comments(self):
         "write comment lines"
         if self.comments is None:
             return
         lines = self.comments.split('\n')
+        self.write_lines("%s %s" % (COM1, COM2))
         self.write_lines(['%s %s' % (COM1, l) for l in lines])
 
     def write_legend(self):
         "write legend"
         cols = []
-        out = ['%sLegend Start: Column: Name || EpicsPV || units' % COM2]
-        fmt = "%s %s: %s %s %s %s %s"
-        for i, pos in enumerate(self.scan.positioners):
-            key = 'p%i' % (i+1)
-            cols.append("   %s  " % (key))
-            if pos.units in (None, 'None', ''):
-                pos.units = ''
-                try:
-                    pos.units = pos.pv.units
-                except TypeError:
-                    time.sleep(0.02)
+        icol = 0
+        out = ['%sLegend.Start:   Column.N: Name, units, EpicsPV' % COM1]
+        for vars  in ((self.scan.positioners, 'positioner', 'unknown'),
+                      (self.scan.counters, 'counter', 'counts')):
+            objs, objtype, objunits = vars
+            for obj in objs:
+                icol += 1
+                key = '%s Column.%i' % (COM1, icol)
+                typ, units =  objtype, objunits
+                if obj.units in (None, 'None', ''):
                     try:
-                        pos.units = pos.pv.units
-                    except:
-                        pass
+                        units = obj.pv.units
+                    except TypeError:
+                        time.sleep(0.02)
+                        try:
+                            units = obj.pv.units
+                        except:
+                            pass
+                lab = fix_filename(obj.label)
+                pvn = obj.pv.pvname
+                out.append("%s: %s, %s %s %s" (key, lab, units, SEP, pvn))
+                cols.append(lab)
 
-            out.append(fmt % (COM1, key, pos.label,
-                              SEP, pos.pv.pvname,
-                              SEP, pos.units))
-        for i, det in enumerate(self.scan.counters):
-            key = 'd%i' % (i+1)
-            cols.append("   %s  " % (key))
-            if det.units in (None, 'None', ''):
-                det.units = ''
-                try:
-                    det.units = det.pv.units
-                except TypeError:
-                    time.sleep(0.02)
-                    try:
-                        det.units = det.pv.units
-                    except:
-                        pass
-
-            out.append(fmt % (COM1, key, det.label,
-                              SEP, det.pv.pvname,
-                              SEP, det.units))
-        out.append('%sLegend End' % COM2)
+        out.append('%s Legend.End: here' % COM1)
         self.write_lines(out)
         self.column_label = '%s %s' % (COM1, '\t'.join(cols))
 
     def write_data(self, breakpoint=0, clear=False, close_file=False, verbose=False):
         "write data"
         if breakpoint == 0:
-            self.write_timestamp(label='Time_Start')
-            self.write_comments()
+            self.write_timestamp(label='start_time')
             self.write_legend()
             return
-        self.write_timestamp(label='Time_End')
+        self.write_timestamp(label='end_time')
         self.write_extrapvs()
-        out = ['%s%s' % (COM1, '-'*66)]
-
-        out.append(self.column_label)
+        if breakpoint == 0:
+            self.write_comments()
+        out = ["%s %s" % (COM1, COM3), self.column_label]
 
         for i in range(len(self.scan.counters[0].buff)):
             words =  self.scan.pos_actual[i][:]
@@ -324,7 +321,6 @@ class ASCIIScanFile(ScanFile):
         self.write_lines(out)
         if clear:
             self.scan.clear_data()
-
 
         if close_file:
             self.close()
