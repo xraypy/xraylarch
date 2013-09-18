c81932b8fb333f15f52ea566b11a1169d55e3d9e
Matt Newville <newville@cars.uchicago.edu>
2013-09-18 06:54:53 -0500

    more improvements

--- lib/gui/viewerApp.py
+++ lib/gui/viewerApp.py
@@ -184,8 +184,8 @@ class PlotterFrame(wx.Frame):
         gname = self.groupname
         lgroup = getattr(self.larch.symtable, gname)
 
-        xfmt = "%s._x1_ = %s (%s)"
-        yfmt = "%s._y1_ = %s ((%s %s %s) %s (%s))"
+        xfmt = "%s._x1_ = %s(%s)"
+        yfmt = "%s._y1_ = %s((%s %s %s) %s (%s))"
         xop = self.x_op.GetStringSelection()
 
         xlabel = x
@@ -200,18 +200,20 @@ class PlotterFrame(wx.Frame):
         op2 = self.y_op2[0].GetStringSelection()
         op3 = self.y_op3[0].GetStringSelection()
 
-        y1 = self.y_arr1[0].GetStringSelection()
-        y2 = self.y_arr2[0].GetStringSelection()
-        y3 = self.y_arr3[0].GetStringSelection()
+        y1 = l1 = self.y_arr1[0].GetStringSelection()
+        y2 = l2 = self.y_arr2[0].GetStringSelection()
+        y3 = l3 = self.y_arr3[0].GetStringSelection()
         if y1 not in ('0', '1'):  y1 = "%s.%s" % (gname, y1)
         if y2 not in ('0', '1'):  y2 = "%s.%s" % (gname, y2)
         if y3 not in ('0', '1'):  y3 = "%s.%s" % (gname, y3)
         if x not in ('0', '1'):  x = "%s.%s" % (gname, x)
         self.larch(xfmt % (gname, xop, x))
         self.larch(yfmt % (gname, op1, y1, op2, y2, op3, y3))
-        # print dir(lgroup)
 
-        self.plotpanel.plot(lgroup._x1_, lgroup._y1_)
+        path, fname = os.path.split(lgroup.filename)
+        label = "%s: %s((%s%s%s)%s%s)" % (lgroup.filename, op1, l1, op2, l2, op3, l3)
+
+        self.plotpanel.plot(lgroup._x1_, lgroup._y1_, label=label)
 
         old = """
         if xop == 'log': x = "log(%s)" % x
@@ -273,18 +275,44 @@ class PlotterFrame(wx.Frame):
         self.y_arr1[0].SetSelection(1)
 
     def createMenus(self):
+        ppnl = self.plotpanel
         self.menubar = wx.MenuBar()
-        # file
+        #
         fmenu = wx.Menu()
+        pmenu = wx.Menu()
         add_menu(self, fmenu, "&Open Scan File\tCtrl+O",
                  "Read Scan File",  self.onReadScan)
 
         fmenu.AppendSeparator()
-        add_menu(self, fmenu, "&Quit\tCtrl+Q",
-                  "Quit program", self.onClose)
+        add_menu(self, fmenu, "&Copy\tCtrl+C",
+                 "Copy Figure to Clipboard", ppnl.canvas.Copy_to_Clipboard)
+        add_menu(self, fmenu, "&Save\tCtrl+S", "Save Figure", self.save_figure)
+        add_menu(self, fmenu, "&Print\tCtrl+P", "Print Figure", ppnl.Print)
+        add_menu(self, fmenu, "Page Setup", "Print Page Setup", ppnl.PrintSetup)
+        add_menu(self, fmenu, "Preview", "Print Preview", ppnl.PrintPreview)
+
+        fmenu.AppendSeparator()
+        add_menu(self, fmenu, "&Quit\tCtrl+Q", "Quit program", self.onClose)
+
+        add_menu(self, pmenu, "Configure\tCtrl+K",
+                 "Configure Plot", ppnl.configure)
+        add_menu(self, pmenu, "Unzoom\tCtrl+Z", "Unzoom Plot", ppnl.unzoom)
+        pmenu.AppendSeparator()
+        add_menu(self, pmenu, "Toggle Legend\tCtrl+L",
+                 "Toggle Legend on Plot", ppnl.toggle_legend)
+        add_menu(self, pmenu, "Toggle Grid\tCtrl+G",
+                 "Toggle Grid on Plot", ppnl.toggle_grid)
+
 
         self.menubar.Append(fmenu, "&File")
+        self.menubar.Append(pmenu, "Plot Options")
         self.SetMenuBar(self.menubar)
+
+    def save_figure(self,event=None, transparent=False, dpi=600):
+        """ save figure image to file"""
+        if self.plotpanel is not None:
+            self.plotpanel.save_figure(event=event,
+                                   transparent=transparent, dpi=dpi)
 
     def onAbout(self,evt):
         dlg = wx.MessageDialog(self, self._about,"About Epics StepScan",
@@ -321,12 +349,12 @@ class PlotterFrame(wx.Frame):
             if hasattr(self.datagroups, gname):
                 time.sleep(0.005)
                 gname = randname(n=6)
-            print 'Larch:: ', gname, path
             parent, fname = os.path.split(path)
             self.larch("%s = read_xdi('%s')" % (gname, path))
             self.larch("%s.path  = '%s'"     % (gname, path))
-            self.filelist.Append(path)
+            self.filelist.Append(fname)
             self.filemap[fname] = gname
+            print 'Larch:: ', gname, path, fname
             self.ShowFile(filename=fname)
 
         dlg.Destroy()
