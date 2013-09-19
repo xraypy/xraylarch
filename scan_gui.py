9e7aeb1e7a0e7eb88dfd2ee81dfd6524b1ae119e
Matt Newville <newville@cars.uchicago.edu>
2013-09-19 14:15:29 -0500

    scan_gui needs to give db connection args

--- scan_gui.py
+++ scan_gui.py
@@ -1,7 +1,10 @@
 import wx
 import epics
+from lib.gui import ScanApp
 
-from lib.gui import ScanFrame, ScanApp
-
-epics.ca.initialize_libca()
-ScanApp().MainLoop()
+#epics.ca.initialize_libca()
+scanner = ScanApp(dbname='epics_scan',
+                  server='postgresql',
+                  host='mini.cars.aps.anl.gov',
+                  user='epics', password='epics')
+scanner.MainLoop()
