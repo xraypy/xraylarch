7841a30a7b9d82ba7f095a95b43b9f3d2214241c
Matt Newville <newville@cars.uchicago.edu>
2013-11-14 12:00:49 -0600

    avoid numpy warning on int array * float array

--- plugins/xrf/gsemca_file.py
+++ plugins/xrf/gsemca_file.py
@@ -82,7 +82,7 @@ class GSEMCA_File(Group):
                 _en  = mca.get_energy()
                 mdat = UnivariateSpline(_en, mdat, s=0)(en)
             if dt_correct:
-                mdat *= mca.dt_factor
+                mdat = mdat * mca.dt_factor
             dat = dat + mdat
         return dat.astype(np.int)
 
