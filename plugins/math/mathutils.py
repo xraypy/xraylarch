cc66e0f2eaf683881154265d9d79dccef9e0e2ab
Matt Newville <newville@cars.uchicago.edu>
2014-01-26 11:45:54 -0600

    interp() now does extrapolation, interp1d() does not

--- plugins/math/mathutils.py
+++ plugins/math/mathutils.py
@@ -5,7 +5,7 @@ Some common math utilities
 import numpy as np
 import scipy
 import scipy.stats
-from scipy.interpolate import interp1d
+from scipy.interpolate import interp1d, UnivariateSpline
 
 # functions more or less directly from scipy or numpy
 def linregress(x, y, _larch=None):
@@ -15,6 +15,31 @@ linregress.__doc__ = scipy.stats.linregress.__doc__
 def polyfit(x, y, deg, rcond=None, full=False, _larch=None):
     return scipy.polyfit(x, y, deg, rcond=rcond, full=full)
 polyfit.__doc__ = scipy.polyfit.__doc__
+
+
+def _interp1d(x, y, xnew, kind='linear', fill_value=np.nan, _larch=None, **kws):
+    """interpolate x, y array onto new x values, using one of
+    linear, quadratic, or cubic interpolation
+
+        > ynew = interp1d(x, y, xnew, kind='linear')
+    arguments
+    ---------
+      x          original x values
+      y          original y values
+      xnew       new x values for values to be interpolated to  
+      kind       method to use: one of 'linear', 'quadratic', 'cubic'
+      fill_value value to use to fill values for out-of-range x values
+
+    note:  unlike interp, this version will not extrapolate for values of `xnew` 
+           that are outside the range of `x` -- it will use NaN or `fill_value`.
+           this is a bare-bones wrapping of scipy.interpolate.interp1d.
+
+    see also: interp
+    """
+    kwargs  = {'kind': kind, 'fill_value': fill_value,
+               'copy': False, 'bounds_error': False}
+    kwargs.update(kws)
+    return  interp1d(x, y, **kwargs)(xnew)
 
 
 def _interp(x, y, xnew, kind='linear', fill_value=np.nan, _larch=None, **kws):
@@ -30,13 +55,40 @@ def _interp(x, y, xnew, kind='linear', fill_value=np.nan, _larch=None, **kws):
       kind       method to use: one of 'linear', 'quadratic', 'cubic'
       fill_value value to use to fill values for out-of-range x values
 
-    see also: scipy.interpolate.interp1d
+    note:  unlike interp1d, this version will extrapolate for values of `xnew` 
+           that are outside the range of `x`, using the polynomial order `kind`.
+
+    see also: interp1d
     """
+    kind = kind.lower()
     kwargs  = {'kind': kind, 'fill_value': fill_value,
                'copy': False, 'bounds_error': False}
     kwargs.update(kws)
-    return  interp1d(x, y, **kwargs)(xnew)
+    out = interp1d(x, y, **kwargs)(xnew)
 
+    below = np.where(xnew<x[0])[0]
+    above = np.where(xnew>x[-1])[0]
+    if len(above) == 0 and len(below) == 0:
+        return out
+    for span, isbelow in ((below, True), (above, False)):
+        if len(span) < 1:
+            continue
+        ncoef = 5
+        if kind.startswith('lin'):
+            ncoef = 2
+        elif kind.startswith('quad'):
+            ncoef = 3
+        sel = slice(None, ncoef) if isbelow  else slice(-ncoef, None)
+        if kind.startswith('lin'):
+            coefs = scipy.polyfit(x[sel], y[sel], 1)
+            out[span] = coefs[1] + coefs[0]*xnew[span]
+        elif kind.startswith('quad'):
+            coefs = scipy.polyfit(x[sel], y[sel], 2)
+            out[span] = coefs[2] + xnew[span]*(coefs[1] + coefs[0]*xnew[span])
+        elif kind.startswith('cubic'):
+            out[span] = UnivariateSpline(x[sel], y[sel], s=0)(xnew[span])
+    return out
+    
 def _deriv(arr, _larch=None, **kws):
     if not isinstance(arr, np.ndarray):
         raise Warning("cannot take derivative of non-numeric array")
@@ -136,6 +188,7 @@ def registerLarchPlugin():
                       'complex_phase': complex_phase,
                       'deriv': _deriv,
                       'interp': _interp,
+                      'interp1d': _interp1d,
                       'remove_dups': remove_dups,
                       'index_of': index_of,
                       'index_nearest': index_nearest,
