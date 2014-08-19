a21ee78c9aa1b84c623ac83f0f20f4850f54c94a
Matt Newville <newville@cars.uchicago.edu>
2014-08-19 16:53:17 -0500

    add ValidateLarchPlugin function decorator

--- lib/__init__.py
+++ lib/__init__.py
@@ -13,7 +13,7 @@ major, minor = sys.version_info[0], sys.version_info[1]
 if major < 2 or (major == 2 and minor < 6):
     raise EnvironmentError('requires python 2.6 or higher')
 
-from .larchlib import plugin_path, use_plugin_path, isNamedClass
+from .larchlib import plugin_path, use_plugin_path, isNamedClass, LarchPluginException
 from .site_config import show_site_config
 from .symboltable import Group, SymbolTable, isgroup
 from .fitting import Minimizer, Parameter, isParameter, param_value
@@ -22,3 +22,21 @@ from .interpreter import Interpreter
 from .inputText import InputText
 from .utils import Closure, fixName
 from .version import __date__, __version__
+
+
+def ValidateLarchPlugin(fcn):
+    """function decorator to ensure that _larch is included in keywords,
+    and that it is a valid Interpeter"""
+    errmsg = "plugin function '%s' needs a valid '_larch' argument"
+    def wrapper(*args, **keywords):
+        "ValidateLarchPlugin"
+        if ('_larch' not in keywords or
+            not isinstance(keywords['_larch'], Interpreter)):
+            raise LarchPluginException(errmsg % fcn.__name__)
+        # raise Warning(errmsg % fcn.__name__)
+        return fcn(*args, **keywords)
+    wrapper.__doc__ = fcn.__doc__
+    wrapper.__name__ = fcn.__name__
+    wrapper.__dict__.update(fcn.__dict__)
+    return wrapper
+
