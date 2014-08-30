8646c4fe9e41f8dd4e869743e8da465aba09d1fe
Matt Newville <newville@cars.uchicago.edu>
2014-08-29 21:53:59 -0500

    add xray eaxmples to git

---
+++ tests/test_larchexamples_xray.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+""" Tests of Larch Scripts  """
+import unittest
+import time
+import ast
+import numpy as np
+from sys import version_info
+
+from utils import TestCase
+from larch import Interpreter
+class TestScripts(TestCase):
+    '''testing of examples/xray'''
+    def test1_mu_elam(self):
+        self.runscript('get_mu_tables.lar', dirname='../examples/xray')
+        assert(len(self.session.get_errors()) == 0)
+        self.isTrue("len(mu1) == 401")
+        self.isTrue("min(mu1) < 20")
+
+    def test2_mu_elam(self):
+        self.session.run("zn_iz = atomic_number('zn')")
+        self.session.run("zn_mass = atomic_mass('zn')")
+        assert(len(self.session.get_errors()) == 0)
+        self.isTrue("zn_iz == 30")
+        self.isTrue("zn_mass > 60.")
+
+
+if __name__ == '__main__':  # pragma: no cover
+    for suite in (TestScripts,):
+        suite = unittest.TestLoader().loadTestsFromTestCase(suite)
+        unittest.TextTestRunner(verbosity=3).run(suite)
+
+
