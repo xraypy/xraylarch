c12e8098397bbd257695b8bb7daa57712c7d2ad4
Matt Newville <newville@cars.uchicago.edu>
2014-09-02 09:50:30 -0500

    epics plugin does not fail to load if pyepics not installed

--- plugins/io/epics_plugin.py
+++ plugins/io/epics_plugin.py
@@ -2,31 +2,25 @@
 """
 Use Epics Channel Access
 """
-import sys
-HAS_PYEPICS = False
+plugins = {}
 try:
     import epics
-    HAS_PYEPICS = True
+    def caget(pvname, _larch=None, **kws):
+        return epics.caget(pvname, **kws)
+    def caput(pvname, value, _larch=None, **kws):
+        return epics.caput(pvname, value, **kws)
+    def PV(pvname, _larch=None, **kws):
+        return epics.PV(pvname, **kws)
+
+    caget.__doc__ = epics.caget.__doc__
+    caput.__doc__ = epics.caput.__doc__
+    PV.__doc__ = epics.PV.__doc__
+    
+    plugins = {'PV': PV, 'caget': caget, 'caput': caput}
+    
 except:
     pass
 
-def caget(pvname, _larch=None, **kws):
-        return epics.caget(pvname, **kws)
-
-caget.__doc__ = epics.caget.__doc__
-
-def caput(pvname, value, _larch=None, **kws):
-    return epics.caput(pvname, value, **kws)
-
-caput.__doc__ = epics.caput.__doc__
-
-def PV(pvname, _larch=None, **kws):
-    return epics.PV(pvname, **kws)
-
-PV.__doc__ = epics.PV.__doc__
-
 def registerLarchPlugin():
-    plugins = {}
-    if HAS_PYEPICS:
-        plugins = {'PV': PV, 'caget': caget, 'caput': caput}
     return ('_io', plugins)
+    
