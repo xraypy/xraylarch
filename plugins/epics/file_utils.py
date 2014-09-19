9e697e9cdc002aa9849c30070d0006399e9252b9
Matt Newville <newville@cars.uchicago.edu>
2014-09-19 15:32:12 -0500

    include scandb interface for epics stepscan code

--- plugins/epics/file_utils.py
+++ plugins/epics/file_utils.py
@@ -7,6 +7,37 @@ from random import seed, randrange
 BAD_FILECHARS = ';~,`!%$@?*#:"/|\'\\\t\r\n (){}[]<>'
 BAD_FILETABLE = maketrans(BAD_FILECHARS, '_'*len(BAD_FILECHARS))
 
+
+def strip_quotes(t):
+    d3, s3, d1, s1 = '"""', "'''", '"', "'"
+    if hasattr(t, 'startswith'):
+        if ((t.startswith(d3) and t.endswith(d3)) or
+            (t.startswith(s3) and t.endswith(s3))):
+            t = t[3:-3]
+        elif ((t.startswith(d1) and t.endswith(d1)) or
+              (t.startswith(s1) and t.endswith(s1))):
+            t = t[1:-1]
+    return t
+
+def get_units(pv, default):
+    try:
+        units = pv.units
+    except:
+        units = ''
+    if units in (None, ''):
+        units = default
+    return units
+
+def normalize_pvname(name):
+    """ make sure Epics PV name either ends with .VAL or .SOMETHING!"""
+    if  '.' in name:
+        return name
+    return "%s.VAL" % name
+
+def asciikeys(adict):
+    """ensure a dictionary has ASCII keys (and so can be an **kwargs)"""
+    return dict((k.encode('ascii'), v) for k, v in adict.items())
+
 def get_timestamp():
     """return ISO format of current timestamp:
     2012-04-27 17:31:12
