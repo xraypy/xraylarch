4bea9ade5078dedf78c95e17550c30ebd44e8b3f
Matt Newville <newville@cars.uchicago.edu>
2014-09-20 09:29:24 -0500

    moved epics plugin, leaving blank stub in plugins/io

--- plugins/io/epics_plugin.py
+++ plugins/io/epics_plugin.py
@@ -1,26 +1,3 @@
-#!/usr/bin/env python
-"""
-Use Epics Channel Access
-"""
-plugins = {}
-try:
-    import epics
-    def caget(pvname, _larch=None, **kws):
-        return epics.caget(pvname, **kws)
-    def caput(pvname, value, _larch=None, **kws):
-        return epics.caput(pvname, value, **kws)
-    def PV(pvname, _larch=None, **kws):
-        return epics.PV(pvname, **kws)
-
-    caget.__doc__ = epics.caget.__doc__
-    caput.__doc__ = epics.caput.__doc__
-    PV.__doc__ = epics.PV.__doc__
-    
-    plugins = {'PV': PV, 'caget': caget, 'caput': caput}
-    
-except:
-    pass
-
-def registerLarchPlugin():
-    return ('_io', plugins)
-    
+#
+# moved to plugins/epics/
+#
