0583e17767aaab1769f4d46edbcf75cca36a575a
Matt Newville <newville@cars.uchicago.edu>
2015-01-23 00:01:40 -0600

    work on better json encoding for rpc

--- lib/utils/jsonutils.py
+++ lib/utils/jsonutils.py
@@ -9,9 +9,15 @@ from larch import isParameter, Parameter, Group
 import numpy as np
 import json
 
+
 def json_encode(expr, _larch=None):
     """
-    return json encoded larch expression
+    return value of a larch expression ready for json encoding
+    this has special handling for
+      numpy array
+      complex numbers
+      Larch Groups
+      Larch Parameters
     """
     def _get(expr, _larch=None):
         if _larch is None:
@@ -21,7 +27,15 @@ def json_encode(expr, _larch=None):
             out = {'__class__': 'Array', '__shape__': obj.shape,
                    '__dtype__': obj.dtype.name}
             out['value'] = obj.tolist()
+            if 'complex' in obj.dtype.name:
+                out[value] = [(obj.real).tolist(), (obj.imag).tolist()]
             return out
+        elif isinstance(obj, (np.float, np.int)):
+            return float(obj)
+        elif isinstance(obj, (np.str, np.unicode)):
+            return str(obj)
+        elif isinstance(obj, np.complex):
+            return {'__class__': 'Complex', 'value': (obj.real, obj.imag)}
         elif _larch.symtable.isgroup(obj):
             out = {'__class__': 'Group'}
             for item in dir(obj):
@@ -35,9 +49,16 @@ def json_encode(expr, _larch=None):
                 if val is not None:
                     out[attr] = val
             return out
+        elif isinstance(obj, (tuple, list)):
+            ctype = 'List'
+            if isinstance(obj, tuple):
+                ctype = 'Tuple'
+            val = [_get(repr(item), _larch=_larch) for item in obj]
+            return {'__class__': ctype, 'value': val}
         else:
             return obj
-    return json.dumps(_get(expr, _larch=_larch))
+    return _get(expr, _larch=_larch)
+
 
 def json_decode(value, _larch=None):
     """
@@ -45,26 +66,36 @@ def json_decode(value, _larch=None):
     for Parameter decoding, a non-None larch instance
     must be passed in
     """
-    out = None
+    if isinstance(value, basestring):
+        try:
+            value = json.loads(value)
+        except ValueError: # happens if value is a string
+            pass
+    dtype = type(value)
+    if isinstance(value, dict):
+        dtype = 'dict %s' % value.get('__class__', 'plain')
+
+    print "DECODE : ", dtype
+
+    out = value
+
     if isinstance(value, dict):
         classname = value.get('__class__', None)
         if classname == 'Array':
-            out = np.fromiter(json.loads(value['value']),
+            out = np.fromiter(value['value'],
                               dtype=value['__dtype__'])
         elif classname == 'Group':
             out = Group()
             value.pop('__class__')
             for key, val in value.items():
-                out[key] = json_decode(val)
+                setattr(out, key,  json_decode(val))
         elif classname == 'Parameter':
             args = {'_larch': _larch}
             for attr in ('value', 'name', 'vary', 'min', 'max', 'expr'):
                 val = value.get(attr, None)
-                if attr == 'value': attr='val'
                 if val is not None:
                     args[attr] = val
-
             out = Parameter(**args)
-    if out is None:
-        out = json.loads(value)
+        elif classname == 'Tuple':
+            out = tuple(out['value'])
     return out
