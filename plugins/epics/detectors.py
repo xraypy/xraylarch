1655e72f74bedfb5733ee4559c6a46f0590a07e9
Matt Newville <newville@cars.uchicago.edu>
2015-03-13 11:57:07 -0500

    allow Xspress3 enable DTC

--- plugins/epics/detectors.py
+++ plugins/epics/detectors.py
@@ -537,7 +537,7 @@ class Xspress3Detector(DetectorMixin):
         self.dwelltime_pv = get_pv('%sAcquireTime' % prefix)
         self.trigger    = Xspress3Trigger(prefix)
         self.extra_pvs  = self.add_extrapvs_GSE()
-        self.use_dtc    = False #  use_dtc  # KLUDGE DTC!!
+        self.use_dtc    = use_dtc  # KLUDGE DTC!!
         self.label      = label
         if self.label is None:
             self.label = self.prefix
