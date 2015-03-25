10c07bc7b5ec82033518a8a882fcd612c48aa08f
Matt Newville <newville@cars.uchicago.edu>
2015-03-25 14:25:33 -0500

    add use_anaconda windows setup for dynamic libs

---
+++ dylibs/conf/use_anaconda.bat
@@ -0,0 +1,14 @@
+@echo off
+
+REM Setup path for using compilers supplied with anaconda python 
+REM This includes 64-bit Windows!
+
+
+REM Location of anaconda environment
+set ANAPY=C:\Users\xas_user\AppData\Local\Continuum\Anaconda\envs\py279
+
+REM Add gnuwin32 for unix tools
+
+set Path=C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;
+set Path=%PATH%;C:\GnuWin32\bin;%ANAPY%;%ANAPY%\Scripts;%ANAPY%\MinGW\bin
+
