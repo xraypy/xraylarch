51b73ba5b4dcf38a22afe5cf0fe7905a3211ea93
Matt Newville <newville@cars.uchicago.edu>
2015-04-04 23:09:36 -0500

    fix for EpicsXRF Display

--- plugins/epics/__init__.py
+++ plugins/epics/__init__.py
@@ -1,5 +1,6 @@
-# from .epics_plugin import PV, caget, caput
+from .epics_plugin import PV, caget, caput
 # from .xrf_detectors import Epics_MultiXMAP, Epics_Xspress3
 # from .detectors import get_detector, Counter
 # from .positioner import  Positioner
 # from .stepscan import LarchStepScan
+from .xrfcontrol import EpicsXRFApp
