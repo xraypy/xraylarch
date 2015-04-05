43fd7f0838a7ed241419a005e8f0aaf657d8ccc8
Matt Newville <newville@cars.uchicago.edu>
2015-04-04 21:55:39 -0500

    update binaries to use new larch_plugins form

--- plugins/wx/__init__.py
+++ plugins/wx/__init__.py
@@ -3,3 +3,6 @@ from .plotter import _newplot, _plot
 from .periodictable import PeriodicTablePanel
 from .xrfdisplay import XRFDisplayFrame, XRFApp, FILE_WILDCARDS
 from .xrfdisplay_utils import CalibrationFrame
+from .gse_dtcorrect import DTViewer
+from .scanviewer import ScanViewer
+from .xrfcontrol import EpicsXRFApp
