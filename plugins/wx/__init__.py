8c289763d3d0a0607639b977273f91cf2280f61f
Matt Newville <newville@cars.uchicago.edu>
2015-04-04 23:00:03 -0500

    reverting epics imports to use_plugin_path for now

--- plugins/wx/__init__.py
+++ plugins/wx/__init__.py
@@ -5,4 +5,3 @@ from .xrfdisplay import XRFDisplayFrame, XRFApp, FILE_WILDCARDS
 from .xrfdisplay_utils import CalibrationFrame
 from .gse_dtcorrect import DTViewer
 from .scanviewer import ScanViewer
-from .xrfcontrol import EpicsXRFApp
