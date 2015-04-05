b0535d957f8b5361a8d770b4943c86c175bb3023
Matt Newville <newville@cars.uchicago.edu>
2015-04-04 21:33:30 -0500

    delay enable_plugins() until validator is defined

--- lib/__init__.py
+++ lib/__init__.py
@@ -24,8 +24,6 @@ from .inputText import InputText
 from .utils import Closure, fixName
 from .version import __date__, __version__
 
-enable_plugins()
-
 def ValidateLarchPlugin(fcn):
     """function decorator to ensure that _larch is included in keywords,
     and that it is a valid Interpeter"""
@@ -44,3 +42,5 @@ def ValidateLarchPlugin(fcn):
     wrapper.__filename__ = fcn.__code__.co_filename
     wrapper.__dict__.update(fcn.__dict__)
     return wrapper
+
+enable_plugins()
