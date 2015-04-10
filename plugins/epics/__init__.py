f60ecf2ba05d104c87e409c2b9c7c44b2d0862bc
Matt Newville <newville@cars.uchicago.edu>
2015-04-10 16:35:13 -0500

    xrfcontrol now using larch_plugins

--- plugins/epics/__init__.py
+++ plugins/epics/__init__.py
@@ -1,5 +1,5 @@
 from .epics_plugin import PV, caget, caput
-# from .xrf_detectors import Epics_MultiXMAP, Epics_Xspress3
+from .xrf_detectors import Epics_MultiXMAP, Epics_Xspress3
 # from .detectors import get_detector, Counter
 # from .positioner import  Positioner
 # from .stepscan import LarchStepScan
