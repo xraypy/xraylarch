0a0209df94544f7ed64b1f87f570004dbb8998d7
Matt Newville <newville@cars.uchicago.edu>
2015-04-14 11:38:37 -0500

    Revert "XDI -- add 32-bit Windows dll"
    This reverts commit a6788c0895aa55d775c3bda4d5c9685cb0f18a07.

--- x.py
+++
@@ -1,8 +0,0 @@
-import os
-import larch
-from larch_plugins.epics import EpicsXRFApp
-os.chdir(larch.site_config.home_dir)
-conf = dict(prefix='dxpMercury:', det_type='ME-4',
-           is_xsp3=False, nmca=4)
-
-EpicsXRFApp(**conf).MainLoop()
