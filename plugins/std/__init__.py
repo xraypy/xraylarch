b36ed0c865f971f79fec60770112c3ab427b4be5
Matt Newville <newville@cars.uchicago.edu>
2015-09-16 14:09:00 -0500

    add OrderedDict and related data structures from collections module

--- plugins/std/__init__.py
+++ plugins/std/__init__.py
@@ -1,3 +1,5 @@
 #
 from .grouputils import parse_group_args
 from .show import show, get, group2dict, dict2group, show_tree
+
+from collections import OrderedDict, Set, defaultdict, deque, namedtuple
