b1bcfd4d115fcfba22d46c66047bf510403c8951
Matt Newville <newville@cars.uchicago.edu>
2015-09-24 17:37:20 -0500

    undo  a3e5912

--- plugins/epics/__init__.py
+++ plugins/epics/__init__.py
@@ -6,4 +6,4 @@ from .xrf_detectors import Epics_MultiXMAP, Epics_Xspress3
 # from .stepscan import LarchStepScan
 from .xrfcontrol import EpicsXRFApp
 from .scandb import ScanDB, InstrumentDB
-from .stepscan_utils import EpicsScanDB
+# from .stepscan_utils import EpicsScanDB
