cefad75c7db8f48c193a6f47e8bf0fe645c1c8e3
Matt Newville <newville@cars.uchicago.edu>
2015-12-08 11:48:57 -0600

    move program banner to be made in version.py

--- lib/version.py
+++ lib/version.py
@@ -1,3 +1,27 @@
 #!/usr/bin/env python
 __date__    = '11-Nov-2015'
 __version__ = '0.9.26'
+
+import sys
+import numpy
+import scipy
+import matplotlib
+
+try:
+    import wx
+    wxversion = wx.__version__
+except:
+    wxversion = 'not available'
+
+def make_banner(extra=None):
+    lines = ('=' * 72)
+    fmt = """Larch %s (%s) M. Newville and Larch Development Team
+Using Python %s,
+Numpy %s, scipy %s, matplotlib %s, and wxpython %s"""
+
+    banner = fmt % (__version__, __date__, sys.version,
+                    numpy.__version__, scipy.__version__,
+                    matplotlib.__version__, wxversion)
+    if extra is not None:
+        banner = "%s\n%s" % (banner, extra)
+    return "%s\n%s\n%s\n" % (lines, banner, lines)
