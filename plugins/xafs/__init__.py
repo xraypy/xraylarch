9cb34fd20d80cf5af1d24eb5f5938692d1f53577
Matt Newville <newville@cars.uchicago.edu>
2016-01-15 16:55:46 -0600

    add import of FeffRunner

--- plugins/xafs/__init__.py
+++ plugins/xafs/__init__.py
@@ -6,10 +6,12 @@ from .pre_edge import pre_edge, preedge, find_e0
 
 from .feffdat import FeffPathGroup, FeffDatFile, _ff2chi
 
-
 from .feffit import FeffitDataSet, TransformGroup, feffit
 
 from .autobk import autobk
 from .mback import mback
 from .diffkk import diffkk
 from .fluo import fluo_corr
+
+from .feffrunner import FeffRunner
+
