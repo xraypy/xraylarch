5c485a465bd5894b2fe6d609bb44dae5f4156075
Matthew Newville <newville@cars.uchicago.edu>
2016-02-15 18:21:27 -0600

    add atomic_density to exported functions

--- plugins/xray/__init__.py
+++ plugins/xray/__init__.py
@@ -7,10 +7,12 @@ from .physical_constants import (R_ELECTRON_CM, AVOGADRO, BARN,
 
 from .xraydb import xrayDB
 
-from .xraydb_plugin import (atomic_mass, atomic_number, atomic_symbol,
-                            xray_line, xray_lines, xray_edge, xray_edges,
-                            f0, f0_ions, mu_elam, mu_chantler, f1_chantler,
-                            f2_chantler, core_width, chantler_data)
+from .xraydb_plugin import (atomic_mass, atomic_number,
+                            atomic_symbol, atomic_density,
+                            xray_line, xray_lines, xray_edge,
+                            xray_edges, f0, f0_ions, mu_elam,
+                            mu_chantler, f1_chantler, f2_chantler,
+                            core_width, chantler_data)
 
 from .materials import material_mu, material_get
 from .cromer_liberman import f1f2
