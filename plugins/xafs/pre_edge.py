617444eadc6e07297b1c9bbdfa9d0929a5006805
Carmelo Prestipino <carmelo.prestipino@univ-rennes1.fr>
2016-02-22 10:01:16 +0100

    I create a subgroup for each of the "extraction" function :   pre_edge_details , autobk_details, xftf, xftr in which hide of the details in each subgroup 'function'_details I add an attribute 'params' that contains the arguments of the functionin a  dictionary two questions:
    1)which attribute should be keep in parent group   and which move in details??
    pre_edge:
    main_group attr
    ['dmude',
     'e0',
     'edge_step',
     'flat',
     'norm',
     'post_edge',
     'pre_edge',
     'pre_edge_details']
    
    autobk
    main group attr.
    ['autobk_details',
     'bkg',
     'chi',
     'chie',
     'k',
     'pre_edge_details']

--- plugins/xafs/pre_edge.py
+++ plugins/xafs/pre_edge.py
@@ -229,16 +229,36 @@ def pre_edge(energy, mu=None, group=None, e0=None, step=None,
        If it exists, group.e0 will be used as e0.
        See First Argrument Group in Documentation
     """
+    
+
+    
     energy, mu, group = parse_group_args(energy, members=('energy', 'mu'),
                                          defaults=(mu,), group=group,
                                          fcn_name='pre_edge')
     pre_dat = preedge(energy, mu, e0=e0, step=step, nnorm=nnorm,
                       nvict=nvict, pre1=pre1, pre2=pre2, norm1=norm1,
                       norm2=norm2)
+    
+    
+    group = set_xafsGroup(group, _larch=_larch)
+    group.pre_edge_details = Group()
+    group.pre_edge_details.params=dict()    
+    group.pre_edge_details.params['e0']=e0
+    group.pre_edge_details.params['step'] = step
+    group.pre_edge_details.params['nvict'] = nvict
+    group.pre_edge_details.params['nnorm'] = nnorm
+    group.pre_edge_details.params['norm1'] = norm1
+    group.pre_edge_details.params['norm2'] = norm2
+    group.pre_edge_details.params['pre1']  = pre1
+    group.pre_edge_details.params['pre2']  = pre2
+    group.pre_edge_details.params['make_flat'] = make_flat    
+    
+    
+
     e0    = pre_dat['e0']
     norm  = pre_dat['norm']
     norm1 = pre_dat['norm1']
-    norm2 = pre_dat['norm2']
+    norm2 = pre_dat['norm2'] 
     # generate flattened spectra, by fitting a quadratic to .norm
     # and removing that.
     flat = norm
@@ -265,27 +285,26 @@ def pre_edge(energy, mu=None, group=None, e0=None, step=None,
         flat        = norm - flat_diff  + flat_diff[ie0]
         flat[:ie0]  = norm[:ie0]
 
-    group = set_xafsGroup(group, _larch=_larch)
+
     group.e0 = e0
     group.norm = norm
     group.flat = flat
     group.dmude = np.gradient(mu)/np.gradient(energy)
-    group.nvict = nvict
-    group.nnorm = nnorm
-    group.norm1 = norm1
-    group.norm2 = norm2
-    group.pre1 = pre_dat['pre1']
-    group.pre2 = pre_dat['pre2']
     group.edge_step  = pre_dat['edge_step']
     group.pre_edge   = pre_dat['pre_edge']
     group.post_edge  = pre_dat['post_edge']
-    group.pre_slope  = pre_dat['precoefs'][0]
-    group.pre_offset = pre_dat['precoefs'][1]
+    group.pre_edge_details.pre1   = pre_dat['pre1']
+    group.pre_edge_details.pre2   = pre_dat['pre2']    
+    group.pre_edge_details.norm1  = pre_dat['norm1']
+    group.pre_edge_details.norm2  = pre_dat['norm2']
+    group.pre_edge_details.pre_slope  = pre_dat['precoefs'][0]
+    group.pre_edge_details.pre_offset = pre_dat['precoefs'][1]
+    
     for i in range(MAX_NNORM):
         if hasattr(group, 'norm_c%i' % i):
             delattr(group, 'norm_c%i' % i)
     for i, c in enumerate(pre_dat['norm_coefs']):
-        setattr(group, 'norm_c%i' % i, c)
+        setattr(group.pre_edge_details, 'norm_c%i' % i, c)
     return
 
 def registerLarchPlugin():
