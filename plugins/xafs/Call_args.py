079a289eccdadb797dbde58e7cb3579bf6803f30
Carmelo Prestipino <carmelo.prestipino@univ-rennes1.fr>
2016-02-24 12:01:48 +0100

    minor bugs
    try to remove error.

--- plugins/xafs/Call_args.py
+++ plugins/xafs/Call_args.py
@@ -10,21 +10,21 @@ def DefCallArgs(attr_name,rem_atr_list):
         def wrapper(*args, **kwargs):
             fnc_return=fcn(*args, **kwargs)
             call_args=getcallargs(fcn, *args, **kwargs)
+            #print call_args.keys()
 
-            a1,a2,group= parse_group_args(call_args[rem_atr_list[0]], 
+            a1,a2,groupx= parse_group_args(call_args[rem_atr_list[0]], 
                                      members=(rem_atr_list[0], rem_atr_list[1]),
                                      defaults=(call_args[rem_atr_list[1]],),
                                      group=call_args['group'],
                                      fcn_name='wrapper') 
             
-            rem_atr_list.extend(['group', '_larch'])
-            for item in rem_atr_list:
-                del call_args[item]
-                
-            if not hasattr(group, attr_name):
-                setattr(group, attr_name, larch.Group())
-            subject=getattr(group, attr_name)    
-            setattr(subject, 'call_args', call_args)
+            rem_atr_list.extend(['group','_larch'])
+            filtered_dict = {k:v for (k,v) in call_args.iteritems() if not(
+                                                             k in rem_atr_list)}    
+            if not hasattr(groupx, attr_name):
+                setattr(groupx, attr_name, larch.Group())
+            subject=getattr(groupx, attr_name)    
+            setattr(subject, 'call_args', filtered_dict)
             return fnc_return
         wrapper.__doc__ = fcn.__doc__
         wrapper.__name__ = fcn.__name__
