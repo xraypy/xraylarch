bf2d4095cff92fa8658d0d1ae7ac6d4644c31755
Matt Newville <newville@cars.uchicago.edu>
2016-04-01 15:21:11 -0500

    fix call to jsonutils

--- examples/rpc/example_client.py
+++ examples/rpc/example_client.py
@@ -3,7 +3,7 @@
 import xmlrpclib
 import time
 import json
-from larch.utils.jsonutils import json_decode
+from larch.utils.jsonutils import decode4js
 s = xmlrpclib.ServerProxy('http://127.0.0.1:4966')
 
 print 'Avaialable Methods from XML-RPC server: ', s.system.listMethods()
@@ -24,13 +24,11 @@ print s.get_messages()
 print '=='
 
 
-# gx  = json_decode(s.get_data('g.z'))
-# print 'm = ', s.get_data('m')
-# print 'x = ', s.get_data('x')
-# 
-# print 'gx = ',  gx, type(gx), gx.dtype
+gx  = decode4js(s.get_data('g.z'))
+print 'm = ', s.get_data('m')
+print 'x = ', s.get_data('x')
+
+print 'gx = ',  gx, type(gx), gx.dtype
 
 # could tell server to exit!
 # s.exit()
-
-
