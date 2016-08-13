6942fc742d694ac7bfe8bed1a745b228f5feefa8
Matthew Newville <newville@cars.uchicago.edu>
2016-08-13 10:06:17 -0500

    remove wx cruft

--- lib/xmlrpc_server.py
+++ lib/xmlrpc_server.py
@@ -11,12 +11,6 @@ from larch.interpreter import Interpreter
 from larch.inputText import InputText
 from larch.utils.jsonutils import encode4js
 
-if not hasattr(sys, 'frozen'):
-    try:
-        import wxversion
-        wxversion.ensureMinimal('2.8')
-    except:
-        pass
 try:
     import wx
     from larch.wxlib import inputhook
