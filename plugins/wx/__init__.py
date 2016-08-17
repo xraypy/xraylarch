a5e128b9ddb344e47780535134c40a9fb30ca898
Matt Newville <newville@cars.uchicago.edu>
2016-08-17 17:29:05 -0500

    add icons file to wx dir, export more plot functions

--- plugins/wx/__init__.py
+++ plugins/wx/__init__.py
@@ -1,9 +1,10 @@
 from . import gui_utils
 from .parameter import ParameterPanel
-from .plotter import _newplot, _plot
+from .plotter import _newplot, _plot, _imshow, _getDisplay
 from .periodictable import PeriodicTablePanel
 from .xrfdisplay import XRFDisplayFrame, XRFApp, FILE_WILDCARDS
 from .xrfdisplay_utils import CalibrationFrame
 from .gse_dtcorrect import DTViewer
 from .scanviewer import ScanViewer
 from .mapviewer import MapViewer
+from .icons import get_icon
