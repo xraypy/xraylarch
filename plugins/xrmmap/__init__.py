b69dfe0177ed9e54579b9ea5c8b17d7a580077fd
M. Koker <koker@cars.uchicago.edu>
2016-08-29 16:09:47 -0500

    update __init__.py with xrf/xrd/xrm changes mkak 2016.08.29

--- plugins/xrmmap/__init__.py
+++ plugins/xrmmap/__init__.py
@@ -1,8 +1,12 @@
 from .configfile import FastMapConfig
 from .xsp3_hdf5 import read_xsp3_hdf5
-from .xmap_netcdf import read_xmap_netcdf
+from .xrf_netcdf import read_xrf_netcdf
+from .xrd_netcdf import (read_xrd_netcdf, roi_xrd_netcdf,
+                         make_xrd_map)
 from .asciifiles import (readASCII, readMasterFile, readROIFile,
-                        readEnvironFile, parseEnviron)
+                         readEnvironFile, parseEnviron)
 from .xrm_mapfile import (read_xrfmap,
                           GSEXRM_MapFile, GSEXRM_FileStatus,
-                          GSEXRM_Exception, GSEXRM_NotOwner)
+                          GSEXRM_Exception, GSEXRM_NotOwner,
+                          read_xrd_netcdf, make_xrd_map)#,
+                          #read_xrd_hdf5)
