2ab5a55c9cbd7c0ee8ef0e61d00502b3439a8a6b
Matthew Newville <newville@cars.uchicago.edu>
2016-09-02 08:07:47 -0500

    updates/simplifications of xmlrpc server

--- lib/xmlrpc_server.py
+++ lib/xmlrpc_server.py
@@ -1,62 +1,41 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
-from six.moves.xmlrpc_server import SimpleXMLRPCServer
 import os
 import sys
-import time
+from time import time
+from select import error as select_error
+from socket import error as socket_error
 from threading import Timer
-import larch
-from larch.interpreter import Interpreter
-from larch.inputText import InputText
+from six.moves.xmlrpc_server import SimpleXMLRPCServer
+from larch import Interpreter, InputText
 from larch.utils.jsonutils import encode4js
 
-try:
-    import wx
-    from larch.wxlib import inputhook
-    HAS_WX = True
-except ImportError:
-    HAS_WX = False
-
 class LarchServer(SimpleXMLRPCServer):
-    IDLE_TIME = 86400.0
-    IDLE_POLL_TIME = 60.0
-    def __init__(self, host='127.0.0.1', port=5465, with_wx=True,
-                 local_echo=True, quiet=False, **kws):
-        self.keep_alive = True
-        self.port = port
-        self.with_wx = HAS_WX and with_wx
-        self.local_echo = local_echo
-        self.quiet = quiet
+    KEEPALIVE_TIME = 3*24*3600.0
+    POLL_TIME = 1.0
+
+    def __init__(self, host='127.0.0.1', port=4966, **kws):
         self.out_buffer = []
-        self.keep_alive_time = time.time() + self.IDLE_TIME
+        self.keep_alive = True
+        self.expiration_time = time() + self.KEEPALIVE_TIME
+        self.timer = None
         SimpleXMLRPCServer.__init__(self, (host, port),
-                                    logRequests=False, allow_none=True, **kws)
-        self.initialized = False
-        self.register_introspection_functions()
-        self.register_function(self.list_dir,      'ls')
-        self.register_function(self.change_dir,    'chdir')
-        self.register_function(self.current_dir,   'cwd')
-        self.register_function(self.exit,          'exit')
-        self.register_function(self.larch_exec,    'larch')
-        self.register_function(self.get_data,      'get_data')
-        self.register_function(self.get_messages,  'get_messages')
-        self.register_function(self.len_messages,  'len_messages')
-        if self.with_wx:
-            self.register_function(self.wx_update,     'wx_update')
+                                    logRequests=False,
+                                    allow_none=True, **kws)
+        for method in  ('ls', 'chdir', 'cd', 'cwd', 'exit', 'larch',
+                        'get_data', 'get_messages', 'len_messages'):
+            self.register_function(getattr(self, method), method)
+
+        self.larch = Interpreter(writer=self)
+        self.input = InputText(prompt='', _larch=self.larch)
+        self.larch.symtable.set_symbol('_sys.color_exceptions', False)
+        self.larch.run_init_scripts()
 
     def write(self, text, **kws):
         self.out_buffer.append(text)
-        if self.local_echo:
-            sys.stdout.write(text)
-
-    def flush(self):
-        if self.local_echo:
-            sys.stdout.flush()
 
     def get_messages(self):
-        if self.local_echo:
-            print( '== clear output buffer (%i)' % len(self.out_buffer))
         out = "".join(self.out_buffer)
         self.out_buffer = []
         return out
@@ -64,111 +43,67 @@ class LarchServer(SimpleXMLRPCServer):
     def len_messages(self):
         return len(self.out_buffer)
 
-    def check_activity(self):
-        if (time.time() > self.keep_alive_time) or not self.keep_alive:
-            sys.exit()
-        else:
-            self.timer = Timer(self.IDLE_POLL_TIME, self.check_activity)
-            self.timer.start()
-
-    def list_dir(self, dir_name):
+    def ls(self, dir_name):
         """list contents of a directory: """
         return os.listdir(dir_name)
 
-    def change_dir(self, dir_name):
+    def chdir(self, dir_name):
         """change directory"""
         return os.chdir(dir_name)
 
-    def current_dir(self):
+    def cd(self, dir_name):
+        """change directory"""
+        return os.chdir(dir_name)
+
+    def cwd(self):
         """change directory"""
         ret = os.getcwd()
         if sys.platform == 'win32':
             ret = ret.replace('\\','/')
         return ret
 
-    def help(self):
-        if not self.quiet:
-            print( 'Serving Larch at port %s' % repr(self.port))
-            # print dir(self)
-            print('Registered Functions:')
-            fnames = ['ls', 'chdir', 'cwd', 'exit', 'larch', 'get_data']
-            if self.with_wx:
-                fnames.append('wx_update')
-            for kname in self.funcs.keys():
-                if not kname.startswith('system') and kname not in fnames:
-                    fnames.append(kname)
-
-            out = ''
-            for fname in sorted(fnames):
-                if len(out) == 0:
-                    out = fname
-                else:
-                    out = "%s, %s" % (out, fname)
-                if len(out) > 70:
-                    print("  %s" % out)
-                    out = ''
-            if len(out) >  0:
-                print("  %s" % out)
-
-
     def exit(self, **kws):
         "shutdown LarchServer"
-        self.keep_alive = False
-        return self.server_close()
+        try:
+            self.timer.stop()
+        except AttributeError:
+            pass
+        self.server_close()
+        sys.exit()
 
-    def initialize_larch(self):
-        self.larch  = Interpreter(writer=self)
-        self.input  = InputText(prompt='', _larch=self.larch)
-        self.larch.symtable.set_symbol('_sys.color_exceptions', False)
-        self.larch.run_init_scripts()
-        self.wxapp = None
-        self.wx_evtloop = None
-        if self.with_wx:
-            self.larch.symtable.set_symbol('_sys.wx.inputhook', inputhook)
-            self.larch.symtable.set_symbol('_sys.wx.force_wxupdate', False)
+    def check_activity(self):
+        if not self.keep_alive or time() > self.expiration_time:
+            self.exit()
+        else:
+            self.timer = Timer(self.POLL_TIME, self.check_activity)
+            self.timer.start()
 
-            self.wxapp = wx.App(redirect=False, clearSigInt=False)
-            self.wx_evtloop = inputhook.EventLoopRunner(parent=self.wxapp)
-
-            self.larch.symtable.set_symbol('_sys.wx.wxapp', self.wxapp)
-            # app.GetTopWindow())
-            self.larch.symtable.set_symbol('_sys.wx.parent',None)
-            inputhook.ON_INTERRUPT = self.exit
-            inputhook.WXLARCH_SYM = self.larch.symtable
-        self.initialized = True
-
-    def wx_update(self, timeout):
-        "allow wx widgets to update until timeout expires"
-        if self.wx_evtloop is not None:
-            t0 = time.time()
-            while time.time() - t0 < timeout:
-                self.larch.symtable.set_symbol('_sys.wx.force_wxupdate', True)
-                self.wx_evtloop.run(poll_time=5)
-        return True
+    def larch(self, text, **kws):
+        "execute larch command"
+        text = text.strip()
+        self.expiration_time = time() + self.KEEPALIVE_TIME
+        if not self.keep_alive or text in ('quit', 'exit', 'EOF'):
+            self.exit()
+        else:
+            self.input.put(text, lineno=0)
+            if self.input.complete:
+                self.input.run()
+            self.flush()
+        return 1
 
     def get_data(self, expr):
         "return json encoded data for a larch expression"
-        self.keep_alive_time = time.time() + self.IDLE_TIME
+        self.expiration_time = time() + self.KEEPALIVE_TIME
         return encode4js(self.larch.eval(expr))
 
-    def larch_exec(self, text, debug=True):
-        "execute larch command"
-        self.debug = debug
-        if not self.initialized:
-            self.initialize_larch()
-        text = text.strip()
-        complete = True
-        if text in ('quit', 'exit', 'EOF'):
-            self.exit()
-        else:
-            ret = None
-            self.keep_alive_time = time.time() + self.IDLE_TIME
-            self.input.put(text, lineno=0)
-            complete = self.input.complete
-            if complete:
-                complete = self.input.run()
-            self.flush()
-        return 1
+    def run(self):
+        """run server until keep_alive is set to False"""
+        self.check_activity()
+        try:
+            self.serve_forever()
+        except (KeyboardInterrupt, select_error, socket_error):
+            self.keep_alive = False
+            sys.exit()
 
 if __name__ == '__main__':
     s = LarchServer(host='localhost', port=4966)
