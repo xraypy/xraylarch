37931bfa4a3c46824ba6e1d5ef4a2367068dba55
M. Koker <koker@cars.uchicago.edu>
2016-11-10 08:35:46 -0600

    Working on 1DXRD viewer mkak 2016.11.10

--- plugins/diFFit/__init__.py
+++ plugins/diFFit/__init__.py
@@ -4,3 +4,4 @@ from .ImageControlsFrame import ImageToolboxFrame
 from .XRDCalculations import fabioOPEN,integrate_xrd
 from .XRDCalibrationFrame import CalibrationPopup
 from .XRDMaskFrame import MaskToolsPopup
+#from .cifdb import 
