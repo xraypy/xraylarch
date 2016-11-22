b24f35381e659ec1caea9e2defab3b2d5719ded0
M. Koker <koker@cars.uchicago.edu>
2016-11-22 12:16:01 -0600

    Removing old xrd viewers from build. Updatind cifdb to build and add to database of cif information. Requires personal build of xrayutilities (to be replaced). mkak 2016.11.22

--- plugins/wx/__init__.py
+++ plugins/wx/__init__.py
@@ -4,7 +4,6 @@ from .plotter import _newplot, _plot
 from .periodictable import PeriodicTablePanel
 from .xrfdisplay import XRFDisplayFrame, XRFApp, FILE_WILDCARDS
 from .xrfdisplay_utils import XRFCalibrationFrame
-from .xrddisplay import XRD1D_DisplayFrame, XRDApp, XRD2D_DisplayFrame
 from .gse_dtcorrect import DTViewer
 from .scanviewer import ScanViewer
 from .mapviewer import MapViewer
