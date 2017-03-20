2af4cfa5d9e5a418c1f8bae14dab38368733ec05
M. Koker <koker@cars.uchicago.edu>
2017-03-20 10:48:17 -0500

    Not using cif database panel right now mkak 2017.03.20

--- plugins/diFFit/XRD1Dviewer.py
+++ plugins/diFFit/XRD1Dviewer.py
@@ -500,11 +500,11 @@ class SelectFittingData(wx.Dialog):
 
 
 
-class CIFDatabaseList(wx.ListCtrl, listmix.ListCtrlAutoWidthMixin):
-    def __init__(self, parent, ID, pos=wx.DefaultPosition,
-                 size=wx.DefaultSize, style=0):
-        wx.ListCtrl.__init__(self, parent, ID, pos, size, style)
-        listmix.ListCtrlAutoWidthMixin.__init__(self)
+# class CIFDatabaseList(wx.ListCtrl, listmix.ListCtrlAutoWidthMixin):
+#     def __init__(self, parent, ID, pos=wx.DefaultPosition,
+#                  size=wx.DefaultSize, style=0):
+#         wx.ListCtrl.__init__(self, parent, ID, pos, size, style)
+#         listmix.ListCtrlAutoWidthMixin.__init__(self)
 
 # class DatabaseXRD(wx.Panel, listmix.ColumnSorterMixin):
 #     """
