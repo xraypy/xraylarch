49a3e7a9c7a90c307b0144fc98666c0c68bf5e94
Matthew Newville <newville@cars.uchicago.edu>
2017-04-03 22:27:47 -0500

    modify banner, add co-authors

--- lib/version.py
+++ lib/version.py
@@ -13,15 +13,14 @@ try:
 except:
     wxversion = 'not available'
 
-def make_banner(extra=None):
-    lines = ('=' * 72)
-    fmt = """Larch %s (%s) M. Newville and Larch Development Team
-Using Python %s,
-Numpy %s, scipy %s, matplotlib %s, and wxpython %s"""
+def make_banner():
+    lines = '=' * 78
+    banner = """%s
+Larch %s (%s) M. Newville, M. Koker, B. Ravel, and others
+Python %s,
+numpy %s, scipy %s, matplotlib %s, wxpython %s
+%s"""
 
-    banner = fmt % (__version__, __date__, sys.version,
+    return banner % (lines, __version__, __date__, sys.version,
                     numpy.__version__, scipy.__version__,
-                    matplotlib.__version__, wxversion)
-    if extra is not None:
-        banner = "%s\n%s" % (banner, extra)
-    return "%s\n%s\n%s\n" % (lines, banner, lines)
+                    matplotlib.__version__, wxversion, lines)
