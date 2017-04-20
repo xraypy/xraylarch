b8a11c98c827bf1b4ba2e84126c8605ec7b14b6f
Matthew Newville <newville@cars.uchicago.edu>
2017-04-19 23:05:14 -0500

    more refactoring for using lmfit

--- lib/fitting/__init__.py
+++ lib/fitting/__init__.py
@@ -1,10 +1,31 @@
 #!/usr/bin/env python
 import six
+import wx
+import matplotlib
+matplotlib.use("WXAgg")
 
-from .parameter import Parameter, isParameter, param_value
-from .minimizer import Minimizer, minimize, fit_report, eval_stderr
-from .confidence import conf_intervals , chisquare_map, conf_report, f_compare
-from .uncertainties import ufloat, correlated_values
+from lmfit import (Parameter, Minimizer, minimize, fit_report,
+                   conf_interval, ci_report, conf_interval2d,
+                   ufloat, correlated_values)
+
+from lmfit.minimizer import eval_stderr
+from lmfit.confidence import f_compare
+
+# from .parameter import Parameter, isParameter, param_value
+# from .minimizer import Minimizer, minimize, fit_report, eval_stderr
+# from .confidence import conf_intervals , chisquare_map, conf_report, f_compare
+# from .uncertainties import ufloat, correlated_values
+
+def isParameter(x):
+    return (isinstance(x, Parameter) or
+            x.__class__.__name__ == 'Parameter')
+
+def param_value(val):
+    "get param value -- useful for 3rd party code"
+    while isinstance(val, Parameter):
+        val = val.value
+    return val
+
 
 def f_test(ndata, nvars, chisquare, chisquare0, nfix=1):
     """return the F-test value for the following input values:
@@ -18,12 +39,12 @@ def confidence_report(conf_vals, **kws):
     """return a formatted report of confidence intervals calcualted
     by confidence_intervals"""
 
-    return conf_report(conf_vals)
+    return ci_report(conf_vals)
 
 def confidence_intervals(minout, sigmas=(1, 2, 3),  **kws):
     """explicitly calculate the confidence intervals from a fit
     for supplied sigma values"""
-    return conf_intervals(minout, sigmas=sigmas, **kws)
+    return conf_interval(minout, sigmas=sigmas, **kws)
 
 def chi2_map(minout, xname, yname, nx=11, ny=11, xrange=None,
              yrange=None, sigmas=None, **kws):
@@ -43,18 +64,22 @@ def chi2_map(minout, xname, yname, nx=11, ny=11, xrange=None,
     =======
         xpts, ypts, map
     """
-    return chisquare_map(minout, xname, yname, nx=nx, ny=ny,
-                         sigmas=sigmas, **kws)
+    return conf_interval2d(minout, xname, yname, nx=nx, ny=ny,
+                           sigmas=sigmas, **kws)
 
 def param(*args, **kws):
     "create a fitting Parameter as a Variable"
-    if len(args) > 0 and isinstance(args[0], six.string_types):
-        expr = args[0]
+    if len(args) > 0:
+        a0 = args[0]
+        if isinstance(a0, six.string_types):
+            kws.update({'expr': a0})
+        elif isinstance(a0, (int, float)):
+            kws.update({'value': a0})
+        else:
+            raise ValueError("first argument to param() must be string or number")
         args = args[1:]
-        kws.update({'expr': expr})
-    if 'val' in kws:
-        val = kws.pop('val')
-        kws.update({'value': val})
+    if '_larch' in kws:
+        kws.pop('_larch')
     return Parameter(*args, **kws)
 
 def guess(value,  **kws):
