9ff6ac01b72a6e77f77ee3ca51e070a604248a00
M. Koker <koker@cars.uchicago.edu>
2017-04-25 09:36:49 -0500

    .

--- plugins/cifdb/__init__.py
+++ plugins/cifdb/__init__.py
@@ -1,2 +1,1 @@
 from .cifdb import cifDB,QSTEP,QMIN,CATEGORIES,SearchCIFdb,match_database
-from .cif import CIFcls,create_cif
--- plugins/xrd/__init__.py
+++ plugins/xrd/__init__.py
@@ -7,4 +7,4 @@ from .xrd_xutil import calcCIFpeaks
 from .xrd_tools import (d_from_q,d_from_twth,twth_from_d,twth_from_q,
                         E_from_lambda,lambda_from_E,q_from_d,q_from_twth,qv_from_hkl,
                         d_from_hkl,unit_cell_volume,generate_hkl)
-
+from .xrd_cif import CIFcls,create_cif
