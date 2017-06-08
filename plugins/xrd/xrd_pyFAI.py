0b2f9bb57fee124899bc63afe1f8978c0fc14004
M. Koker <koker@cars.uchicago.edu>
2017-06-08 09:38:14 -0500

    Cleaning up pyFAI function in plugins for adding in wedges mkak 2017.06.08

--- plugins/xrd/xrd_pyFAI.py
+++ plugins/xrd/xrd_pyFAI.py
@@ -105,9 +105,6 @@ def integrate_xrd(xrd2d, calfile, unit='q', steps=10000, file='', mask=None, dar
         print('pyFAI not imported. Cannot calculate 1D integration.')
 
 def calc_cake(xrd2d, calfile, unit='q', mask=None, dark=None, verbose=False):
-    '''
-
-    '''
     
     if HAS_pyFAI:
         try:
@@ -131,103 +128,11 @@ def calc_cake(xrd2d, calfile, unit='q', mask=None, dark=None, verbose=False):
     else:
         print('pyFAI not imported. Cannot calculate 1D integration.')
 
-
 def calcXRD1d(xrd2d,ai,steps,attrs):
     return ai.integrate1d(xrd2d,steps,**attrs)
 
 def calcXRDcake(xrd2d,ai,attrs):
     return ai.integrate2d(xrd2d,2048,2048,**attrs) ## returns I,q,eta
-    
-
-
-    
-    
-
-
-
-# def calculate_ai(AI):
-#     '''
-#     Builds ai structure using AzimuthalIntegrator from hdf5 parameters
-#     mkak 2016.08.30
-#     '''
-# 
-#     if HAS_pyFAI:
-#         try:
-#             distance = float(AI.attrs['distance'])
-#         except:
-#             distance = 1
-#      
-#         ## Optional way to shorten this script... will need to change units of pixels
-#         ## mkak 2016.08.30   
-#         #floatattr = ['poni1','poni2','rot1','rot2','rot3','pixel1','pixel2']
-#         #valueattr = np.empty(7)
-#         #for f,fattr in enumerate(floatattr):
-#         #     try:
-#         #         valueattr[f] = float(AI.attr[fattr])
-#         #     except:
-#         #         valueattr[f] =  0
-#     
-#         try:
-#             poni_1 = float(AI.attrs['poni1'])
-#         except:
-#             poni_1 = 0
-#         try:
-#             poni_2 = float(AI.attrs['poni2'])
-#         except:
-#             poni_2 = 0
-#         
-#         try:
-#             rot_1 = float(AI.attrs['rot1'])
-#         except:
-#             rot_1 = 0
-#         try:
-#             rot_2 = float(AI.attrs['rot2'])
-#         except:
-#             rot_2 = 0
-#         try:
-#             rot_3 = float(AI.attrs['rot3'])
-#         except:
-#             rot_3 = 0
-# 
-#         try:
-#             pixel_1 = float(AI.attrs['ps1'])
-#         except:
-#             pixel_1 = 0
-#         try:
-#             pixel_2 = float(AI.attrs['ps2'])
-#         except:
-#             pixel_2 = 0
-# 
-#         try:
-#             spline = AI.attrs['spline']
-#             if spline == '':
-#                 spline = None
-#         except:
-#             spline = None
-#         
-#         try:
-#             detname = AI.attrs['detector']
-#             if detname == '':
-#                 detname = None
-#         except:
-#             detname = None
-#     
-#         try:
-#             xraylambda =float(AI.attrs['wavelength'])
-#         except:
-#             xraylambda = None
-#     else:
-#         print('pyFAI not imported. Cannot calculate ai for calibration.')
-#         return
-# 
-#         
-#     return pyFAI.AzimuthalIntegrator(dist = distance, poni1 = poni_1, poni2 = poni_2,
-#                                     rot1 = rot_1, rot2 = rot_2, rot3 = rot_3,
-#                                     pixel1 = pixel_1, pixel2 = pixel_2,
-#                                     splineFile = spline, detector = detname,
-#                                     wavelength = xraylambda)
-#########################################################################
-
-                     
+                    
 def registerLarchPlugin():
     return ('_xrd', {'integrate_xrd': integrate_xrd}) #,'calculate_ai': calculate_ai})
