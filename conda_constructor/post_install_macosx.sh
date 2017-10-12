7333642a4411449779230982e6aedcf5d832cbb1
Matthew Newville <newville@cars.uchicago.edu>
2017-10-12 17:16:02 -0500

    update constructor for 0.9.35rc2

--- conda_constructor/post_install_macosx.sh
+++ conda_constructor/post_install_macosx.sh
@@ -9,26 +9,5 @@ mv $PREFIX/python.app  $PREFIX/orig_python_app
 $PREFIX/bin/conda install python.app
 
 # fix scripts to use pythonw instead of python
-scripts="diFFit1D
-diFFit2D
-feff8l
-gse_dtcorrect
-gse_mapviewer
-gse_scanviewer
-larch
-larch_client
-larch_gui
-larch_makeicons
-larch_server
-xrfdisplay
-xrfdisplay_epics
-xyfit"
-
-for f in $scripts; do
-    fname="$PREFIX/bin/$f"
-    sed "s|$PREFIX/bin/python\$|$PREFIX/bin/pythonw|" $fname > Tmp
-    mv Tmp $fname
-done
-
 # make folder of simple Apps / Icons
 $PREFIX/bin/python $PREFIX/bin/larch_makeicons
