7e560f7efd609cc605c31dd2e09b6001374aaead
Matthew Newville <newville@cars.uchicago.edu>
2018-02-18 17:11:32 -0600

    simply/improve GSEXRM_Exceptions, no special NotOwner exception

--- plugins/xrmmap/__init__.py
+++ plugins/xrmmap/__init__.py
@@ -5,9 +5,9 @@ from .xrd_netcdf import read_xrd_netcdf
 from .xrd_hdf5 import read_xrd_hdf5
 from .asciifiles import (readASCII, readMasterFile, readROIFile,
                          readEnvironFile, read1DXRDFile, parseEnviron)
-from .xrm_mapfile import (read_xrfmap, read_xrmmap, 
+from .xrm_mapfile import (read_xrfmap, read_xrmmap,
                           process_mapfolder,
                           process_mapfolders,
                           h5str, ensure_subgroup,
                           GSEXRM_MapFile, GSEXRM_FileStatus,
-                          GSEXRM_Exception, GSEXRM_NotOwner)
+                          GSEXRM_Exception)
