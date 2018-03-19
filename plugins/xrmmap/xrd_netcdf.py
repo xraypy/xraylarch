0eee752b07d786e139a50a34c093bdc72fda536d
margaretkolbeannellen <koker@cars.uchicago.edu>
2018-03-19 15:30:53 -0500

    working on netcdf reader mkak 2018.03.19

--- plugins/xrmmap/xrd_netcdf.py
+++ plugins/xrmmap/xrd_netcdf.py
@@ -17,20 +17,17 @@ try:
 except ImportError:
     raise ImportError('cannot find scipy netcdf module')
 
-def read_xrd_netcdf(fname,verbose=False): #npixels=self.nrows_expected
+def read_xrd_netcdf(fname,verbose=False): 
     ## Reads a netCDF file created for XRD mapping
     
     if verbose:
         print(' reading %s' % fname)
     
     ## Reads an XRD netCDF file with the netCDF plugin buffers
-    xrd_file = netcdf_open(fname,'r')
-    xrd_data = xrd_file.variables['array_data'].data.copy()
+    xrd_data = read_netcdf(fname,only_key='array_data')
+    
     xrd_data = xrd_data.astype('uint16')
-    try:
-        xrd_file.close()
-    except:
-       pass
+
     
     ## Forces data into 3D shape
     shape = xrd_data.shape ## (no_images,pixels_x,pixels_y)
@@ -39,6 +36,34 @@ def read_xrd_netcdf(fname,verbose=False): #npixels=self.nrows_expected
         xrd_data.shape = (1, shape[0], shape[1])
 
     return xrd_data
+    
+def read_xrd_netcdf_exptime(fname,verbose=False):
+    '''
+    returns header information for provided xrd netcdf file
+    '''
+    
+    return read_netcdf(fname,only_key='Attr_FrameTime')
+
+def read_netcdf(fname,verbose=False,only_key=None):
+    '''
+    returns dictionary of all information in provided xrd netcdf file
+    '''
+   
+    file_netcdf = netcdf_open(fname)
+    netcdf_dict = {}
+
+    for key,val in dict(file_netcdf.variables).iteritems():
+        netcdf_dict[key] = val.data
+    
+    try:
+        if only_key is not None:
+            return netcdf_dict[only_key]
+        else:
+            return netcdf_dict
+    except:
+        file_netcdf.close()
+        pass
+
 
 def test_read(fname):
     print( fname,  os.stat(fname))
