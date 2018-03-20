f32a678584a8f798452393f58b740361325e7903
margaretkolbeannellen <koker@cars.uchicago.edu>
2018-03-20 10:55:07 -0500

    Testing netcdf reader options for xrd data files mkak 2018.03.20

--- plugins/xrmmap/xrd_netcdf.py
+++ plugins/xrmmap/xrd_netcdf.py
@@ -10,6 +10,8 @@ import os
 import copy
 from copy import deepcopy
 
+import time
+
 ## Package for reading netcdf files (in scipy)
 try:
     import scipy.io.netcdf
@@ -20,25 +22,158 @@ except ImportError:
 def read_xrd_netcdf(fname,verbose=False): #npixels=self.nrows_expected
     ## Reads a netCDF file created for XRD mapping
     
+#     a = time.time()
+#     if verbose:
+#         print(' reading %s' % fname)
+#     
+#     ## Reads an XRD netCDF file with the netCDF plugin buffers
+#     xrd_file = netcdf_open(fname,'r')
+#     xrd_data = xrd_file.variables['array_data'].data.copy()
+# #     xrd_data = xrd_data.astype('uint16')
+#     try:
+#         xrd_file.close()
+#     except:
+#        pass
+# #     
+# #     ## Forces data into 3D shape
+# #     shape = xrd_data.shape ## (no_images,pixels_x,pixels_y)
+# #     if len(shape) == 2:
+# #         print('Reshaping to (%i, %i, %i)' % (1, shape[0], shape[1]))
+# #         xrd_data.shape = (1, shape[0], shape[1])
+# # 
+#     print '1',time.time()-a
+#     return xrd_data
+
+    a = time.time()
     if verbose:
         print(' reading %s' % fname)
     
     ## Reads an XRD netCDF file with the netCDF plugin buffers
-    xrd_file = netcdf_open(fname,'r')
-    xrd_data = xrd_file.variables['array_data'].data.copy()
+    xrd_data = read_netcdf(fname,only_key='array_data').data
     xrd_data = xrd_data.astype('uint16')
-    try:
-        xrd_file.close()
-    except:
-       pass
-    
+
     ## Forces data into 3D shape
     shape = xrd_data.shape ## (no_images,pixels_x,pixels_y)
     if len(shape) == 2:
         print('Reshaping to (%i, %i, %i)' % (1, shape[0], shape[1]))
         xrd_data.shape = (1, shape[0], shape[1])
 
+    print '1',time.time()-a
     return xrd_data
+
+
+
+def read_xrd_netcdf2(fname,verbose=False): #npixels=self.nrows_expected
+    ## Reads a netCDF file created for XRD mapping
+    
+    a = time.time()
+    if verbose:
+        print(' reading %s' % fname)
+    
+    ## Reads an XRD netCDF file with the netCDF plugin buffers
+    xrd_file = netcdf_open(fname,'r')
+    xrd_data = xrd_file.variables['array_data'].data ##.copy()
+#     xrd_data = xrd_data.astype('uint16')
+    try:
+        xrd_file.close()
+    except:
+       pass
+#     
+#     ## Forces data into 3D shape
+#     shape = xrd_data.shape ## (no_images,pixels_x,pixels_y)
+#     if len(shape) == 2:
+#         print('Reshaping to (%i, %i, %i)' % (1, shape[0], shape[1]))
+#         xrd_data.shape = (1, shape[0], shape[1])
+
+    print '2',time.time()-a
+    return xrd_data
+
+
+def read_xrd_netcdf3(fname,verbose=False): #npixels=self.nrows_expected
+    ## Reads a netCDF file created for XRD mapping
+    
+    a = time.time()
+    if verbose:
+        print(' reading %s' % fname)
+    
+    ## Reads an XRD netCDF file with the netCDF plugin buffers
+    xrd_file = netcdf_open(fname,'r')
+    xrd_data = xrd_file.variables['array_data'] ##.data ##.copy()
+#     xrd_data = xrd_data.astype('uint16')
+    try:
+        xrd_file.close()
+    except:
+       pass
+#     
+#     ## Forces data into 3D shape
+#     shape = xrd_data.shape ## (no_images,pixels_x,pixels_y)
+#     if len(shape) == 2:
+#         print('Reshaping to (%i, %i, %i)' % (1, shape[0], shape[1]))
+#         xrd_data.shape = (1, shape[0], shape[1])
+
+    print '3',time.time()-a
+    return xrd_data
+
+def read_xrd_netcdf4(fname,verbose=False): #npixels=self.nrows_expected
+    ## Reads a netCDF file created for XRD mapping
+    
+    a = time.time()
+    if verbose:
+        print(' reading %s' % fname)
+    
+
+    xrd_data = read_xrd_netcdf3(fname).data
+
+    print '4',time.time()-a
+    return xrd_data
+
+
+# def read_xrd_netcdf(fname,verbose=False): 
+#     ## Reads a netCDF file created for XRD mapping
+#     
+#     if verbose:
+#         print(' reading %s' % fname)
+#     
+#     ## Reads an XRD netCDF file with the netCDF plugin buffers
+#     xrd_data = read_netcdf(fname,only_key='array_data')
+#     
+#     xrd_data = xrd_data.astype('uint16')
+# 
+#     
+#     ## Forces data into 3D shape
+#     shape = xrd_data.shape ## (no_images,pixels_x,pixels_y)
+#     if len(shape) == 2:
+#         print('Reshaping to (%i, %i, %i)' % (1, shape[0], shape[1]))
+#         xrd_data.shape = (1, shape[0], shape[1])
+# 
+#     return xrd_data
+#     
+# def read_xrd_netcdf_exptime(fname,verbose=False):
+#     '''
+#     returns header information for provided xrd netcdf file
+#     '''
+#     
+#     return read_netcdf(fname,only_key='Attr_FrameTime')
+# 
+def read_netcdf(fname,verbose=False,only_key=None):
+    '''
+    returns dictionary of all information in provided xrd netcdf file
+    '''
+   
+    a = time.time()
+    file_netcdf = netcdf_open(fname)
+
+    if only_key is not None and only_key is not in file_netcdf.variables.keys():
+        print 'end-a:',time.time()-a
+        return file_netcdf.variables[only_key]
+    else:
+        netcdf_dict = {}
+        for key,val in dict(file_netcdf.variables).iteritems():
+            netcdf_dict[key] = val
+        print 'end-b:',time.time()-a
+        return netcdf_dict
+
+
 
 def test_read(fname):
     print( fname,  os.stat(fname))
