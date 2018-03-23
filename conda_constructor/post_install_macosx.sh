8f28361f8ce7c651e97701393fc3ca2de602226d
Matthew Newville <newville@cars.uchicago.edu>
2018-03-23 14:42:37 -0500

    update constructor OSX script to not hack python.app, not pip install extras

--- conda_constructor/post_install_macosx.sh
+++ conda_constructor/post_install_macosx.sh
@@ -2,12 +2,13 @@
 
 echo "post_install script for Mac OSX, installing to $PREFIX"
 
-$PREFIX/bin/pip install --upgrade --use-wheel fabio pyFAI tifffile
-
-#  fix what seems to be a broken python.app install
-mv $PREFIX/python.app  $PREFIX/orig_python_app
-$PREFIX/bin/conda install -y python.app
-
 # fix scripts to use pythonw instead of python
 # make folder of simple Apps / Icons
+
 $PREFIX/bin/python $PREFIX/bin/larch_makeicons
+
+# $PREFIX/bin/pip install --upgrade --use-wheel fabio pyFAI tifffile
+
+### fix what seems to be a broken python.app install
+# mv $PREFIX/python.app  $PREFIX/orig_python_app
+# $PREFIX/bin/conda install -y python.app
