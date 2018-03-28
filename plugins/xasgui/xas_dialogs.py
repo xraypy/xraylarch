1c802e174c31145ba88ae2f98ecb0d9644a97ff3
Matthew Newville <newville@cars.uchicago.edu>
2018-03-28 11:22:31 -0500

    add dialog for setting energy units for conversion to eV

--- plugins/xasgui/xas_dialogs.py
+++ plugins/xasgui/xas_dialogs.py
@@ -1,8 +1,84 @@
 from collections import namedtuple
 
+import numpy as np
 import wx
+from wxutils import (FloatCtrl, SimpleText, Choice, OkCancel, GridPanel, LCEN)
 
-from wxutils import (SimpleText, Choice, OkCancel, GridPanel, LCEN)
+PI = np.pi
+DEG2RAD  = PI/180.0
+
+# Planck constant over 2 pi times c: 197.3269718 (0.0000044) MeV fm
+PLANCK_HC = 1973.269718 * 2 * PI # hc in eV * Ang = 12398.4193
+
+
+class EnergyUnitsDialog(wx.Dialog):
+    """dialog for selecting, changing energy units, forcing data to eV"""
+    msg = """Specify Energy Units"""
+    unit_choices = ['eV', 'keV', 'deg', 'steps']
+
+    def __init__(self, parent, unitname, energy_array, **kws):
+
+        self.energy = energy_array[:]
+
+        title = "Select Energy Units to convert to 'eV'"
+        wx.Dialog.__init__(self, parent, wx.ID_ANY, title=title)
+
+        panel = GridPanel(self, ncols=3, nrows=4, pad=2, itemstyle=LCEN)
+
+        self.en_units = Choice(panel, choices=self.unit_choices, size=(125, -1),
+                               action=self.onUnits)
+        self.en_units.SetStringSelection(unitname)
+        self.mono_dspace = FloatCtrl(panel, value=1.0, minval=0, maxval=100.0,
+                                     precision=6, size=(125, -1))
+        self.steps2deg  = FloatCtrl(panel, value=1.0, minval=0,
+                                     precision=1, size=(125, -1))
+
+        self.mono_dspace.Disable()
+        self.steps2deg.Disable()
+
+        panel.Add(SimpleText(panel, 'Energy Units : '), newrow=True)
+        panel.Add(self.en_units)
+
+        panel.Add(SimpleText(panel, 'Mono D spacing : '), newrow=True)
+        panel.Add(self.mono_dspace)
+
+        panel.Add(SimpleText(panel, 'Mono Steps per Degree : '), newrow=True)
+        panel.Add(self.steps2deg)
+        panel.Add((5, 5))
+
+        panel.Add(OkCancel(panel), dcol=2, newrow=True)
+
+        panel.pack()
+
+    def onUnits(self, event=None):
+        units = self.en_units.GetStringSelection()
+
+        self.steps2deg.Disable()
+        self.mono_dspace.Disable()
+
+        if units in ('deg', 'steps'):
+            self.mono_dspace.Enable()
+            if units == 'steps':
+                self.steps2deg.Enable()
+
+    def GetResponse(self, master=None, gname=None, ynorm=True):
+        self.Raise()
+        response = namedtuple('EnergyUnitsResponse', ('ok', 'units', 'energy'))
+        ok, units, en = False, 'eV', None
+
+        if self.ShowModal() == wx.ID_OK:
+            units = self.en_units.GetStringSelection()
+            if units == 'eV':
+                en = self.energy
+            elif units == 'keV':
+                en = self.energy * 1000.0
+            elif units in ('steps', 'deg'):
+                dspace = float(self.mono_dspace.GetValue())
+                if units == 'steps':
+                    self.energy /= self.steps2deg.GetValue()
+                en = (PLANCK_HC/(2*dspace))/np.sin(self.energy * DEG2RAD)
+            ok = True
+        return response(ok, units, en)
 
 class MergeDialog(wx.Dialog):
     """dialog for merging groups"""
