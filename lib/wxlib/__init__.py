5d989bb47e3c2a5ba68b564d0574ab4d6be80ece
Matthew Newville <newville@cars.uchicago.edu>
2018-05-09 16:01:44 -0500

    move icons from plugin to wxlib

--- lib/wxlib/__init__.py
+++ lib/wxlib/__init__.py
@@ -8,9 +8,10 @@ from . import readlinetextctrl
 from . import utils
 from .utils import (ToggleButton, BitmapButton, SetTip,
                     FileCheckList, FileDropTarget, GridPanel)
-
+from .icons import get_icon
 from .larchframe import LarchFrame, LarchPanel
 from .columnframe import ColumnDataFileFrame, EditColumnFrame
 from .reportframe import ReportFrame
-from .floats import make_steps, set_float, FloatCtrl, NumericCombo, FloatSpin
+from .floats import (make_steps, set_float, FloatCtrl, NumericCombo,
+                     FloatSpin, FloatSpinWithPin)
 from .parameter import ParameterWidgets
