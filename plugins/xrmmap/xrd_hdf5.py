76b0dd7e3ae0f44ec9da172b66212685db384718
Matthew Newville <newville@cars.uchicago.edu>
2018-11-18 22:42:47 -0600

    look for multiple h5 locations to read XRD data from

--- plugins/xrmmap/xrd_hdf5.py
+++ plugins/xrmmap/xrd_hdf5.py
@@ -12,19 +12,21 @@ from larch import Group, ValidateLarchPlugin
 
 def read_xrd_hdf5(fname, verbose=False, _larch=None):
     # Reads a HDF5 file created for XRD mapping
-    
-    t0 = time.time()
     h5file = h5py.File(fname, 'r')
 
-    ## f['entry/data/data'] & f['entry/instrument/detector/data'] are same values
-    xrd_data = h5file['entry/instrument/detector/data'].value
+    addr = 'entry/data/data'
+    for section in ('entry/data/data_000001',
+                    'entry/instrument/detector/data'):
+        if section in h5file:
+            addr = section
+            break
+    xrd_data = h5file[addr].value
 
     ## Forces data into 3D shape
     shape = xrd_data.shape ## (no_images,pixels_x,pixels_y)
     if len(shape) == 2:
         print('Reshaping to (%i, %i, %i)' % (1, shape[0], shape[1]))
         xrd_data.shape = (1, shape[0], shape[1])
-
     return xrd_data
 
 def test_read(fname):
