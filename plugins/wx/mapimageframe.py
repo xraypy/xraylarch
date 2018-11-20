a260993bf1c4de32bbd05cf87e0ad650b57915a9
Matthew Newville <newville@cars.uchicago.edu>
2018-11-20 11:22:58 -0600

    size tweaks

--- plugins/wx/mapimageframe.py
+++ plugins/wx/mapimageframe.py
@@ -35,7 +35,7 @@ class MapImageFrame(ImageFrame):
     MatPlotlib Image Display on a wx.Frame, using ImagePanel
     """
 
-    def __init__(self, parent=None, size=(750, 675), mode='intensity',
+    def __init__(self, parent=None, size=(650, 650), mode='intensity',
                  lasso_callback=None, move_callback=None, save_callback=None,
                  show_xsections=False, cursor_labels=None,
                  output_title='Image', **kws):
@@ -59,10 +59,11 @@ class MapImageFrame(ImageFrame):
         self.panel.report_leftdown = self.report_leftdown
         self.panel.report_motion   = self.report_motion
 
-        w, h = self.GetSize()
-        w = min(w, 750)
-        h = min(h, 675)
-        self.SetSize((w, h))
+        w0, h0 = self.GetSize()
+        w1, h1 = self.GetBestSize()
+
+        self.SetSize((max(w0, w1)+5, max(h0, h1)+5))
+        self.SetMinSize((500, 500))
 
         self.prof_plotter = None
         self.zoom_ini =  None
