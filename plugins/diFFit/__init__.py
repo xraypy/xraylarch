d548aeafc2c0b016ebcc1709c91f75fbc2a077cb
Matthew Newville <newville@cars.uchicago.edu>
2018-11-28 19:47:43 -0600

    refactor and fix running of XRD 1D and 2D apps

--- plugins/diFFit/__init__.py
+++ plugins/diFFit/__init__.py
@@ -1,5 +1,5 @@
-from .XRD2Dviewer import diFFit2DFrame
-from .XRD1Dviewer import diFFit1DFrame,Calc1DPopup,plot_sticks
+from .XRD2Dviewer import XRD2DViewerFrame
+from .XRD1Dviewer import XRD1DViewerFrame, Calc1DPopup, plot_sticks
 from .ImageControlsFrame import ImageToolboxFrame
-from .XRDCalibrationFrame import CalibrationPopup,CalXRD
+from .XRDCalibrationFrame import CalibrationPopup, CalXRD
 from .XRDMaskFrame import MaskToolsPopup
