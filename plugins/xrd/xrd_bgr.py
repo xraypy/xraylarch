d7169dd0cc461a93da8e5c21ee3e9d27e5203e2a
Matthew Newville <newville@cars.uchicago.edu>
2018-12-26 08:16:52 -0800

    no need for type_int or data_type

--- plugins/xrd/xrd_bgr.py
+++ plugins/xrd/xrd_bgr.py
@@ -15,9 +15,9 @@ def xrd_background(xdata, ydata, width=4, compress=5, exponent=2, slope=None): #
     Arguments
     ---------
     xdata     array of q values (or 2th, d?)
-    
+
     ydata     associated array of I values
-    
+
     group     group for outputs
 
     width      full width of the concave down polynomials
@@ -26,21 +26,19 @@ def xrd_background(xdata, ydata, width=4, compress=5, exponent=2, slope=None): #
     compress   compression factor to apply to spectra.
 
     exponent   power of polynomial used.  Should be even.
-    
+
     slope      channel to energy conversion, from energy calibration
                (default == None --> found from input energy array)
     -------
     bgr       background array
     bgr_info  dictionary of parameters used to calculate background
     """
-   
+
     if slope is None:
         slope = (xdata[-1] - xdata[0])/len(xdata)
 
     xbgr = XrayBackground(ydata, width=width, compress=compress,
-                         exponent=exponent, slope=slope, tangent=True,
-                         type_int=False, data_type='xrd')
-
+                          exponent=exponent, slope=slope, tangent=True)
     return xbgr.bgr
 
 # def registerLarchPlugin():
