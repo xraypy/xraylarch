b59b5c1a8df6727d1dfd63ebb0cfb8875b1ca47d
Matthew Newville <newville@cars.uchicago.edu>
2018-12-26 10:00:21 -0800

    more tweaks to PeriodicTable layout

--- plugins/wx/periodictable.py
+++ plugins/wx/periodictable.py
@@ -69,12 +69,11 @@ class PeriodicTablePanel(wx.Panel):
 
     FRAME_BG = (253, 253, 250) ## light grey
     TITLE_BG = (253, 253, 250) ## light grey
-    REG_BG   = (253, 253, 250) ## light grey
-    REG_FG   = ( 20,  20, 120) ## blue
-    SEL_BG   = (254, 254,   0) ## yellow
-    SEL_FG   = ( 20,  20, 120) ## blue
+    FGCOL = ( 20,  20, 120) ## blue
+    BGCOL = (253, 253, 250) ## light grey
+    BGSEL = (254, 254,   0) ## yellow
 
-    def __init__(self, parent, title='Select Element', multi_select=True,
+    def __init__(self, parent, title='Select Element', multi_select=False,
                  onselect=None, tooltip_msg=None, size=(-1, -1), **kws):
         wx.Panel.__init__(self, parent, -1, size=size, name='PeriodicTable', **kws)
         self.parent = parent
@@ -148,8 +147,7 @@ class PeriodicTablePanel(wx.Panel):
 
     def on_clear_all(self, event=None):
         for wid in self.ctrls.values():
-            wid.SetForegroundColour(self.REG_FG)
-            wid.SetBackgroundColour(self.REG_BG)
+            wid.SetBackgroundColour(self.BGCOL)
         self.selected = []
         self.current = None
 
@@ -162,30 +160,25 @@ class PeriodicTablePanel(wx.Panel):
             label = textwid.GetLabel()
         if label is None:
             return
-        if textwid is None and label:
+        if textwid is None and label is not None:
             textwid = self.ctrls[label]
 
         if self.multi_select:
-            if textwid.GetBackgroundColour() == self.REG_BG:
-                textwid.SetForegroundColour(self.SEL_FG)
-                textwid.SetBackgroundColour(self.SEL_BG)
+            if textwid.GetBackgroundColour() == self.BGCOL:
+                textwid.SetBackgroundColour(self.BGSEL)
                 self.selected.append(label)
             else:
-                textwid.SetForegroundColour(self.REG_FG)
-                textwid.SetBackgroundColour(self.REG_BG)
+                textwid.SetBackgroundColour(self.BGCOL)
                 try:
                     self.selected.remove(label)
                 except ValueError:
                     pass
         else:
-            textwid.SetForegroundColour(self.SEL_FG)
-            textwid.SetBackgroundColour(self.SEL_BG)
+            textwid.SetBackgroundColour(self.BGSEL)
             if self.current is not None and self.current != textwid:
-                self.current.SetForegroundColour(self.REG_FG)
-                self.current.SetBackgroundColour(self.REG_BG)
+                self.current.SetBackgroundColour(self.BGCOL)
             self.current = textwid
-            self.selected.clear()
-            self.selected.apend(textwid)
+            self.selected = [textwid]
 
         znum = self.syms.index(label)
         name = self.names[znum]
@@ -205,8 +198,8 @@ class PeriodicTablePanel(wx.Panel):
         for name, coords in self.elems.items():
             tw = wx.StaticText(self, -1, label=name)
             tw.SetFont(self.elemfont)
-            tw.SetForegroundColour(self.REG_FG)
-            tw.SetBackgroundColour(self.REG_BG)
+            tw.SetForegroundColour(self.FGCOL)
+            tw.SetBackgroundColour(self.BGCOL)
             #tw.SetMinSize((18, 18))
             tw.Bind(wx.EVT_LEFT_DOWN, self.onclick)
             if self.tooltip_msg is not None:
@@ -222,9 +215,9 @@ class PeriodicTablePanel(wx.Panel):
             a.SetFont(self.titlefont)
             a.SetBackgroundColour(self.TITLE_BG)
 
-        sizer.Add(self.title, (0, 4), (1, 8), wx.ALIGN_CENTER, 3)
-        sizer.Add(self.tsym,  (0, 2), (1, 2), wx.ALIGN_LEFT, 3)
-        sizer.Add(self.tznum, (0, 12), (1, 3), wx.ALIGN_LEFT, 3)
+        sizer.Add(self.title, (0, 4), (1, 8), wx.ALIGN_CENTER, 5)
+        sizer.Add(self.tsym,  (0, 2), (1, 2), wx.ALIGN_LEFT, 5)
+        sizer.Add(self.tznum, (0, 12), (1, 3), wx.ALIGN_LEFT, 5)
 
         self.subtitle = [None, None]
         self.subtitle[0] = wx.StaticText(self, -1, label='         ')
