9b264147f43fd75556b1c0251a1d003706f5d637
Matthew Newville <newville@cars.uchicago.edu>
2019-01-09 21:34:09 -0600

    add (start of) improved XRF modeling

--- plugins/xrf/__init__.py
+++ plugins/xrf/__init__.py
@@ -2,5 +2,8 @@ from .mca import MCA, isLarchMCAGroup, Environment
 from .roi import ROI, split_roiname
 from .deadtime import calc_icr, correction_factor
 from .xrf_bgr import xrf_background
+
 from .xrf_calib import xrf_calib_fitrois, xrf_calib_compute, xrf_calib_apply
+
 from .xrf_peak import xrf_peak
+from .xrf_model import xrf_model
