7f5b5ab6f45fdb73bfeb2a810376121e36186762
Matthew Newville <newville@cars.uchicago.edu>
2019-01-27 22:00:48 -0600

    specify some versions

--- conda_constructor/post_install_linux.sh
+++ conda_constructor/post_install_linux.sh
@@ -7,7 +7,7 @@ echo "### Larch > installing more scientific python packages"
 $PREFIX/bin/conda install cython h5py pillow sqlalchemy psycopg2 scikit-image scikit-learn pandas wxpython=4.0.4
 
 echo "### Larch > installing Larch-specific packages"
-$PREFIX/bin/conda install -c gsecars pycifrw peakutils pyshortcuts pyepics lmfit asteval wxutils wxmplot xraylarch dioptas tomopy
+$PREFIX/bin/conda install -c gsecars pycifrw peakutils pyshortcuts pyepics=3.3.3 lmfit=0.9.12 asteval wxutils=0.2.3 wxmplot=0.9.34 xraylarch dioptas tomopy
 
 $PREFIX/bin/pip install --upgrade fabio pyFAI tifffile silx hdf5plugin
 
