6752b4ba32671e8154042abe438279454b0a208b
Matthew Newville <newville@cars.uchicago.edu>
2019-02-25 16:16:16 -0600

    add install of qtpy/pyqtgraph for dioptas, avoid duplicate files by installing tomopy in post-install (windows only)

--- conda_constructor/post_install_windows.bat
+++ conda_constructor/post_install_windows.bat
@@ -1,6 +1,5 @@
 REM  post install for windows
 
-REM %PREFIX%\Scripts\conda.exe install -yc gsecars pycifrw dioptas tomopy xraylarch=0.9.41
-REM %PREFIX%\Scripts\pip.exe install --upgrade fabio pyFAI silx hdf5plugin
+%PREFIX%\Scripts\conda.exe install -yc gsecars tomopy
 
 %PREFIX%\python.exe %PREFIX%\Scripts\larch-script.py -m
