87a0179641dfb5da92c319842770b031cf61156d
Matthew Newville <newville@cars.uchicago.edu>
2019-02-25 15:01:01 -0600

    update constructor script

--- conda_constructor/post_install_linux.sh
+++ conda_constructor/post_install_linux.sh
@@ -1,9 +1,8 @@
 #/bin/bash
 
-echo "### Larch > installing Larch-specific packages"
-$PREFIX/bin/conda install -yc gsecars dioptas tomopy xraylarch=0.9.41
-
-$PREFIX/bin/pip install --upgrade fabio pyFAI tifffile silx hdf5plugin
+# echo "### Larch > installing Larch-specific packages"
+# $PREFIX/bin/conda install -yc gsecars dioptas tomopy xraylarch=0.9.41
+# $PREFIX/bin/pip install --upgrade fabio pyFAI tifffile silx hdf5plugin
 
 echo "### Larch > creating desktop shortcuts in Desktop/Larch folder"
 $PREFIX/bin/python $PREFIX/bin/larch -m
--- conda_constructor/post_install_windows.bat
+++ conda_constructor/post_install_windows.bat
@@ -1,7 +1,6 @@
-;;  post install for windows
+REM  post install for windows
 
-%PREFIX%\Scripts\conda.exe install -yc gsecars pycifrw dioptas tomopy xraylarch=0.9.41
-
-%PREFIX%\Scripts\pip.exe install --upgrade fabio pyFAI silx hdf5plugin
+REM %PREFIX%\Scripts\conda.exe install -yc gsecars pycifrw dioptas tomopy xraylarch=0.9.41
+REM %PREFIX%\Scripts\pip.exe install --upgrade fabio pyFAI silx hdf5plugin
 
 %PREFIX%\python.exe %PREFIX%\Scripts\larch-script.py -m
