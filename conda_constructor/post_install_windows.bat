53fe7cd37fba95a250269df23e2cd733db5b7135
Matthew Newville <newville@cars.uchicago.edu>
2019-02-26 07:52:04 -0600

    update constructor recipes to install qtpy (noarch) in post_install

--- conda_constructor/post_install_windows.bat
+++ conda_constructor/post_install_windows.bat
@@ -1,5 +1,6 @@
 REM  post install for windows
 
 %PREFIX%\Scripts\conda.exe install -yc gsecars tomopy
+%PREFIX%\Scripts\conda.exe install --force-reinstall qtpy
 
 %PREFIX%\python.exe %PREFIX%\Scripts\larch-script.py -m
