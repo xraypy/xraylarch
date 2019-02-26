391dc0ee543fa52ed5521c02925092583b37622f
Matthew Newville <newville@cars.uchicago.edu>
2019-02-26 12:10:41 -0600

    force non-interactive reinstall

--- conda_constructor/post_install_windows.bat
+++ conda_constructor/post_install_windows.bat
@@ -1,5 +1,6 @@
 REM  post install for windows
 
-%PREFIX%\Scripts\conda.exe install -yc gsecars --force-reinstall tomopy qtpy
+%PREFIX%\Scripts\conda.exe install -y --force-reinstall qtpy
+%PREFIX%\Scripts\conda.exe install -yc gsecars tomopy
 
 %PREFIX%\python.exe %PREFIX%\Scripts\larch-script.py -m
