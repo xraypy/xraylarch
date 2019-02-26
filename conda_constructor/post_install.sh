8b16bf998a19883267a1d0e793c79d9064832cc3
Matthew Newville <newville@cars.uchicago.edu>
2019-02-25 21:16:26 -0600

    updated constructor to build macosx pkg, common unix post-install script to make desktop icons

---
+++ conda_constructor/post_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# $2 is the install location, ($HOME by default)
+if [ xx$PREFIX == 'xx' ]; then
+    PREFIX=$(echo "$2/__NAME_LOWER__" | sed -e 's,//,/,g')
+fi
+
+echo "### Larch > creating desktop shortcuts in Desktop/Larch folder"
+$PREFIX/bin/python $PREFIX/bin/larch -m
