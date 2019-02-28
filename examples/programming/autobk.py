a5dc57d61974b88724adc6f1e0fda5f21cdfd0c8
Matthew Newville <newville@cars.uchicago.edu>
2019-02-27 20:13:40 -0600

    some Python3 fixes for print() in examples

--- examples/programming/autobk.py
+++ examples/programming/autobk.py
@@ -13,14 +13,14 @@ _larch = Interpreter(with_plugins=False)
 fname = '../xafsdata/cu_rt01.xmu'
 
 cu = read_ascii(fname, labels='energy mu i0', _larch=_larch)
-print 'Read ASCII File:', cu
-print dir(cu)
+print( 'Read ASCII File:', cu)
+print( dir(cu))
 
 pre_edge(cu, _larch=_larch)
-print 'After pre-edge:'
-print dir(cu)
+print( 'After pre-edge:')
+print( dir(cu))
 
 autobk(cu, rbkg=1.0, kweight=1, _larch=_larch)
 
-print 'After autobk:'
-print dir(cu)
+print( 'After autobk:')
+print( dir(cu))
