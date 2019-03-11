ee88552904e5e8f403d039c9556226b98855e9e7
Matthew Newville <newville@cars.uchicago.edu>
2019-03-10 22:46:12 -0500

    migrate xrd, cif plugins

--- larch/xrd/__init__.py
+++ larch/xrd/__init__.py
@@ -17,3 +17,5 @@ from .xrd_tools import (d_from_q, d_from_twth, twth_from_d, twth_from_q,
                         unit_cell_volume, generate_hkl)
 
 from .xrd_cif import (SPACEGROUPS, create_cif, check_elemsym, SPGRP_SYMM)
+from .cifdb import (get_cifdb, cifDB, SearchCIFdb, match_database,
+                    CATEGORIES, QSTEP, QMIN, QMAX, QAXIS)
--- plugins/cifdb/__init__.py
+++
@@ -1,2 +0,0 @@
-from .cifdb import (get_cifdb, cifDB, SearchCIFdb, match_database,
-                    CATEGORIES, QSTEP, QMIN, QMAX, QAXIS)
