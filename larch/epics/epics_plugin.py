ee75c767d81b1663075832c39a25990cf2c005c4
Matthew Newville <newville@cars.uchicago.edu>
2019-03-16 11:29:12 -0500

    more updates to epics code

---
+++ larch/epics/epics_plugin.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python
+"""
+Use Epics Channel Access
+"""
+
+MODDOC = """
+Functions for Epics Channel Access, using pyepics interface.
+For further details, consult the pyepics documentation
+
+  Functions
+  -------------
+  caget(pvname, as_string=False, count=None, timeout=None)
+           get and return value for an Epics Process Variable
+           with options to ensure return value is a string,
+           to limit the count for array values, or to specify
+           a maximum time to wait for a value.
+  caput(pvname, value, wait=False, timeout=60)
+           put a value to an Epics Process Variable, optionally
+           waiting to return until record is fully processed,
+           with a timeout value.
+  cainfo(pvname, print_out=True)
+           print out information about Process Variable. with
+           print_out = False, returns string of information.
+  PV(pvname)
+           create a Process Variable object with get()/put()
+           and several other methods.
+  pv_units(pvname)
+           return units for PV
+  pv_fullname(pvname)
+           return fullname (that is, including '.VAL' if needed) for PV
+"""
+
+caget_doc = """caget(pvname, as_string=False, use_numpy=True, timeout=None)
+
+    get value for Epics PV
+
+    Parameters
+    ----------
+      pvname:    string name of PV
+      as_string: True/False(default) return string representation
+      use_numpy: True(default)/False return numpy array for array PVs
+      count:     max count to return for array PVs
+      timeout:   time in seconds to wait for slow/unconnected PV
+
+    Returns
+    -------
+       PV's value, can by any type.
+
+    Examples
+    --------
+       x = caget('XXX.VAL')
+       x = caget('XXX.VAL', as_string=True)
+
+    Notes
+    ------
+      1. The default timeout is 0.5 sec for scalar PVs.
+%s
+"""
+
+caput_doc = """caput(pvname, value, wait=False, timeout=60)
+
+    put value for Epics PV
+
+    Parameters
+    ----------
+      pvname:    string name of PV
+      value:     value to put.
+      wait:      True/False(default) whether to wait for processing
+                 to complete before returning.
+      timeout:   time in seconds to wait for completion
+
+    Examples
+    --------
+       caput('XXX.VAL', 22)
+       caput('XXX.VAL', 0.0, wait=True)
+
+    Returns
+    -------
+       None
+
+    Notes
+    -----
+     1.  waiting may take a long time, as it waits for all processing
+         to complete (motor move, detector acquire to finish, etc).
+%s
+"""
+
+cainfo_doc = """cainfo(pvname, print_out=True)
+
+    return printable information about pv
+
+    Parameters
+    ----------
+      pvname:    string name of PV
+      print_out: True(default)/False whether to print info to standard out
+                 or return sring with info
+
+    Returns
+    -------
+       None or string with info paragraph
+
+    Examples
+    --------
+      cainfo('xx.VAL')
+%s    
+   """
+
+pv_doc = """PV(pvname)
+
+    create an Epics PV (Process Variable) object
+
+    Parameters
+    ----------
+      pvname:    string name of PV
+
+    Examples
+    --------
+      mypv = PV('xx.VAL')
+      mypv.get()
+      mypv.put(value)
+      print(mypv.pvname, mypv.count, mypv.type)
+
+    Notes
+    -----
+      A PV has many attributes and methods.  Consult the documentation.
+%s
+"""
+
+
+def nullfcn(*args, **kwargs): 
+    return None
+
+def caget(pvname, _larch=None, **kws):
+    return nullfcn(pvname, **kws)
+
+def caput(pvname, value, _larch=None, **kws):
+    return nullfcn(pvname, value, **kws)
+
+def cainfo(pvname, print_out=True, _larch=None):
+    return nullfcn(pvname, print_out=print_out)
+
+def PV(pvname, _larch=None, **kws):
+    return nullfcn(pvname, **kws)
+
+def pv_fullname(name):
+    """ make sure an Epics PV name ends with .VAL or .SOMETHING!
+    
+    Parameters
+    ----------
+       pvname:   name of PV
+
+    Returns
+    -------
+       string with full PV name
+
+    """
+    name = str(name)
+    if '.' not in name:
+        name = "%s.VAL" % name
+    return name
+
+def pv_units(pv, default=''):
+    """get units for pv object, with optional default value
+
+    Parameters
+    ----------
+       pv:       pv object (created by PV())
+       default:  string value for default units
+
+    Returns
+    -------
+       string with units
+
+    """
+    try:
+        units = pv.units
+    except:
+        units = ''
+    if units in (None, ''):
+        units = default
+    return units
+
+
+msg = """
+    !!! WARNING: !!!
+    ---------------- 
+       epics not installed!  Function will return None!
+"""
+
+try:
+    import epics
+    msg = ''
+except:
+    pass
+else:
+    def caget(pvname, _larch=None, **kws):
+        return epics.caget(pvname, **kws)
+
+    def caput(pvname, value, _larch=None, **kws):
+        return epics.caput(pvname, value, **kws)
+
+    def cainfo(pvname, print_out=True, _larch=None):
+        return epics.cainfo(pvname, print_out=print_out)
+
+    def PV(pvname, _larch=None, **kws):
+        return epics.get_pv(pvname, **kws)
+
+caget.__doc__ = caget_doc % msg
+caput.__doc__ = caput_doc % msg
+cainfo.__doc__ = cainfo_doc % msg
+PV.__doc__ = pv_doc % msg
+
+def initializeLarchPlugin(_larch=None):
+    """initialize _epics"""
+    _larch.symtable._epics.__doc__ = MODDOC
+
+def registerLarchPlugin():
+    return ('_epics', {'PV': PV, 
+                       'caget': caget, 
+                       'caput': caput, 
+                       'cainfo': cainfo, 
+                       'pv_units': pv_units,
+                       'pv_fullname': pv_fullname})
+
---
+++ larch/epics/larchscan.py
@@ -0,0 +1,239 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+MODDOC = """
+=== Epics Scanning Functions for Larch ===
+
+
+This does not used the Epics SScan Record, and the scan is intended to run
+as a python application, but many concepts from the Epics SScan Record are
+borrowed.  Where appropriate, the difference will be noted here.
+
+A Step Scan consists of the following objects:
+   a list of Positioners
+   a list of Triggers
+   a list of Counters
+
+Each Positioner will have a list (or numpy array) of position values
+corresponding to the steps in the scan.  As there is a fixed number of
+steps in the scan, the position list for each positioners must have the
+same length -- the number of points in the scan.  Note that, unlike the
+SScan Record, the list of points (not start, stop, step, npts) must be
+given.  Also note that the number of positioners or number of points is not
+limited.
+
+A Trigger is simply an Epics PV that will start a particular detector,
+usually by having 1 written to its field.  It is assumed that when the
+Epics ca.put() to the trigger completes, the Counters associated with the
+triggered detector will be ready to read.
+
+A Counter is simple a PV whose value should be recorded at every step in
+the scan.  Any PV can be a Counter, including waveform records.  For many
+detector types, it is possible to build a specialized class that creates
+many counters.
+
+Because Triggers and Counters are closely associated with detectors, a
+Detector is also defined, which simply contains a single Trigger and a list
+of Counters, and will cover most real use cases.
+
+In addition to the core components (Positioners, Triggers, Counters, Detectors),
+a Step Scan contains the following objects:
+
+   breakpoints   a list of scan indices at which to pause and write data
+                 collected so far to disk.
+   extra_pvs     a list of (description, PV) tuples that are recorded at
+                 the beginning of scan, and at each breakpoint, to be
+                 recorded to disk file as metadata.
+   pre_scan()    method to run prior to scan.
+   post_scan()   method to run after scan.
+   at_break()    method to run at each breakpoint.
+
+Note that Postioners and Detectors may add their own pieces into extra_pvs,
+pre_scan(), post_scan(), and at_break().
+
+With these concepts, a Step Scan ends up being a fairly simple loop, going
+roughly (that is, skipping error checking) as:
+
+   pos = <DEFINE POSITIONER LIST>
+   det = <DEFINE DETECTOR LIST>
+   run_pre_scan(pos, det)
+   [p.move_to_start() for p in pos]
+   record_extra_pvs(pos, det)
+   for i in range(len(pos[0].array)):
+       [p.move_to_pos(i) for p in pos]
+       while not all([p.done for p in pos]):
+           time.sleep(0.001)
+       [trig.start() for trig in det.triggers]
+       while not all([trig.done for trig in det.triggers]):
+           time.sleep(0.001)
+       [det.read() for det in det.counters]
+
+       if i in breakpoints:
+           write_data(pos, det)
+           record_exrta_pvs(pos, det)
+           run_at_break(pos, det)
+   write_data(pos, det)
+   run_post_scan(pos, det)
+
+Note that multi-dimensional mesh scans over a rectangular grid is not
+explicitly supported, but these can be easily emulated with the more
+flexible mechanism of unlimited list of positions and breakpoints.
+Non-mesh scans are also possible.
+
+A step scan can have an Epics SScan Record or StepScan database associated
+with it.  It will use these for PVs to post data at each point of the scan.
+"""
+import os, shutil
+import time
+from threading import Thread
+import json
+import numpy as np
+import random
+import six
+
+from datetime import timedelta
+
+from larch import Group, ValidateLarchPlugin
+from larch.utils import debugtime, fix_varname
+
+try:
+    from epics import PV, caget, caput, get_pv, poll
+    HAS_EPICS = True
+except ImportError:
+    HAS_EPICS = False
+
+try:
+    import epicsscan
+    from epicsscan import (Counter, Trigger, AreaDetector, get_detector,
+                           ASCIIScanFile, Positioner)
+    from epicsscan.scandb import ScanDBException, ScanDBAbort
+    from epics.devices.struck import Struck
+
+    HAS_EPICSSCAN = True
+except ImportError:
+    HAS_EPICSSCAN = False
+
+
+MODNAME = '_scan'
+
+MIN_POLL_TIME = 1.e-3
+
+
+@ValidateLarchPlugin
+def scan_from_db(scanname, filename='scan.001',  _larch=None):
+    """
+    get scan definition from ScanDB by name
+    """
+    if _larch.symtable._scan._scandb is None:
+        return
+    scandb = _larch.symtable._scan._scandb
+    # print(" Scan From DB ", scanname, scandb)
+    try:
+        scan = scandb.make_scan(scanname, larch=_larch)
+        scan.filename = filename
+    except ScanDBException:
+        raise ScanDBException("no scan definition '%s' found" % scanname)
+    return scan
+
+@ValidateLarchPlugin
+def do_scan(scanname, filename='scan.001', nscans=1, comments='', _larch=None):
+    """do_scan(scanname, filename='scan.001', nscans=1, comments='')
+
+    execute a step scan as defined in Scan database
+
+    Parameters
+    ----------
+    scanname:     string, name of scan
+    filename:     string, name of output data file
+    comments:     string, user comments for file
+    nscans:       integer (default 1) number of repeats to make.
+
+    Examples
+    --------
+      do_scan('cu_xafs', 'cu_sample1.001', nscans=3)
+
+    Notes
+    ------
+      1. The filename will be incremented so that each scan uses a new filename.
+    """
+
+    if _larch.symtable._scan._scandb is None:
+        print('need to connect to scandb!')
+        return
+    scandb =  _larch.symtable._scan._scandb
+    if nscans is not None:
+        scandb.set_info('nscans', nscans)
+
+    scan = scan_from_db(scanname, filename=filename,  _larch=_larch)
+    scan.comments = comments
+    if scan.scantype == 'slew':
+        return scan.run(filename=filename, comments=comments)
+    else:
+        scans_completed = 0
+        nscans = int(scandb.get_info('nscans'))
+        abort  = scandb.get_info('request_abort', as_bool=True)
+        while (scans_completed  < nscans) and not abort:
+            scan.run()
+            scans_completed += 1
+            nscans = int(scandb.get_info('nscans'))
+            abort  = scandb.get_info('request_abort', as_bool=True)
+        return scan
+
+
+@ValidateLarchPlugin
+def get_dbinfo(key, default=None, as_int=False, as_bool=False,
+               full_row=False, _larch=None, **kws):
+    """get a value for a keyword in the scan info table,
+    where most status information is kept.
+
+    Arguments
+    ---------
+     key        name of data to look up
+     default    (default None) value to return if key is not found
+     as_int     (default False) convert to integer
+     as_bool    (default False) convert to bool
+     full_row   (default False) return full row, not just value
+
+    Notes
+    -----
+     1.  if this key doesn't exist, it will be added with the default
+         value and the default value will be returned.
+     2.  the full row will include notes, create_time, modify_time
+
+    """
+    if _larch.symtable._scan._scandb is None:
+        print('need to connect to scandb!')
+        return
+    get_info = _larch.symtable._scan._scandb.get_info
+    return get_info(key, default=default, full_row=full_row,
+                    as_int=as_int, as_bool=as_bool, **kws)
+
+@ValidateLarchPlugin
+def set_dbinfo(key, value, notes=None, _larch=None, **kws):
+    """
+    set a value for a keyword in the scan info table.
+    """
+    if _larch.symtable._scan._scandb is None:
+        print('need to connect to scandb!')
+        return
+    return _larch.symtable._scan._scandb.set_info(key, value, notes=notes)
+
+def initializeLarchPlugin(_larch=None):
+    """initialize _scan"""
+    if not _larch.symtable.has_group(MODNAME):
+        g = Group()
+        g.__doc__ = MODDOC
+        _larch.symtable.set_symbol(MODNAME, g)
+
+def registerLarchPlugin():
+    symbols = {}
+    # print("Larch add basic scan Commands ", HAS_EPICSSCAN)
+    if HAS_EPICSSCAN:
+
+        symbols = {'scan_from_db':   scan_from_db,
+                   'do_scan': do_scan,
+                   'do_slewscan': do_scan,
+                   'get_dbinfo': get_dbinfo,
+                   'set_dbinfo': set_dbinfo}
+
+    return (MODNAME, symbols)
