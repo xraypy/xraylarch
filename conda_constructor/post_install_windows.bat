927ff06a615c1ddccf852289b848264d0da9142d
Matt Newville <newville@cars.uchicago.edu>
2019-07-04 12:05:53 -0500

    add force of old conda version averting "conda vs constructor" bug

--- conda_constructor/post_install_windows.bat
+++ conda_constructor/post_install_windows.bat
@@ -1,12 +1,11 @@
-REM  post install for windows
 
 
 REM # force install of packages that are noarch or have install problems
-%PREFIX%\Scripts\conda.exe install -y --force-reinstall qtpy dask pytz pyparsing networkx bokeh
+%PREFIX%\Scripts\conda.exe install -y --force qtpy dask pytz pyparsing networkx bokeh conda==4.5.13
 
 REM # make desktop icons
-%PREFIX%\python.exe %PREFIX%\Scripts\larch-script.py -m
-
+%PREFIX%\Scripts\larch.exe -m
 
 echo '# Larch post install done!'
-sleep 1
+sleep 15
+
