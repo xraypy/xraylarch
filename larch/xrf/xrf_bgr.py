7c58322b2776863c0ce62aa2b451a23874de1bad
Matthew Newville <newville@cars.uchicago.edu>
2019-08-10 21:50:17 -0500

    tweaks, maybe faster xrf background

--- larch/xrf/xrf_bgr.py
+++ larch/xrf/xrf_bgr.py
@@ -38,30 +38,39 @@ def xrf_background(energy, counts=None, group=None, width=None, exponent=2, **kw
         width = max(energy)/4.0
 
     tcounts = 1.0 * counts
-    tcounts[np.where(tcounts<0.01)] = 0.01
+    tcounts[np.where(tcounts<1.e-12)] = 1.e-12
 
-    bgr = -1.0*np.ones(nchans)
+    bgr = 0*counts
 
-    # use 1% of 99% percentile of counts as height at which
+    # use 99% percentile of counts as height at which
     # the polynomial should have full width = width
     max_count = np.percentile(tcounts, [99])[0]
-
     indices = np.linspace(-nchans, nchans, 2*nchans+1) * (2.0 * slope / width)
     polynom = 0.01 * max_count * indices**exponent
     polynom = np.compress((polynom <= max_count), polynom)
     max_index = int(len(polynom)/2 - 1)
-    for chan in range(nchans-1):
-       chan0  = max((chan - max_index), 0)
-       chan1  = min((chan + max_index), (nchans-1))
-       chan1  = max(chan1, chan0) + 1
-       idx0   = chan0 - chan + max_index
-       idx1   = chan1 - chan + max_index
-       offset = tcounts[chan] - polynom[idx0:idx1]
-       test   = tcounts[chan0:chan1] - offset
-       bgr[chan0:chan1] = np.maximum(bgr[chan0:chan1],
-                                       min(test)+offset)
+    nx = int(len(polynom)/2)
 
-    bgr[np.where(bgr <= 0)] = 0.0
+    ch0 = np.arange(nchans) + 1 - nx
+    ch0[np.where(ch0  < 0)] = 0
+
+    ch1 = np.arange(nchans) + nx
+    ch1[np.where(ch1 > nchans)] = nchans
+
+    ix1 = np.linspace(nx-1, nx-nchans, nchans, dtype='i4')
+    ix1[np.where(ix1 < 0)] = 0
+
+    ix2 = np.linspace(nx+nchans-1, nx, nchans, dtype='i4')
+    ix2[np.where(ix2 > (2*nx-1))] = 2*nx-1
+
+    for chan in range(nchans-1):
+       c0, c1, i0, i1 = ch0[chan], ch1[chan], ix1[chan], ix2[chan]
+       offset = tcounts[chan] - polynom[i0:i1]
+       tmax = offset + min(tcounts[c0:c1] - offset)
+       bgr[c0:c1] = np.maximum(bgr[c0:c1], tmax)
+
+    bgr[np.where(bgr < 0)] = 0.0
+    bgr[np.where(counts < 1)] = 0.0
 
     if group is not None:
         group.bgr = bgr
