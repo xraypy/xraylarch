7f1b4bbc3d873916d9c28d74a5bead94ae78b3da
Matthew Newville <newville@cars.uchicago.edu>
2019-09-13 09:35:33 -0500

    update recipe to install tomopy on Windows in post-install, from conda-forge

--- conda_constructor/post_install_windows.bat
+++ conda_constructor/post_install_windows.bat
@@ -1,10 +1,9 @@
 
-
-REM # force install of packages that are noarch or have install problems
-REM %PREFIX%\Scripts\conda.exe install -y --force qtpy dask pytz
-
 REM # make desktop icons
 %PREFIX%\Scripts\larch.exe -m
+
+REM # force install of packages that are noarch or have install problems
+%PREFIX%\Scripts\conda.exe install -y -c conda-forge tomopy
 
 echo '# Larch post install done!'
 sleep 10
