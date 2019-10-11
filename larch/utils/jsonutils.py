0290678e7c95639664d275c7c357fd62fe9e9522
Matthew Newville <newville@cars.uchicago.edu>
2019-10-11 10:47:27 -0500

    fix saving numpy.bools

--- larch/utils/jsonutils.py
+++ larch/utils/jsonutils.py
@@ -31,8 +31,9 @@ def encode4js(obj, grouplist=None):
             out['value'] = [(obj.real).tolist(), (obj.imag).tolist()]
         elif obj.dtype.name == 'object':
             out['value'] = [encode4js(i, grouplist=grouplist) for i in out['value']]
-
         return out
+    elif isinstance(obj, (np.bool, np.bool_)):
+        return bool(obj)
     elif isinstance(obj, (np.float, np.int)):
         return float(obj)
     elif isinstance(obj, str):
