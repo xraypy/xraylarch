b379288d6be13e441dd2dc5a904951f88a893452
Matthew Newville <newville@cars.uchicago.edu>
2019-10-13 22:25:36 -0500

    fix xrf calibration wrapper

--- larch/wxlib/xrfdisplay.py
+++ larch/wxlib/xrfdisplay.py
@@ -90,12 +90,13 @@ class XRFDisplayFrame(wx.Frame):
         self.plotframe = None
         self.wids = {}
         self.larch = _larch
-        if isinstance(_larch, Interpreter):  # called from shell
+        if isinstance(self.larch, Interpreter):  # called from shell
             pass
         else:
             self.larch_buffer = parent
             if not isinstance(parent, LarchFrame):
-                self.larch_buffer = LarchFrame(_larch=_larch, is_standalone=False)
+                self.larch_buffer = LarchFrame(_larch=self.larch,
+                                               is_standalone=False)
                 self.larch_buffer.Show()
                 self.larch_buffer.Raise()
                 self.larch_buffer.Hide()
@@ -1303,12 +1304,10 @@ class XRFDisplayFrame(wx.Frame):
             self.win_calib.Raise()
         except:
             self.win_calib = XRFCalibrationFrame(self, mca=self.mca,
-                                                 callback=self.onCalibrationChange,
-                                                 larch=self.larch)
+                                                 callback=self.onCalibrationChange)
 
     def onCalibrationChange(self, mca):
         """update whenn mca changed calibration"""
-        # print("new calib ", mca.offset, mca.slope)
         self.plotmca(mca)
 
     def onFitSpectrum(self, event=None, **kws):
