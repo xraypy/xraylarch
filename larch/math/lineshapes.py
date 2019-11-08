c85e3f3c5cce9f70c4e871e2651f42d4656ac393
Matthew Newville <newville@cars.uchicago.edu>
2019-11-08 15:51:34 -0600

    hypermet again, this time for sure

--- larch/math/lineshapes.py
+++ larch/math/lineshapes.py
@@ -6,7 +6,12 @@ Some common lineshapes and distribution functions
 from __future__ import division
 
 from numpy import exp, pi, sqrt, where
-from scipy import special
+
+from scipy.special import erfc as sp_erfc
+from scipy.special import wofz as sp_wofz
+from scipy.special import erf as sp_erf
+from scipy.special import gamma as sp_gamma
+from scipy.special import gammaln as sp_gammaln
 
 from lmfit.lineshapes import (gaussian, lorentzian, voigt, pvoigt, moffat,
                               pearson7, breit_wigner, damped_oscillator,
@@ -15,12 +20,11 @@ from lmfit.lineshapes import (gaussian, lorentzian, voigt, pvoigt, moffat,
                               skewed_voigt, step, rectangle, exponential,
                               powerlaw, linear, parabolic)
 
-s2pi = sqrt(2*pi)
+stau = sqrt(2*pi)
 s2 = sqrt(2.0)
 
 def hypermet(x, amplitude=1.0, center=0., sigma=1.0,
-             step=0.001, tail=0, beta=0.1,
-             use_voigt=True, gamma=0.25):
+             step=0.1, tail=0.1, beta=0.5, gamma=0.01):
     """
     hypermet function to simulate XRF peaks and/or Compton Scatter Peak,
     slightly modified.
@@ -31,18 +35,17 @@ def hypermet(x, amplitude=1.0, center=0., sigma=1.0,
       amplitude  overall scale factor
       center     peak centroid
       sigma      peak width parameter sigma
-      step       step parameter for low-x erfc step [0.001]
-      tail       amplitude of tail function         [0]
-      beta       slope of tail function             [0.1]
-      use_voigt  use Voigt lineshape instead of Gaussian [True]
-      gamma      gamma value for Voigt lineshape [0.25
+      step       heigh of low-x erfc step function   [0.1]
+      tail       height of tail function             [0.1]
+      beta       slope of tail function              [0.5]
+      gamma      gamma value for Voigt lineshape     [0.01]
 
 
     Notes
     -----
     The function is given by (with some error checking for
     small values of sigma, beta, and gamma, and with
-    s2 = sqrt(2) and s2pi = sqrt(2*pi)):
+    s2 = sqrt(2) and stau = sqrt(2*pi)):
 
         arg  = (x - center)/sigma
         if use_voigt:
@@ -52,7 +55,7 @@ def hypermet(x, amplitude=1.0, center=0., sigma=1.0,
 
         stepfunc = step * erfc(arg/2.0) / 200.0
         tailfunc = tail * exp(arg/beta) * erfc(arg/s2 + 1.0/beta))
-        hypermet = amplitude * (peak + stepfunc + tailfunc) / (2.0*s2pi*sigma)
+        hypermet = amplitude * (peak + stepfunc + tailfunc) / (2.0*stau*sigma)
 
     This follows (for Gaussian lineshape) the definitions given in
         ED-XRF SPECTRUM EVALUATION AND QUANTITATIVE ANALYSIS
@@ -66,47 +69,46 @@ def hypermet(x, amplitude=1.0, center=0., sigma=1.0,
     changing values of tail and beta.
 
     """
-    sigma = max(1.e-8, sigma)
-    beta = max(1.e-8, beta)
-    gamma = max(1.e-8, gamma)
-    arg   = (x - center)/sigma
+    sigma = max(1.e-15, sigma)
+    beta  = max(1.e-15, beta)*s2
+    gamma = max(1.e-15, gamma)/s2
+    step  = step/100.0
+
+    arg   = (x - center)/(s2*sigma)
     arg[where(arg>700)] = 700.0
 
-    if use_voigt:
-        peak = special.wofz(arg + 1j*gamma).real
-    else:
-        peak = exp(-arg**2 / 2.0)
+    peakfunc = sp_wofz((arg + 1j*gamma) ).real
 
-    stepfunc = step*special.erfc((x-center)/(s2*sigma))/1000.0
+    stepfunc = step*sp_erfc(arg)/(2*sp_wofz(1j*gamma).real)
 
-    arg[where(arg>beta*700)] = beta*700.0
+    arg[where(arg>beta*350)] = beta*350.0
+    tailfunc = sp_erfc(arg + 1./beta)*exp(2*arg/beta)
+    tailfunc *= 2*tail/(beta*exp(-1.0/beta**2))
 
-    tailfunc = exp(arg/beta) * special.erfc(arg/s2 + 1.0/(s2*beta))
-    tailfunc *= tail / (2*beta*sigma*exp(-1/2*beta**2))
-    return amplitude * (peak + stepfunc + tailfunc) / (2.0*s2pi*sigma)
+    return amplitude*(peakfunc + stepfunc + tailfunc)/(stau*sigma)
 
 def erf(x):
     """Return the error function.
     erf = 2/sqrt(pi)*integral(exp(-t**2), t=[0, z])
     """
-    return special.erf(x)
+    return sp_erf(x)
 
 def erfc(x):
     """Return the complementary error function.
     erfc = 1 - erf(x)
     """
-    return special.erfc(x)
+    return sp_erfc(x)
 
 def wofz(x):
     """Return the fadeeva function for complex argument.
     wofz = exp(-x**2)*erfc(-i*x)
     """
-    return special.wofz(x)
+    return sp_wofz(x)
 
 def gamma(x):
     """Return the gamma function."""
-    return special.gamma(x)
+    return sp_gamma(x)
 
 def gammaln(x):
     """Return the log of absolute value of gamma function."""
-    return special.gammaln(x)
+    return sp_gammaln(x)
