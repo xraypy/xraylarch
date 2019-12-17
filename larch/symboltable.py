9d1af2c4a6455c659b7955d637b9b65ac50d4216
Matthew Newville <newville@cars.uchicago.edu>
2019-12-16 20:46:34 -0600

    make sure that plugins_path is set on startup

--- larch/symboltable.py
+++ larch/symboltable.py
@@ -179,6 +179,7 @@ class SymbolTable(Group):
                                  history_file= site_config.history_file,
                                  init_files  = site_config.init_files,
                                  modules_path= site_config.modules_path,
+                                 plugins_path= site_config.plugins_path,
                                  user_larchdir= site_config.usr_larchdir,
                                  larch_version= site_config.larch_version)
 
