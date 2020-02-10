aaf7a1fed15a7072a31e75a06d4ef85a9565ac37
Matt Newville <newville@cars.uchicago.edu>
2020-02-10 15:27:04 -0600

    add specific copy_xafs_group()

--- larch/utils/__init__.py
+++ larch/utils/__init__.py
@@ -31,9 +31,47 @@ def copy_group(group, _larch=None):
     out = Group(datatype=getattr(group, 'datatype', 'unknown'),
                 copied_from=getattr(group, 'groupname', repr(group)))
 
+    class NoCopy:
+        c = 'no copy'
+        
     for attr in dir(group):
-        setattr(out, attr, copy.deepcopy(getattr(group, attr)))
+        val = NoCopy
+        try:
+            val = copy.deepcopy(getattr(group, attr))
+        except ValueError:
+            try:
+                val = copy.copy(getattr(group, attr))
+            except:
+                val = NoCopy
+        
+        if val != NoCopy:
+            setattr(out, attr, val)
     return out
+
+def copy_xafs_group(group, _larch=None):
+    """specialized group copy for XAFS data groups"""
+    from larch import Group
+    out = Group(datatype=getattr(group, 'datatype', 'unknown'),
+                copied_from=getattr(group, 'groupname', repr(group)))
+
+    for attr in dir(group):
+        do_copy = True
+        if attr in ('xdat', 'ydat', 'i0', 'data' 'yerr',
+                    'energy', 'mu'):
+            val = getattr(group, attr)*1.0
+        elif attr in ('norm', 'flat', 'deriv', 'deconv',
+                      'post_edge', 'pre_edge', 'norm_mback',
+                      'norm_vict', 'norm_poly'):
+            do_copy = False
+        else:
+            try:
+                val = copy.deepcopy(getattr(group, attr))
+            except ValueError:
+                do_copy = False
+        if do_copy:
+            setattr(out, attr, val)
+    return out
+   
 
 def isotime(t=None, with_tzone=False):
     if t is None:
@@ -72,6 +110,7 @@ def _larch_init(_larch):
 _larch_builtins = dict(copy=_copy, deepcopy=_deepcopy, more= _more,
                        parent=_parent, ls=_ls, mkdir=_mkdir, cd=_cd,
                        cwd=_cwd, group2dict=group2dict,
-                       copy_group=copy_group, dict2group=dict2group,
-                       debugtimer=debugtimer, isotime=isotime,
-                       json_dump=json_dump, json_load=json_load)
+                       copy_group=copy_group, copy_xafs_group=copy_xafs_group,
+                       dict2group=dict2group, debugtimer=debugtimer,
+                       isotime=isotime, json_dump=json_dump,
+                       json_load=json_load)
