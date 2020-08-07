f5a9fd1acefbb49172e46ef692aea30fb49b7598
Mauro Rovezzi <mauro.rovezzi@gmail.com>
2020-08-07 09:31:16 +0200

    removed Jupyter UI

--- larch/io/athena_project.py
+++ larch/io/athena_project.py
@@ -21,13 +21,6 @@ from larch import __version__ as larch_version
 from larch.utils.strutils import bytes2str, str2bytes, fix_varname
 from xraydb import guess_edge
 
-try:
-    import ipywidgets as widgets
-    from IPython.display import display
-    HAS_IPYWIDGETS = True
-except ImportError:
-    HAS_IPYWIDGETS = False
-
 alist2json = str.maketrans("();'\n", "[] \" ")
 
 
@@ -472,78 +465,6 @@ class AthenaGroup(Group):
     @groups.setter
     def groups(self, groups):
         self._athena_groups = groups
-
-    def edit_groups(self):
-        """Edit with UI"""
-        if HAS_IPYWIDGETS is False:
-            raise ModuleNotFoundError("`ipywidgets` not installed")
-
-        # store initial labels and selections
-        self._labs = [lab for lab in self.groups.keys()]
-        self._sels = [grp.sel for grp in self.groups.values()]
-
-        # store widgets
-        self._widgets = [AthenaGroupWidget(idx, lab, grp.sel) for idx, (lab, grp) in enumerate(self.groups.items())]
-
-        #make layout and display
-        iLabs = [wdg.wlab for wdg in self._widgets]
-        iSels = [wdg.wsel for wdg in self._widgets]
-        vLabs = widgets.VBox(iLabs)
-        vSels = widgets.VBox(iSels)
-        hGrps = widgets.HBox([vLabs, vSels])
-        tLabel = widgets.Label("Edit group names and selection (-> click 'Apply changes' once finished)")
-        sButton = widgets.Button(description="Apply changes")
-        sButton.on_click(self._apply_edit_groups)
-
-        editor = widgets.VBox([tLabel, hGrps, sButton])
-
-        return display(editor)
-
-    def _apply_edit_groups(self, event):
-        """Overwrite _athena_groups dictionary with the modified keys/selection from the UI"""
-
-        newgroups = OrderedDict()
-        for idx, (name, grp) in enumerate(self.groups.items()):
-            gw = self._widgets[idx]
-            if gw._changed is True:
-                grp.sel = int(gw._newsel)
-                newgroups[gw._newlab] = deepcopy(grp)
-            else:
-                newgroups[name] = deepcopy(grp)
-        self.groups = newgroups
-        print("Groups updated")
-
-
-class AthenaGroupWidget(object):
-
-    def __init__(self, idx, lab, sel):
-
-        self._changed = False
-        self._idx = idx
-        self._lab = lab
-        self._sel = bool(sel)
-        self._newlab = lab
-        self._newsel = sel
-        self.make_widget_label()
-        self.make_widget_sel()
-
-    def make_widget_label(self):
-        self.wlab = widgets.Text(value=self._lab)
-        self.wlab.observe(self.label_changed, 'value')
-
-    def make_widget_sel(self):
-        self.wsel = widgets.Checkbox(self._sel)
-        self.wsel.observe(self.sel_changed, 'value')
-
-    def label_changed(self, change):
-        self._newlab = change.new
-        self._changed = True
-        # print(f"[{self._idx}] {self._lab} -> {self._newlab}") # DEBUG
-
-    def sel_changed(self, change):
-        self._newsel = change.new
-        self._changed = True
-        # print(f"[{self._idx}] {self._lab}: {self._sel} -> {self._newsel}") # DEBUG
 
 
 class AthenaProject(object):
