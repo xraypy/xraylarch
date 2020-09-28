abc6bb84117ee47fb232c357c37dd5720faa9374
Matt Newville <newville@cars.uchicago.edu>
2020-09-28 10:29:07 -0500

    fix paths for linux-64 libraries

--- larch/bin/linux64/pathfix.sh
+++ larch/bin/linux64/pathfix.sh
@@ -1,1 +1,6 @@
-patchelf --set-rpath  lib* feff*
+patchelf --set-rpath '$ORIGIN' feff6l feff8l_*
+patchelf --set-rpath '$ORIGIN' libfeff6.so libcldata.so libxdifile.so
+patchelf --set-rpath '$ORIGIN' libfeff8lpath.so libfeff8lpotph.so
+patchelf --set-rpath '$ORIGIN' libgfortran.so.3 libquadmath.so.0
+
+# libgcc_s.so.1
