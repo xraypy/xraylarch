2f9276bfcea9157e51db6905eeea606d44620a4f
Matt Newville <newville@cars.uchicago.edu>
2021-04-22 10:44:00 -0500

    don't need windows "call" for exe files

--- installers/conda_constructor/post_install_windows.bat
+++ installers/conda_constructor/post_install_windows.bat
@@ -3,13 +3,12 @@ REM # install wxpython and tomopy from conda-forge
 call %PREFIX%\condabin\conda install -yc conda-forge wxpython tomopy
 
 REM # use pip to install some known-safe-for-pip packages
-call %PREFIX%\Scripts\pip.exe install xraylarch pyepics epicsapps psycopg2-binary PyCIFRW pyFAI numdifftools
+%PREFIX%\Scripts\pip.exe install xraylarch pyepics epicsapps psycopg2-binary PyCIFRW pyFAI numdifftools
 
 sleep 1
 
 REM # make desktop icons
-call %PREFIX%\Scripts\larch.exe -m
-
+%PREFIX%\Scripts\larch.exe -m
 
 echo '# Larch post install done!'
-sleep 10
+sleep 5
