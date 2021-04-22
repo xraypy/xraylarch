2eb18c1759de1210d5ca22a7643c07c3ca4dd38a
Matt Newville <newville@cars.uchicago.edu>
2021-04-22 14:47:07 -0500

    a few tweaks to windows install script

--- installers/conda_constructor/post_install_windows.bat
+++ installers/conda_constructor/post_install_windows.bat
@@ -1,14 +1,12 @@
 
 REM # install wxpython and tomopy from conda-forge
-call %PREFIX%\condabin\conda install -yc conda-forge wxpython tomopy
+call %PREFIX%\condabin\conda install --force-reinstall -yc conda-forge wxpython tomopy
 
 REM # use pip to install some known-safe-for-pip packages
 %PREFIX%\Scripts\pip.exe install xraylarch pyepics epicsapps psycopg2-binary PyCIFRW pyFAI numdifftools
-
-sleep 1
 
 REM # make desktop icons
 %PREFIX%\Scripts\larch.exe -m
 
 echo '# Larch post install done!'
-sleep 5
+timeout /t 10
