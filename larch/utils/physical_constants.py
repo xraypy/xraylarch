52df4b502b22ca5de7b2a0b3c0d6e711c70711fc
Matthew Newville <newville@cars.uchicago.edu>
2021-05-04 09:22:16 -0500

    add e_mass in eV

--- larch/utils/physical_constants.py
+++ larch/utils/physical_constants.py
@@ -17,6 +17,9 @@ AVOGADRO = consts.Avogadro
 # ATOMIC MASS in grams
 AMU = consts.atomic_mass * 1000.0
 
+# electron rest mass in eV
+E_MASS = consts.electron_mass * consts.c**2 / consts.e
+
 # Planck's Constant
 #   h*c    ~= 12398.42 eV*Ang
 #   hbar*c ~=  1973.27 eV*Ang
