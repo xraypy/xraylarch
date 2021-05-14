9bd5c7c2e3b72b4fef8f711b95387c91d5e9af4d
Matt Newville <newville@cars.uchicago.edu>
2021-05-13 22:08:19 -0500

    some cleanups

--- larch/xrd/xrd_tools.py
+++ larch/xrd/xrd_tools.py
@@ -10,8 +10,8 @@ mkak 2017.02.06 (originally written spring 2016)
 
 import math
 import numpy as np
-
-from ..utils.physical_constants import PLANCK_HC
+from numpy import cos, sin, arcsin,  degrees
+from ..utils.physical_constants import PLANCK_HC, TAU, DEG2RAD, RAD2DEG
 
 import re
 import os
@@ -32,7 +32,7 @@ def d_from_q(q):
     Converts q axis into d (returned units inverse of provided units)
     d = 2*PI/q
     '''
-    return (2.*math.pi)/q
+    return TAU/q
 
 def d_from_twth(twth,wavelength,ang_units='degrees'):
     '''
@@ -42,8 +42,8 @@ def d_from_twth(twth,wavelength,ang_units='degrees'):
     ang_unit : default in degrees; will convert from 'rad' if given
     '''
     if not ang_units.startswith('rad'):
-        twth = np.radians(twth)
-    return wavelength/(2*np.sin(twth/2.))
+        twth = DEG2RAD*twth
+    return wavelength/(2*sin(twth/2.))
 
 
 def twth_from_d(d,wavelength,ang_units='degrees'):
@@ -53,11 +53,11 @@ def twth_from_d(d,wavelength,ang_units='degrees'):
 
     ang_unit : default in degrees; will convert to 'rad' if given
     '''
-    twth = 2.*np.arcsin(wavelength/(2.*d))
+    twth = 2*arcsin(wavelength/(2.*d))
     if ang_units.startswith('rad'):
         return twth
     else:
-        return np.degrees(twth)
+        return RAD2DEG*twth
 
 
 def twth_from_q(q,wavelength,ang_units='degrees'):
@@ -67,22 +67,21 @@ def twth_from_q(q,wavelength,ang_units='degrees'):
 
     ang_unit : default in degrees; will convert to 'rad' if given
     '''
-    twth = 2.*np.arcsin((q*wavelength)/(4.*math.pi))
+    twth = 2*arcsin((q*wavelength)/(2*TAU))
     if ang_units.startswith('rad'):
         return twth
     else:
-        return np.degrees(twth)
-
+        return RAD2DEG*twth
 
 def q_from_d(d):
     '''
     Converts d axis into q (returned units inverse of provided units)
     q = 2*PI/d
     '''
-    return (2.*math.pi)/d
+    return TAU/d
 
 
-def q_from_twth(twth,wavelength,ang_units='degrees'):
+def q_from_twth(twth, wavelength, ang_units='degrees'):
     '''
     Converts 2th axis into q (q returned in inverse units of wavelength)
     q = [(4*PI)/lamda]*sin(2th/2)
@@ -90,45 +89,42 @@ def q_from_twth(twth,wavelength,ang_units='degrees'):
     ang_unit : default in degrees; will convert from 'rad' if given
     '''
     if not ang_units.startswith('rad'):
-        twth = np.radians(twth)
-    return ((4.*math.pi)/wavelength)*np.sin(twth/2.)
+        twth = DEG2RAD*twth
+    return ((2*TAU)/wavelength)*sin(twth/2.)
 
-def qv_from_hkl(hklall,a,b,c,alp,bet,gam):
+def qv_from_hkl(hklall, a, b, c, alpha, beta, gamma):
 
     qv = np.zeros(np.shape(hklall))
-    uvol = unit_cell_volume(a,b,c,alp,bet,gam)
+    uvol = unit_cell_volume(a,b,c,alpha,beta,gamma)
+    alpha, beta, gamma = DEG2RAD*alpha, DEG2RAD*beta, DEG2RAD*gamma
+    q0 = [(b*c*sin(alpha))/uvol,(c*a*sin(beta))/uvol,(a*b*sin(gamma))/uvol]
 
-    alp,bet,gam = np.radians(alp),np.radians(bet),np.radians(gam)
-    q0 = [(b*c*np.sin(alp))/uvol,(c*a*np.sin(bet))/uvol,(a*b*np.sin(gam))/uvol]
-
-    for i,hkl in enumerate(hklall):
-        qv[i] = [2*math.pi*hkl[0]*q0[0],
-                 2*math.pi*hkl[1]*q0[1],
-                 2*math.pi*hkl[2]*q0[2]]
+    for i, hkl in enumerate(hklall):
+        qv[i] = [TAU*hkl[0]*q0[0], TAU*hkl[1]*q0[1], TAU*hkl[2]*q0[2]]
     return qv
 
-def d_from_hkl(hklall,a,b,c,alp,bet,gam):
+def d_from_hkl(hklall, a, b, c, alpha, beta, gamma):
 
     d = np.zeros(len(hklall))
-    alp,bet,gam = np.radians(alp),np.radians(bet),np.radians(gam)
+    alpha, beta, gamma = DEG2RAD*alpha, DEG2RAD*beta, DEG2RAD*gamma
     for i,hkl in enumerate(hklall):
         h,k,l = hkl
-        x = 1-np.cos(alp)**2 - np.cos(bet)**2 - np.cos(gam)**2 \
-                + 2*np.cos(alp)*np.cos(bet)*np.cos(gam)
-        y =   (h*np.sin(alp)/a)**2 + 2*k*l*(np.cos(bet)*np.cos(gam)-np.cos(alp))/(b*c) \
-            + (k*np.sin(bet)/b)**2 + 2*l*h*(np.cos(gam)*np.cos(alp)-np.cos(bet))/(c*a) \
-            + (l*np.sin(gam)/c)**2 + 2*h*k*(np.cos(alp)*np.cos(bet)-np.cos(gam))/(a*b)
+        x = 1-cos(alpha)**2 - cos(beta)**2 - cos(gamma)**2 \
+            + 2*cos(alpha)*cos(beta)*cos(gamma)
+        y = (h*sin(alpha)/a)**2 + 2*k*l*(cos(beta)*cos(gamma)-cos(alpha))/(b*c) \
+            + (k*sin(beta)/b)**2 + 2*l*h*(cos(gamma)*cos(alpha)-cos(beta))/(c*a) \
+            + (l*sin(gamma)/c)**2 + 2*h*k*(cos(alpha)*cos(beta)-cos(gamma))/(a*b)
         d[i] = np.sqrt(x/y)
-
+    print(" -> ", d)
     return d
 
-def unit_cell_volume(a,b,c,alp,bet,gam):
+def unit_cell_volume(a, b, c, alpha, beta, gamma):
+    alpha, beta, gamma = DEG2RAD*alpha, DEG2RAD*beta, DEG2RAD*gamma
+    return a*b*c*(1-cos(alpha)**2-cos(beta)**2-cos(gamma)**2+
+                  2*cos(alpha)*cos(beta)*cos(gamma))**0.5
 
-    alp,bet,gam = np.radians(alp),np.radians(bet),np.radians(gam)
-    return a*b*c*(1-np.cos(alp)**2-np.cos(bet)**2-np.cos(gam)**2+2*np.cos(alp)*np.cos(bet)*np.cos(gam))**0.5
 
-
-def E_from_lambda(wavelength,E_units='keV',lambda_units='A'):
+def E_from_lambda(wavelength, E_units='keV', lambda_units='A'):
     '''
     Converts lambda into energy
     E = hf ; E = hc/lambda
@@ -139,7 +135,7 @@ def E_from_lambda(wavelength,E_units='keV',lambda_units='A'):
     if lambda_units == 'm':
         wavelength = wavelength*1e10
     elif lambda_units == 'nm':
-        wavelength = wavelength*1e1
+        wavelength = wavelength*10
     if E_units.lower() == 'kev':
         return PLANCK_HC/wavelength*1e-3 # keV
     else:
@@ -156,15 +152,15 @@ def lambda_from_E(E, E_units='keV', lambda_units='A'):
     '''
     if E_units.lower() != 'kev':
         E = E*1e-3 # keV
+    scale = 1.e-3
     if lambda_units == 'm':
-        return (PLANCK_HC/E)*1e-13
+        scale = 1.e-13
     elif lambda_units == 'nm':
-        return (PLANCK_HC/E)*1e-4
-    else:
-        return PLANCK_HC/E*1e-3 # A
+        scale = 1e-4
+    return scale*PLANCK_HC/E
 
-def generate_hkl(hmax=10,kmax=10,lmax=10,positive_only=True):
-    if positive_only is True:
+def generate_hkl(hmax=15, kmax=15, lmax=15, positive_only=True):
+    if positive_only:
         hklall = np.mgrid[0:hmax+1, 0:kmax+1, 0:lmax+1].reshape(3, -1).T
     else:
         hklall = np.mgrid[-hmax:hmax+1, -kmax:kmax+1, -lmax:lmax+1].reshape(3, -1).T
