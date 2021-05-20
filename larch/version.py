6c3a9c3b7c8e9a909e152d6884cf8ac96b39e34c
Matt Newville <newville@cars.uchicago.edu>
2021-05-20 16:17:02 -0500

    some refactoring of version checks

--- larch/version.py
+++ larch/version.py
@@ -8,7 +8,10 @@ import numpy
 import scipy
 import matplotlib
 import lmfit
-from collections import OrderedDict
+from collections import OrderedDict, namedtuple
+from packaging.version import parse as ver_parse
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 def version_data(mods=None):
     sysvers = sys.version
@@ -52,3 +55,43 @@ def make_banner(mods=None):
     lines.append(border)
 
     return '\n'.join(lines)
+
+
+########
+## for comparing current with remote version
+########
+VERSION_URL='https://raw.githubusercontent.com/xraypy/xraylarch/gh-pages/version.txt'
+
+VersionStatus = namedtuple('VersionStatus', ('update_available',
+                                             'local_version',
+                                             'remote_version',
+                                             'message'))
+
+UPDATE_MESSAGE = """#=== Update Available ===
+Larch version {remote_version:s} is available. Your version is currently {local_version:s}.
+To update the latest version run
+   larch -u
+from a Command Window or Terminal.
+#========================"""
+
+LATEST_MESSAGE = """Larch version {local_version:s} is up to date."""
+
+def check_larchversion():
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+    req = requests.get(VERSION_URL, verify=False, timeout=5)
+    remote_version = '0.9.001'
+    if req.status_code == 200:
+        try:
+            for line in req.text.split('\n'):
+                line = line.strip()
+                if not line.startswith('#'):
+                    remote_version = line
+                    break
+        except:
+            pass
+    local_version = __version__
+    update_available = ver_parse(remote_version) > ver_parse(local_version)
+    message = UPDATE_MESSAGE if update_available else LATEST_MESSAGE
+    message = message.format(remote_version=remote_version,
+                             local_version=local_version)
+    return VersionStatus(update_available,local_version, remote_version, message)
