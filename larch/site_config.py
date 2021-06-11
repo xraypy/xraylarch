bdf8df824a57fa81327c044ba65bc183e0ef606b
Matthew Newville <newville@cars.uchicago.edu>
2021-06-10 21:07:31 -0500

    add pymatgen as extras for XRD

--- larch/site_config.py
+++ larch/site_config.py
@@ -19,7 +19,7 @@ from .version import __version__ as larch_version
 # but may be installed if several of the larch apps are run.
 extras_wxgraph = set(['wxutils', 'wxmplot'])
 extras_epics = set(['pyepics', 'epicsapps', 'psycopg2-binary'])
-extras_xrd   = set(['fabio', 'pyfai', 'pycifrw'])
+extras_xrd   = set(['fabio', 'pyfai', 'pycifrw', 'pymatgen'])
 extras_doc   = set(['pytest', 'sphinx', 'numpydoc',
                     'sphinxcontrib-bibtex', 'sphinxcontrib-argdoc'])
 extras_qtgraph = set(['pyqt5', 'pyqtwebengine', 'pyqtgraph'])
