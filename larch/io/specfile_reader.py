68ca2824c99a42954c05c7647cb76ad14ddaf122
blissadm at rubis on bm16 <fame@esrf.fr>
2021-06-15 11:49:04 +0200

    handle if set_scan/get_counters fail

--- larch/io/specfile_reader.py
+++ larch/io/specfile_reader.py
@@ -242,12 +242,15 @@ class DataSourceSpecH5(object):
                 if ft in str(self._sourcefile):
                     self._sourcefile_type = ft
             self._scans = self.get_scans()
-            _iscn = 0
-            self.set_scan(self._scans[_iscn][0])  # set the first scan at init
-            while len(self.get_counters()) == 1:
-                self._logger.warning(f"not enough data in scan {_iscn+1} '{self.get_title()}'")
-                _iscn += 1
-                self.set_scan(self._scans[_iscn][0])
+            try:
+                _iscn = 0
+                self.set_scan(self._scans[_iscn][0])  # set the first scan at init
+                while len(self.get_counters()) == 1:
+                    self._logger.warning(f"not enough data in scan {_iscn+1} '{self.get_title()}'")
+                    _iscn += 1
+                    self.set_scan(self._scans[_iscn][0])
+            except Exception as e:
+                self._logger.error(e)
         except OSError:
             self._logger.error(f"cannot open {self._fname}")
 
