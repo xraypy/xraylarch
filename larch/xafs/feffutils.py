1b970722e688c7d77a20fb84d180653c4b221b58
Matt Newville <newville@cars.uchicago.edu>
2021-06-17 12:20:37 -0500

    Update feffutils.py
    tweaks to FeffPathInfo

--- larch/xafs/feffutils.py
+++ larch/xafs/feffutils.py
@@ -2,34 +2,32 @@ import os
 from datetime import datetime
 from collections import namedtuple
 
-FeffPathInfo = namedtuple('FeffPathInfo',
-                          ('filename', 'reff', 'degeneracy',
-                           'nleg', 'zabinsky', 'geom'))
-
+FPathInfo = namedtuple('FeffPathInfo',
+                       ('filename', 'reff', 'nleg',
+                        'degeneracy', 'cwratio', 'geom'))
 class FeffResults:
-    def __init__(self, folder, header=None, ipots=None, paths=None,
-                 date=None):
+    def __init__(self, folder, header=None, ipots=None,
+                 paths=None, datetime=None):
         self.folder = folder
         self.header = header
         self.ipots = ipots
         self.paths = paths
-        self.date = date
-
+        self.datetime = datetime
                           
 def get_feffpathinfo(folder):
     """get list of Feff path info for a Feff folder
     """
     
     fdat = os.path.join(folder, 'files.dat')
-    if not os.path.exists(fdat):
+    pdat = os.path.join(folder, 'paths.dat')
+    if not os.path.exists(fdat) or not os.path.exists(pdat):
         raise ValueError(f'{folder:s} is not a complete Feff folder - run feff?')
 
-
-    dtime = datetime.fromtimestamp(os.stat('files.dat').st_mtime).isoformat()
-    with open(os.path.join(folder, 'files.dat'), 'r') as fh:
+    dtime = datetime.fromtimestamp(os.stat(fdat).st_mtime).isoformat()
+    with open(fdat, 'r') as fh:
         fdatlines = fh.readlines()
 
-    with open(os.path.join(folder, 'paths.dat'), 'r') as fh:
+    with open(pdat, 'r') as fh:
         pathslines = fh.readlines()
     
     paths = {}
@@ -43,9 +41,9 @@ def get_feffpathinfo(folder):
             header.append(xline)
             continue
         if xline.startswith('feff0'):
-            words = xline.split()
-            index = int(words[0].replace('feff', '').replace('.dat', ''))
-            paths[index] = [words[0], words[5], words[4], words[3], words[2]]
+            w = xline.split()
+            index = int(w[0].replace('feff', '').replace('.dat', ''))
+            paths[index] = [w[0], w[5], w[4], w[3], w[2]]
 
     ipots = ['']*12
     waiting_for_dashes = True
@@ -73,30 +71,20 @@ def get_feffpathinfo(folder):
             if index in paths:
                 paths[index].append(elems)
 
-   
     ipots = [i for i in ipots if len(i) > 0]
     ipots[0] = '[%s]' % ipots[0]
     opaths = []
     for pindex, pinfo in paths.items():
         pots = [0] + pinfo[5]
-        geom =  ' -> '.join([ipots[i] for i in pots])
-        opaths.append(FeffPathInfo(filename=pinfo[0],
-                                   reff=float(pinfo[1]),
-                                   nleg=int(float(pinfo[2])),
-                                   degeneracy=float(pinfo[3]),
-                                   zabinsky=float(pinfo[4]),
-                                   geom=geom))
+        geom =  ' > '.join([ipots[i] for i in pots])
+        opaths.append(FPathInfo(filename=pinfo[0],
+                                reff=float(pinfo[1]),
+                                nleg=int(float(pinfo[2])),
+                                degeneracy=float(pinfo[3]),
+                                cwratio=float(pinfo[4]),
+                                geom=geom))
 
-    return FeffResults(folder, ipots=ipots,
+    return FeffResults(os.path.abspath(folder),
+                       ipots=ipots,
                        header='\n'.join(header),
-                       paths=opaths, date=dtime)
-
-if __name__ == '__main__':
-    ret = get_feffpathinfo('/Users/Newville/.larch/feff/Cu1_K_Cuprite_cif9326')
-    print(ret.folder)
-    print(ret.ipots)
-    print(ret.date)
-    print(ret.header)
-    for  p in ret.paths:
-        print(p)
-    
+                       paths=opaths, datetime=dtime)
