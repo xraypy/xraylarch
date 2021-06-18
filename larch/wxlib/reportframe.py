7c3ffde89504e50051d447b489d77224901692ba
Matthew Newville <newville@cars.uchicago.edu>
2021-06-18 17:56:07 -0500

    improve plain-report page

--- larch/wxlib/reportframe.py
+++ larch/wxlib/reportframe.py
@@ -1,19 +1,21 @@
 import wx
 from wx.richtext import RichTextCtrl
 
-from wxutils import pack, FRAMESTYLE
+from wxutils import pack, FRAMESTYLE, Font
+from . import FONTSIZE
 
 class ReportFrame(wx.Frame):
     """basic frame for displaying a text report -- should be improved!
     """
-    def __init__(self, parent=None, text=None, size=(550, 600), **kws):
+    def __init__(self, parent=None, text=None, size=(550, 600),
+                 title='Report', **kws):
         wx.Frame.__init__(self, parent, size=size, style=FRAMESTYLE)
-        self.report = RichTextCtrl(self,size=(500, 500),
-                                   style=wx.VSCROLL)
+        self.SetTitle(title)
 
+        self.report = RichTextCtrl(self,size=size, style=wx.VSCROLL)
         self.report.SetEditable(False)
-        self.report.SetFont(Font(11))
-        self.reportx.SetMinSize((500, 500))
+        self.report.SetFont(Font(FONTSIZE+1))
+        self.report.SetMinSize((500, 500))
 
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(self.report, 1, wx.ALL|wx.GROW, 2)
