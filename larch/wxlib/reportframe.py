4b6e0e8995c9e0ec21775ef7183dc1e1620f1aef
Matthew Newville <newville@cars.uchicago.edu>
2021-06-27 22:58:04 -0500

    all reportframe to save text to files

--- larch/wxlib/reportframe.py
+++ larch/wxlib/reportframe.py
@@ -2,16 +2,27 @@ import wx
 from wx.richtext import RichTextCtrl
 
 from wxutils import pack, FRAMESTYLE, Font
-from . import FONTSIZE
+from . import FONTSIZE, MenuItem, FileSave
 
 class ReportFrame(wx.Frame):
     """basic frame for displaying a text report -- should be improved!
     """
     def __init__(self, parent=None, text=None, size=(550, 600),
-                 title='Report', **kws):
-        wx.Frame.__init__(self, parent, size=size, style=FRAMESTYLE)
+                 title='Report', default_filename='out.txt', wildcard='*.txt', **kws):
+        self.default_filename = default_filename
+        self.wildcard = wildcard
+        wx.Frame.__init__(self, parent, size=size, style=FRAMESTYLE, **kws)
         self.SetTitle(title)
 
+        self.menubar = wx.MenuBar()
+        fmenu = wx.Menu()
+
+        MenuItem(self, fmenu, "Save", "Save Text to File", self.onSave)
+        MenuItem(self, fmenu, "Quit",  "Exit", self.onClose)
+        self.menubar.Append(fmenu, "&File")
+        self.SetMenuBar(self.menubar)
+        self.Bind(wx.EVT_CLOSE,  self.onClose)
+        
         self.report = RichTextCtrl(self,size=size, style=wx.VSCROLL)
         self.report.SetEditable(False)
         self.report.SetFont(Font(FONTSIZE+1))
@@ -29,3 +40,18 @@ class ReportFrame(wx.Frame):
         self.report.SetEditable(True)
         self.report.SetValue(text)
         self.report.SetEditable(False)
+
+
+    def onClose(self, event=None):
+        self.Destroy()
+
+    def onSave(self, eventt=None):
+        wildcard = f'{self.wildcard}|All files (*.*)|*.*'
+        path = FileSave(self, message='Save text to file',
+                        wildcard=wildcard,
+                        default_file=self.default_filename)
+        if path is not None:
+            with open(path, 'w') as fh:
+                fh.write(self.report.GetValue())
+                fh.write('')
+
