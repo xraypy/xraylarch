c6652bd959f2e5dff8f93a3b8c85247a4ac25a28
Matthew Newville <newville@cars.uchicago.edu>
2021-06-30 07:04:54 -0500

    wip update of conda scripts and post-install scripts

--- installers/conda_constructor/post_install_windows.bat
+++ installers/conda_constructor/post_install_windows.bat
@@ -1,9 +1,9 @@
 
 REM # install wxpython and tomopy from conda-forge
-call %PREFIX%\condabin\conda install --force-reinstall -yc conda-forge wxpython tomopy pymatgen
+call %PREFIX%\condabin\conda install --force-reinstall -yc conda-forge wxpython tomopy pymatgen pycifrw
 
 REM # use pip to install some known-safe-for-pip packages
-%PREFIX%\Scripts\pip.exe install xraylarch pyepics epicsapps psycopg2-binary PyCIFRW pyFAI numdifftools
+%PREFIX%\Scripts\pip.exe install xraylarch pyepics epicsapps psycopg2-binary pyFAI numdifftools
 
 REM # make desktop icons
 %PREFIX%\Scripts\larch.exe -m
