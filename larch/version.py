62ae609a2c3764c34abd8f77f6358d51b6d60210
kbuc <28578761+kbuc@users.noreply.github.com>
2021-07-18 19:37:21 +0200

    Fix importlib metadata loading for python <= 3.7

--- larch/version.py
+++ larch/version.py
@@ -10,7 +10,12 @@ import matplotlib
 import lmfit
 from collections import OrderedDict, namedtuple
 from packaging.version import parse as ver_parse
-from importlib.metadata import version, PackageNotFoundError
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError: # for Python<3.8
+    from importlib_metadata import version, PackageNotFoundError
+
 try:
     __version__ = version("xraylarch")
 except PackageNotFoundError:
