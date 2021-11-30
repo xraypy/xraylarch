fe9a017b85ca1251c890d7e84abdd2fcc0f25040
Matthew Newville <newville@cars.uchicago.edu>
2021-11-30 15:43:20 -0600

    add readtest for Project files

---
+++ examples/xafsdata/AthenaProjectFiles/readtest.py
@@ -0,0 +1,14 @@
+from larch.io import read_athena
+from glob import glob
+
+filelist = glob('*.prj')
+
+passed = 0
+for fname in sorted(glob('*.prj')):
+    print("### ", fname)
+    group = read_athena(fname)
+    print('read ', group, list(group.groups.keys()))
+    passed += 1
+
+
+print(f"Read {passed:d} Athena Project files")
