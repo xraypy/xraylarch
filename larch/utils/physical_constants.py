c5fea8e5e95c7f50ee81ffee9e8b0bdf74d6de49
Matthew Newville <newville@cars.uchicago.edu>
2022-07-25 10:02:47 -0500

    add atomic symbols and names to physical constants

--- larch/utils/physical_constants.py
+++ larch/utils/physical_constants.py
@@ -32,3 +32,39 @@ PLANCK_HBARC = PLANCK_HC / TAU
 # classical electron radius in cm
 R_ELECTRON_CM  = 100.0 * consts.codata.physical_constants['classical electron radius'][0]
 R_ELECTRON_ANG = 1.e10 * consts.codata.physical_constants['classical electron radius'][0]
+
+
+
+ATOM_SYMS = ['H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'Mg',
+           'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca', 'Sc', 'Ti', 'V',
+           'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se',
+           'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh',
+           'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba',
+           'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho',
+           'Er', 'Tm', 'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt',
+           'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac',
+           'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm',
+           'Md', 'No', 'Lr']
+
+
+ATOM_NAMES = ['hydrogen', 'helium', 'lithium', 'beryllium', 'boron',
+            'carbon', 'nitrogen', 'oxygen', 'fluorine', 'neon', 'sodium',
+            'magnesium', 'aluminum', 'silicon', 'phosphorus', 'sulfur',
+            'chlorine', 'argon', 'potassium', 'calcium', 'scandium',
+            'titanium', 'vanadium', 'chromium', 'manganese', 'iron',
+            'cobalt', 'nickel', 'copper', 'zinc', 'gallium', 'germanium',
+            'arsenic', 'selenium', 'bromine', 'krypton', 'rubidium',
+            'strontium', 'yttrium', 'zirconium', 'niobium', 'molybdenum',
+            'technetium', 'ruthenium', 'rhodium', 'palladium', 'silver',
+            'cadmium', 'indium', 'tin', 'antimony', 'tellurium', 'iodine',
+            'xenon', 'cesium', 'barium', 'lanthanum', 'cerium',
+            'praseodymium', 'neodymium', 'promethium', 'samarium',
+            'europium', 'gadolinium', 'terbium', 'dysprosium', 'holmium',
+            'erbium', 'thulium', 'ytterbium', 'lutetium', 'hafnium',
+            'tantalum', 'tungsten', 'rhenium', 'osmium', 'iridium',
+            'platinum', 'gold', 'mercury', 'thallium', 'lead', 'bismuth',
+            'polonium', 'astatine', 'radon', 'francium', 'radium',
+            'actinium', 'thorium', 'protactinium', 'uranium', 'neptunium',
+            'plutonium', 'americium', 'curium', 'berkelium', 'californium',
+            'einsteinium', 'fermium', 'mendelevium', 'nobelium',
+            'lawrencium']
