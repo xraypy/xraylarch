0bcb38a914fa15a1850bc07f52b68f951fc9c991
Matthew Newville <newville@cars.uchicago.edu>
2022-08-03 15:12:07 -0500

    allow some notebook config

--- larch/wxlib/notebooks.py
+++ larch/wxlib/notebooks.py
@@ -2,11 +2,21 @@ import wx
 
 import wx.lib.agw.flatnotebook as flat_nb
 
-FNB_STYLE = flat_nb.FNB_NO_X_BUTTON|flat_nb.FNB_NO_NAV_BUTTONS|flat_nb.FNB_NODRAG
+FNB_STYLE = flat_nb.FNB_NO_X_BUTTON|flat_nb.FNB_NODRAG
 
 def flatnotebook(parent, paneldict, panelkws={},
-                 on_change=None, selection=0, **kws):
-    nb = flat_nb.FlatNotebook(parent, agwStyle=FNB_STYLE, **kws)
+                 on_change=None, selection=0, style=None, with_dropdown=False,
+                 with_nav_buttons=False, with_smart_tabs=False, **kws):
+    if style is None:
+        style = FNB_STYLE
+    if with_dropdown:
+        style |= flat_nb.FNB_DROPDOWN_TABS_LIST
+    if not with_nav_buttons:
+        style |= flat_nb.FNB_NO_NAV_BUTTONS
+    if with_smart_tabs:
+        style |= flat_nb.FNB_SMART_TABS
+
+    nb = flat_nb.FlatNotebook(parent, agwStyle=style, **kws)
     nb.SetTabAreaColour(wx.Colour(250, 250, 250))
     nb.SetActiveTabColour(wx.Colour(254, 254, 195))
     nb.SetNonActiveTabTextColour(wx.Colour(10, 10, 128))
