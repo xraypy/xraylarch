ed1ac649657526b98699cb127e6e923fd317e96d
Matthew Newville <newville@cars.uchicago.edu>
2022-12-16 13:13:55 -0600

    less _larch

--- larch/io/hdf5group.py
+++ larch/io/hdf5group.py
@@ -7,33 +7,34 @@ import h5py
 import numpy
 from functools import partial
 from larch.utils import fixName
+from larch import Group as larchGroup
 import scipy.io.netcdf
 
-def netcdf_group(fname, _larch=None, **kws):
+def netcdf_group(fname, **kws):
     """open a NetCDF file and map the variables in it to larch groups
     g = netcdf_group('tmp.nc')
     """
     finp = scipy.io.netcdf.netcdf_file(fname, mode='r')
-    group = _larch.symtable.create_group()
+    group = larchGroup()
     for k, v in finp.variables.items():
         setattr(group, k, v.data)
     finp.close()
     return group
 
-def netcdf_file(fname, mode='r', _larch=None):
+def netcdf_file(fname, mode='r'):
     """open and return a raw NetCDF file, equvialent to
     scipy.io.netcdf.netcdf_file(fname, mode=mode)
     """
     return scipy.io.netcdf.netcdf_file(fname, mode=mode)
 
-def h5file(fname, mode='r', _larch=None):
+def h5file(fname, mode='r'):
     """open and return a raw HDF5 file, equvialent to
     import h5py
     h5py.File(fname, mode)
     """
     return h5py.File(fname, mode)
 
-def h5group(fname, mode='r+', _larch=None):
+def h5group(fname, mode='r+'):
     """open an HDF5 file, and map to larch groups
     g = h5group('myfile.h5')
 
@@ -49,18 +50,17 @@ def h5group(fname, mode='r+', _larch=None):
        'itemname_attrs'.
     """
     fh = h5py.File(fname, mode)
-    group = _larch.symtable.create_group
 
     def add_component(key, val, top):
         parents = [fixName(w, allow_dot=False) for w in key.split('/')]
         current = parents.pop()
         for word in parents:
             if not hasattr(top, word):
-                setattr(top, word, group())
+                setattr(top, word, larchGroup())
             top = getattr(top, word)
         tname = top.__name__
         if isinstance(val, h5py.Group):
-            setattr(top, current,  group(name="%s/%s" % (tname, current)))
+            setattr(top, current,  larchGroup(name="%s/%s" % (tname, current)))
             if len(val.attrs) > 0:
                 getattr(top, current)._attrs = dict(val.attrs)
         else:
@@ -76,7 +76,7 @@ def h5group(fname, mode='r+', _larch=None):
             setattr(top, current, dat)
             if len(val.attrs) > 0:
                 setattr(top, "%s_attrs" % current, dict(val.attrs))
-    top = group(name=fname)
+    top = larchGroup(name=fname)
     top.h5_file = fh
     fh.visititems(partial(add_component, top=top))
     return top
