fe7c7e1a841adafdda7d41715948cd503d81924b
Matthew Newville <newville@cars.uchicago.edu>
2023-03-15 14:48:00 -0500

    no longer need to import wx and set matplotlib to use WXAgg early

--- larch/__init__.py
+++ larch/__init__.py
@@ -6,40 +6,32 @@
 import os
 import sys
 import logging
-import time
 import warnings
 warnings.simplefilter('ignore')
+
+logger = logging.getLogger()
+logger.level = logging.WARNING
 
 # note: may need to set CONDA env *before* loading numpy!
 if os.name == 'nt':
     os.environ['CONDA_DLL_SEARCH_MODIFICATION_ENABLE'] = '1'
 
-if (sys.version_info.major < 3 or sys.version_info.minor < 5):
-    raise EnvironmentError('larch requires python 3.6 or higher')
+if (sys.version_info.major < 3 or sys.version_info.minor < 8):
+    raise EnvironmentError('larch requires python 3.8 or higher')
 
 import numpy
+import scipy, scipy.optimize, scipy.special, scipy.stats
 import matplotlib
 import lmfit
-
-logger = logging.getLogger()
-logger.level = logging.WARNING
 
 # note: for HDF5 File / Filter Plugins to be useful, the
 # hdf5plugin module needs to be imported before h5py
 try:
     import hdf5plugin
+    import h5py
 except ImportError:
     pass
 
-# we set the matplotlib backend before import lmfit / pyplot
-#    import matplotlib.pyplot as plt
-try:
-    import wx
-    with warnings.catch_warnings():
-        warnings.filterwarnings('error')
-        matplotlib.use("WXAgg")
-except ImportError:
-    pass
 
 ## be careful here: it is easy to have cicrular imports!
 
