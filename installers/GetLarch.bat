c5a7d837dc8b7f005f8284ea00f443f265900906
Matt Newville <newville@cars.uchicago.edu>
2023-04-26 12:15:43 -0500

    update Windows install script to use mamba

--- installers/GetLarch.bat
+++ installers/GetLarch.bat
@@ -3,40 +3,30 @@
 
 set prefix=%USERPROFILE%\xraylarch
 
-set condaurl=https://github.com/conda-forge/miniforge/releases/download/4.10.2-0/
-set condafile=Miniforge3-Windows-x86_64.exe
+set condaurl=https://github.com/conda-forge/miniforge/releases/latest/download
+set condafile=Mambaforge-Windows-x86_64.exe
 
 if not exist %~dp0%condafile% (
     echo ## Downloading Miniconda from https://repo.anaconda.com/miniconda/, please wait...
-    bitsadmin /transfer getminiconda /download /priority normal %condaurl%/%condafile% %~dp0%condafile%
+    bitsadmin /transfer getmamba /download /priority normal %condaurl%/%condafile% %~dp0%condafile%
 )
 
 echo ## Installing miniconda environment to %prefix%, please wait...
 
-set CONDA_EXE=
-set CONDA_PYTHON_EXE=
-set CONDA_PREFIX=
-set PROJ_LIB=
-
 %~dp0%condafile% /InstallationType=JustMe /RegisterPython=0 /S /D=%prefix%
 
-echo ## basic miniconda installed, running updates
+echo ## basic mamba installed, running updates
 
 set PATH=%prefix%;%prefix%\bin;%prefix%\condabin;%prefix%\Scripts;%PATH%
 
-call %prefix%\condabin\conda update -n base -yc defaults --all
-call %prefix%\condabin\conda activate base
-call %prefix%\condabin\conda config --set auto_activate_base true
-
-echo ## Installing wxpython and tomopy from conda-forge
-call %prefix%\condabin\conda install -yc conda-forge wxpython tomopy scipy matplotlib scikit-image scikit-learn pymatgen pycifrw
+echo ## Installing basic python scipy packages
+call %prefix%\Scripts\mamba install -yc conda-forge numpy scipy matplotlib scikit-image scikit-learn pycifrw pandas jupyter plotly wxpython pymatgen mkl_fft tomopy
 
 echo ## Installing xraylarch and dependencies from PyPI
-call %prefix%\Scripts\pip install wxmplot wxutils lmfit asteval pyshortcuts pyfai xraylarch
+call %prefix%\Scripts\pip install wxmplot lmfit pyshortcuts pyfai xraylarch epicsapps 
 
 echo ## Creating desktop shortcuts
 call %prefix%\Scripts\larch -m
-
 
 echo ## Installation to %prefix% done!
 echo ## Applications can be run from the Larch folder on your Desktop.
