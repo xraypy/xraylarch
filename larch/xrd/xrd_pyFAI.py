76c9d799efa98cff3580a8138cbc8837d32b46a0
Matthew Newville <newville@cars.uchicago.edu>
2023-05-30 15:46:53 -0500

    add read_poni/write_poni from PONI file to dict

--- larch/xrd/xrd_pyFAI.py
+++ larch/xrd/xrd_pyFAI.py
@@ -23,6 +23,46 @@ from larch.io import tifffile
 ##########################################################################
 # FUNCTIONS
 
+def read_poni(fname):
+    """read pyFAI PONI file to dict"""
+    conf = {}
+    with open(fname, 'r') as fh:
+        for line in fh.readlines():
+            line = line[:-1].strip()
+            if line.startswith('#'):
+                continue
+            key, val = [a.strip() for a in line.split(':')]
+            key = key.lower()
+            if key == 'distance': key='dist'
+            if key == 'pixelsize1': key='pixel1'
+            if key == 'pixelsize2': key='pixel2'
+            conf[key] = float(val)
+    return conf
+
+def write_poni(filename, calname='', pixel1=0, pixel2=0,
+               poni1=0, poni2=0, dist=0, rot1=0, rot2=0, rot3=0,
+               wavelength=0, **kws):
+    """write pyFAI PONI file"""
+    buff = '''# XRD Calibration  {calname:s}
+# Saved {ctime:s}
+PixelSize1: {pixel1:16.11g}
+PixelSize2: {pixel2:16.11g}
+Distance: {dist:16.11g}
+Poni1: {poni1:16.11g}
+Poni2: {poni2:16.11g}
+Rot1: {rot1:16.11g}
+Rot2: {rot2:16.11g}
+Rot3: {rot3:16.11g}
+Wavelength: {wavelength:16.11g}
+'''
+    with open(filename, 'w') as fh:
+        fh.write(buff.format(calname=calname, ctime=time.ctime(),
+                             pixel1=pixel1, pixel2=pixel2,
+                             poni1=poni1, poni2=poni2,
+                             rot1=rot1, rot2=rot2, rot3=rot3,
+                             dist=dist, wavelength=wavelength))
+        
+
 def return_ai(calfile):
 
     if calfile is not None and os.path.exists(calfile):
@@ -126,7 +166,7 @@ def integrate_xrd_row(rowxrd2d, calfile, unit='q', steps=2048,
     return np.array(q), np.array(xrd1d)
 
 def integrate_xrd(xrd2d, calfile, unit='q', steps=2048, file='',  wedge_limits=None,
-                  mask=None, dark=None, is_eiger=True, save=False, verbose=False):
+                  k=None, dark=None, is_eiger=True, save=False, verbose=False):
     '''
     Uses pyFAI (poni) calibration file and 2D XRD image to produce 1D XRD data
 
