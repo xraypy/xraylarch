9c520fef6e7f70b04e5bc6b85fd62204b23c71f1
Matthew Newville <newville@cars.uchicago.edu>
2023-05-30 15:47:19 -0500

    and export read/write_poni()

--- larch/xrd/__init__.py
+++ larch/xrd/__init__.py
@@ -10,7 +10,8 @@ from .xrd_fitting import (peakfinder, peaklocater, peakfitter, peakfilter,
 
 from .xrd_pyFAI import (integrate_xrd, integrate_xrd_row, read_lambda,
                         calc_cake, save1D, return_ai, twth_from_xy,
-                        q_from_xy, eta_from_xy)
+                        q_from_xy, eta_from_xy,
+                        read_poni, write_poni)
 
 from .xrd_tools import (d_from_q, d_from_twth, twth_from_d, twth_from_q,
                         E_from_lambda, lambda_from_E, q_from_d,
