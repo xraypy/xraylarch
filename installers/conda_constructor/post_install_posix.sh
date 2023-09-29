8ef9207047249db7fa88e22e24e62f126be75bff
Matt Newville <newville@cars.uchicago.edu>
2023-09-29 14:56:04 -0500

    updates for constructor scripts

---
+++ installers/conda_constructor/post_install_posix.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# $2 is the install location, ($HOME by default)
+if [ xx$PREFIX == 'xx' ]; then
+    PREFIX=$(echo "$2/__NAME_LOWER__" | sed -e 's,//,/,g')
+fi
+
+unset CONDA_EXE CONDA_PYTHON_EXE CONDA_PREFIX PROJ_LIB
+
+# use pip to install some known-safe-for-pip packages
+$PREFIX/bin/pip install xraylarch wxmplot wxutils pyepics epicsapps psycopg2-binary pyfai
+
+# make desktop icons
+$PREFIX/bin/python $PREFIX/bin/larch -m
+
+chown -R $USER  $HOME/.larch
+printenv > $HOME/.larch/xraylarch_pkginstall.log
+echo '# Larch post install done!'
+sleep 1
