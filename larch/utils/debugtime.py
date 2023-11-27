30440dd3ef03994dc0d0dfba76b78780258227c4
Matt Newville <newville@cars.uchicago.edu>
2023-11-27 11:05:56 -0600

    add precision option to debugtime

--- larch/utils/debugtime.py
+++ larch/utils/debugtime.py
@@ -4,7 +4,7 @@ import time
 import sys
 
 class debugtime(object):
-    def __init__(self, verbose=False, _larch=None):
+    def __init__(self, verbose=False, _larch=None, precision=3):
         self._larch = _larch
         self.clear()
         self.verbose = verbose
@@ -25,26 +25,30 @@ class debugtime(object):
             self._print(msg, time.ctime())
         self.times.append((msg, time.time()))
 
-    def get_report(self):
+    def get_report(self, precision=None):
+        if precision is not None:
+            self.precision = precision
+        prec = self.precision
         m0, t0 = self.times[0]
         tlast= t0
         out = []
         add = out.append
         add("# %s  %s " % (m0,time.ctime(t0)))
         add("#----------------")
-        add("#       Message                       Total     Delta")
+        m, tt, dt = 'Message', 'Total' , 'Delta'
+        add(f"# {m:32s}    {tt:{prec+2}s}  {dt:{prec+2}s}")
         for m,t in self.times[1:]:
             tt = t-t0
             dt = t-tlast
             if len(m)<32:
                 m = m + ' '*(32-len(m))
-            add("  %32s    %.3f    %.3f" % (m,tt, dt))
+            add(f"  {m:32s}    {tt:.{prec}f}  {dt:.{prec}f}")
             tlast = t
         add('')
         return "\n".join(out)
 
-    def show(self):
-        self._print(self.get_report())
+    def show(self, precision=None):
+        self._print(self.get_report(precision=precision))
 
     def save(self, fname='debugtimer.dat'):
         dat = self.get_report()
