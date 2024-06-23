cce938130f03f715368a3d8e793ce7995753a3ee
Matthew Newville <newville@cars.uchicago.edu>
2024-06-23 09:40:02 -0500

    update debugtime to use tabulate

--- larch/utils/debugtime.py
+++ larch/utils/debugtime.py
@@ -2,78 +2,106 @@
 
 import time
 import sys
+from tabulate import tabulate
 
-class debugtime(object):
-    def __init__(self, verbose=False, _larch=None, precision=3):
-        self._larch = _larch
-        self.precision = precision
-        self.clear()
+class DebugTimer():
+    '''
+    Measure run times for lines of code and summarize results
+    '''
+    def __init__(self, initial_message=None, verbose=False,
+                 with_mod_count=False, precision=3):
         self.verbose = verbose
-        self.add('init')
+        self.precision = precision
+        self.with_mod_count = with_mod_count
+        self.clear(initial_message=initial_message)
 
-    def clear(self):
-        self.times = []
+    def clear(self, initial_message=None):
+        self.data = []
+        if initial_message is None:
+            initial_message = 'DebugTimer'
+        self.add(f'initial_message {time.ctime()}')
 
-    def _print(self, *args):
-        writer = sys.stdout
-        if self._larch is not None:
-            writer = self._larch.writer
-        writer.write(*args)
-
-
-    def add(self,msg=''):
+    def add(self, msg=None):
+        if msg is None:
+            msg = time.ctime()
+        self.data.append((msg, len(sys.modules), time.perf_counter()))
         if self.verbose:
-            self._print(msg, time.ctime())
-        self.times.append((msg, time.time()))
+            print(msg)
 
-    def get_report(self, precision=None):
-        if precision is not None:
-            self.precision = precision
+    def get_table(self, precision=None, with_mod_count=True,
+                 tablefmt='simple_outline'):
         prec = self.precision
-        m0, t0 = self.times[0]
-        tlast= t0
-        out = []
-        add = out.append
-        add("# %s  %s " % (m0,time.ctime(t0)))
-        add("#----------------")
-        m, tt, dt = 'Message', 'Total' , 'Delta'
-        add(f"# {m:32s}    {tt:{prec+2}s}  {dt:{prec+2}s}")
-        for m,t in self.times[1:]:
-            tt = t-t0
-            dt = t-tlast
-            if len(m)<32:
-                m = m + ' '*(32-len(m))
-            add(f"  {m:32s}    {tt:.{prec}f}  {dt:.{prec}f}")
-            tlast = t
-        add('')
-        return "\n".join(out)
+        if precision is not None:
+            prec = precision
+        with_nmod= self.with_mod_count
+        if with_mod_count is not None:
+            with_nmod = with_mod_count
+        m0, n0, t0 = self.data[0]
+        tprev= t0
+        header = ['Message','Delta Time', 'Total Time']
+        row  = [m0, 0, 0]
+        if with_nmod:
+            header.append('# Modules')
+            row.append(n0)
+        table = [row[:]]
+        for m, n, t in self.data[1:]:
+            tt, dt = t-t0, t-tprev
+            row = [m, dt, tt, n] if with_nmod else [m, dt, tt]
+            table.append(row[:])
+            tprev = t
+        ffmt = f'.{prec}f'
+        return tabulate(table, header, floatfmt=ffmt, tablefmt=tablefmt)
 
-    def show(self, precision=None):
-        self._print(self.get_report(precision=precision))
+    def show(self, precision=None, with_mod_count=True,
+             tablefmt='outline'):
+        print(self.get_table(precision=precision,
+                             with_mod_count=with_mod_count,
+                             tablefmt=tablefmt))
 
-    def save(self, fname='debugtimer.dat'):
-        dat = self.get_report()
-        with open(fname, 'w') as fh:
-            fh.write('%s\n' % dat)
 
-def debugtimer(_larch=None):
-    """debugtimer returns a Timer object that can be used
-    to time the running of portions of code, and then
-    write a simple report of the results.  the Timer object
-    has methods:
+def debugtimer(initial_message=None, with_mod_count=False,
+               verbose=False, precision=3):
+    '''debugtimer returns a DebugTimer object to measure the runtime of
+    portions of code, and then write a simple report of the results.
 
-      clear()   -- reset Timer
-      add(msg)  -- record time, with message
-      show_report -- print timer report
+    Arguments
+    ------------
+    iniitial message: str, optional initial message ['DebugTimer']
+    precision:      int, precision for timing results [3]
+    with_mod_count: bool, whether to include number of imported modules [True]
+    verbose:        bool, whether to print() each message when entered [False]
 
-    An example:
+    Returns
+    --------
+    DebugTimer object, with methods:
 
-      timer = debugtimer()
-      x = 1
-      timer.add('now run foo')
-      foo()
-      timer.add('now run bar')
-      bar()
-      timer.show_report()
-    """
-    return debugtime(_larch=_larch)
+      clear(initial_message=None)      -- reset Timer
+      add(message)                     -- record time, with message
+      show(tablefmt='simple_outline')  -- print timer report
+          where tableftmt can be any tablefmt for the tabulate module.
+
+    Example:
+    -------
+      timer = debugtimer('started timer', precision=4)
+      result = foo(x=100)
+      timer.add('ran foo')
+      bar(result)
+      timer.add('ran bar')
+      timer.show(tablefmt='outline')
+    '''
+    return DebugTimer(initial_message=initial_message,
+                      with_mod_count=with_mod_count,
+                      verbose=verbose, precision=precision)
+
+
+if __name__ == '__main__':
+    dt = debugtimer('test timer')
+    time.sleep(1.102)
+    dt.add('slept for 1.102 seconds')
+    import numpy as np
+    n = 10_000_000
+    x = np.arange(n, dtype='float64')/3.0
+    dt.add(f'created numpy array len={n}')
+    s = np.sqrt(x)
+    dt.add('took sqrt')
+    dt.show(tablefmt='outline')
