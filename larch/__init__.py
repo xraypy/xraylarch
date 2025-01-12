ff6c643e737527673d60316d22d9162e4ff036ce
Matthew Newville <newville@cars.uchicago.edu>
2025-01-12 11:07:54 -0600

    set release version to 2025.1.0, faster initial import

--- larch/__init__.py
+++ larch/__init__.py
@@ -11,18 +11,14 @@ warnings.simplefilter('ignore')
 
 logger = logging.getLogger()
 logger.level = logging.WARNING
-
 # note: may need to set CONDA env *before* loading numpy!
 if os.name == 'nt':
     os.environ['CONDA_DLL_SEARCH_MODIFICATION_ENABLE'] = '1'
 
-if (sys.version_info.major < 3 or sys.version_info.minor < 8):
-    raise EnvironmentError('larch requires python 3.8 or higher')
+if (sys.version_info.major < 3 or sys.version_info.minor < 9):
+    raise EnvironmentError('larch requires python 3.9 or higher')
 
-import numpy
-import scipy, scipy.optimize, scipy.special, scipy.stats
 import matplotlib
-import lmfit
 
 # note: for HDF5 File / Filter Plugins to be useful, the
 # hdf5plugin module needs to be imported before h5py
@@ -33,15 +29,12 @@ except ImportError:
     pass
 
 ## be careful here: it is easy to have cicrular imports!
-
 from .version import __date__, __version__, __release_version__
 from .symboltable import Group, isgroup, repr_value
 from .larchlib import Make_CallArgs, parse_group_args, isNamedClass, Journal, Entry
-from .fitting import Parameter, isParameter, param_value, ParameterGroup
-
-# from . import builtins
 from .inputText import InputText
 from .interpreter import Interpreter
+
 from . import larchlib
 from . import utils
 from . import site_config
