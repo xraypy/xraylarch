57ca90d6a8482db71bdf5b25c3b6e5b320bde4ab
Mauro Rovezzi <mauro.rovezzi@gmail.com>
2025-02-13 22:21:16 +0100

    test with simpler structures

--- tests/test_wxlib_cif_browser.py
+++ tests/test_wxlib_cif_browser.py
@@ -6,11 +6,11 @@ from pathlib import Path
 from larch.wxlib.cif_browser import CIFViewer
 
 toppath = Path(__file__).parent.parent
-structpath = toppath / "examples" / "structuredata" / "structure2feff"
+structpath = toppath / "examples" / "structuredata" / "struct2xas"
 
 
 def test_cif2feff():
-    cif_file = structpath / "tBu3CpRu2H4.cif"
+    cif_file = structpath / "ZnO_mp-2133.cif"
     viewer = CIFViewer(with_feff=True)
     frame = viewer.GetTopWindow()
     frame.onImportCIF(path=cif_file.as_posix())
--- tests/test_wxlib_structure2feff_browser.py
+++ tests/test_wxlib_structure2feff_browser.py
@@ -6,11 +6,11 @@ from pathlib import Path
 from larch.wxlib.structure2feff_browser import Structure2FeffViewer
 
 toppath = Path(__file__).parent.parent
-structpath = toppath / "examples" / "structuredata" / "structure2feff"
+structpath = toppath / "examples" / "structuredata" / "struct2xas"
 
 
 def test_structure2feff():
-    struct_file = structpath / "tBu3CpRu2H4.xyz"
+    struct_file = structpath / "GaBr_single_frame.xyz"
     viewer = Structure2FeffViewer()
     frame = viewer.GetTopWindow()
     frame.onImportStructure(path=struct_file.as_posix())
