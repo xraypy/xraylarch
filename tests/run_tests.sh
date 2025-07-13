85e4951056f0b037f85e6825c04d00bc837ca09b
Matthew Newville <newville@cars.uchicago.edu>
2025-07-13 12:31:27 -0500

    add bash scripts to run tests with more care for temporary resources

---
+++ tests/run_tests.sh
@@ -0,0 +1,8 @@
+pytest test_athena_addgroup.py test_basic_processing.py \
+ test_funccalls.py test_importlarch.py test_interpreter.py \
+ test_jsonutils.py test_larch_interpreter.py test_larchexamples_basic.py \
+ test_larchexamples_xafs.py test_larchexamples_xray.py \
+ test_math_deglitch.py test_math_utils.py test_read_beamlinedata.py \
+ test_read_xafsdata.py test_symbol_callbacks.py test_xas_data_source.py
+pytest test_wxlib_cif_browser.py
+pytest test_wxlib_structure2feff_browser.py
