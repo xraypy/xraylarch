d24ceff68fc5dd89c381993a01ab469f5b012071
Matt Newville <newville@cars.uchicago.edu>
2025-09-02 15:27:06 -0500

    always use sciy for gridxyz, drop unused numexpr import

--- larch/math/gridxyz.py
+++ larch/math/gridxyz.py
@@ -8,12 +8,7 @@ from __future__ import division, print_function
 
 import warnings
 import numpy as np
-
-# suppress warnings
-try:
-    import numexpr
-except ImportError:
-    pass
+from scipy.interpolate import griddata
 
 from larch.utils.logging import getLogger
 
@@ -23,7 +18,7 @@ _logger = getLogger("gridxyz")
 MODNAME = "_math"
 
 
-def gridxyz(xcol, ycol, zcol, xystep=None, lib="scipy", method="linear"):
+def gridxyz(xcol, ycol, zcol, xystep=None, method="linear", lib=None):
     """Grid (X, Y, Z) 1D data on a 2D regular mesh
 
     Parameters
@@ -34,16 +29,13 @@ def gridxyz(xcol, ycol, zcol, xystep=None, lib="scipy", method="linear"):
     xystep : float or None (optional)
         the step size of the XY grid, if None -> 0.1
 
-    lib : str, optional ["scipy"]
-        library used for griddata
-            - scipy
-            - matplotlib
-
     method : str, optional ["cubic"]
         interpolation method
             - nearest
             - linear
             - cubic
+
+    lib : allowwd but ignored (always uses scipy.interpolate.griddata)
 
     Returns
     -------
@@ -67,31 +59,10 @@ def gridxyz(xcol, ycol, zcol, xystep=None, lib="scipy", method="linear"):
     xgrid = np.linspace(xcol.min(), xcol.max(), num=nxpoints)
     ygrid = np.linspace(ycol.min(), ycol.max(), num=nypoints)
     xx, yy = np.meshgrid(xgrid, ygrid)
-    if "matplotlib" in lib.lower():
-        _logger.warning("matplotlib deprecated -> using scipy")
-        lib = "scipy"
-    if not "scipy" in lib.lower():
-        _logger.error("lib should be scipy")
-        return np.nan, np.nan, np.nan
 
-    try:
-        from scipy.interpolate import griddata
-    except ImportError:
-        _logger.error("Cannot load griddata from Scipy")
-        return np.nan, np.nan, np.nan
-
-    _logger.info("Gridding data with {0}/{1}...".format(lib, method))
-    zz = griddata(
-        (xcol, ycol),
-        zcol,
+    _logger.info(f"Gridding data with {method=}")
+    zz = griddata( (xcol, ycol),  zcol,
         (xgrid[None, :], ygrid[:, None]),
-        method=method,
-        fill_value=np.nan,
-    )
-
+        method=method, fill_value=np.nan)
 
     return xgrid, ygrid, zz
-
-
-if __name__ == "__main__":
-    pass
