df79d3483752a768570b66090a8fb49b816fddfd
Matt Newville <newville@cars.uchicago.edu>
2026-03-10 18:47:38 -0500

    2026.1.2 update release version and date

--- larch/version.py
+++ larch/version.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 """Version information"""
 
-__release_version__ = '2026.1.1'
-__date__    = '2026-January-22'
+__release_version__ = '2026.1.2'
+__date__    = '2026-March-10'
 __authors__ = "M. Newville, M. Rovezzi, M. Koker, B. Ravel, and others"
 
 from ._version import __version__, __version_tuple__
