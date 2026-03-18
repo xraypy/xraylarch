c0ca645fcb01d3f85f05b11d4215ee5150dc5711
ljrh <luke.higgins@diamond.ac.uk>
2026-03-18 14:59:19 +0000

    fix: use output directory from filename arg in groups2csv individual mode
    When saving individual CSV files, save_group built the output path as a
    bare filename (e.g. 'IrOx-L3.csv'), causing writes to resolve against the
    current working directory. On macOS this is typically '/' when launched as
    a GUI app, resulting in a PermissionError (errno 13). Fix by deriving the
    output directory from the filename argument already passed to groups2csv.

--- larch/io/csvfiles.py
+++ larch/io/csvfiles.py
@@ -3,6 +3,7 @@
 Code to write and read CVS files
 
 """
+
 import sys
 import os
 import time
@@ -18,8 +19,8 @@ from larch.math import interp, remove_dups
 
 maketrans = str.maketrans
 
-def groups2csv(grouplist, filename, delim=',',
-               x='energy', y='norm', individual=False):
+
+def groups2csv(grouplist, filename, delim=",", x="energy", y="norm", individual=False):
     """save data from a list of groups to a CSV file
 
     Arguments
@@ -31,32 +32,36 @@ def groups2csv(grouplist, filename, delim=',',
     individual toggle saving individual x/y in separate files
 
     """
-    delim = delim.strip() + ' '
+    delim = delim.strip() + " "
+
     def get_label(grp):
-        'get label for group'
-        for attr in ('filename', 'label', 'name', 'file', '__name__'):
+        "get label for group"
+        for attr in ("filename", "label", "name", "file", "__name__"):
             o = getattr(grp, attr, None)
             if o is not None:
                 return o
         return repr(o)
 
-    def save_group(g, delim=', ', x='energy', y='norm'):
+    def save_group(g, delim=", ", x="energy", y="norm"):
         label = get_label(g)
         _x = getattr(g, x)
         _y = getattr(g, y)
         _n = len(_x)
         labels = [x, label]
         outarr = np.array([_x, _y])
-        buff = [f"#saved {time.ctime()}",
-                f"#saving x array={x}, y array={y}",
-                f"#{label}: {g.filename}",
-                "#------------------------------------------",
-                "# %s" % delim.join(labels)]
+        buff = [
+            f"#saved {time.ctime()}",
+            f"#saving x array={x}, y array={y}",
+            f"#{label}: {g.filename}",
+            "#------------------------------------------",
+            "# %s" % delim.join(labels),
+        ]
         for i in range(_n):
             buff.append(delim.join([gformat(_x[i]), gformat(_y[i])]))
-        buff.append('')
-        fnout = f"{label}.csv"
-        with open(fnout, 'w', encoding=sys.getdefaultencoding()) as fh:
+        buff.append("")
+        outdir = os.path.dirname(os.path.abspath(filename))
+        fnout = os.path.join(outdir, f"{label}.csv")
+        with open(fnout, "w", encoding=sys.getdefaultencoding()) as fh:
             fh.write("\n".join(buff))
         print(f"Wrote group to {fnout}")
 
@@ -69,11 +74,13 @@ def groups2csv(grouplist, filename, delim=',',
     x0 = getattr(grouplist[0], x)
     npts = len(x0)
     columns = [x0, getattr(grouplist[0], y)]
-    labels = [x, get_label(grouplist[0]) ]
+    labels = [x, get_label(grouplist[0])]
 
-    buff = ["# %d files saved %s" % (len(grouplist), time.ctime()),
-            "# saving x array='%s', y array='%s'" % (x, y),
-            "# %s: %s" % (labels[1], grouplist[0].filename)]
+    buff = [
+        "# %d files saved %s" % (len(grouplist), time.ctime()),
+        "# saving x array='%s', y array='%s'" % (x, y),
+        "# %s: %s" % (labels[1], grouplist[0].filename),
+    ]
 
     for g in grouplist[1:]:
         label = get_label(g)
@@ -81,8 +88,8 @@ def groups2csv(grouplist, filename, delim=',',
         labels.append(label)
         _x = remove_dups(getattr(g, x))
         _y = getattr(g, y)
-        if ((len(_x) != npts) or (abs(_x -x0)).sum() > 1.0):
-            columns.append(interp(_x, _y, x0, kind='linear'))
+        if (len(_x) != npts) or (abs(_x - x0)).sum() > 1.0:
+            columns.append(interp(_x, _y, x0, kind="linear"))
         else:
             columns.append(_y)
 
@@ -91,11 +98,11 @@ def groups2csv(grouplist, filename, delim=',',
     for i in range(npts):
         buff.append(delim.join([gformat(s[i]) for s in columns]))
 
-    buff.append('')
-    with open(filename, 'w', encoding=sys.getdefaultencoding()) as fh:
+    buff.append("")
+    with open(filename, "w", encoding=sys.getdefaultencoding()) as fh:
         fh.write("\n".join(buff))
 
-    print("Wrote %i groups to %s" % (len(columns)-1, filename))
+    print("Wrote %i groups to %s" % (len(columns) - 1, filename))
 
 
 def str2float(word, allow_times=True):
@@ -128,10 +135,11 @@ def str2float(word, allow_times=True):
             pass
     return val
 
+
 def read_csv(filename):
     """read CSV file, return group with data as columns"""
-    csvfile = open(filename, 'r')
-    dialect = csv.Sniffer().sniff(csvfile.read(),  [',',';', '\t'])
+    csvfile = open(filename, "r")
+    dialect = csv.Sniffer().sniff(csvfile.read(), [",", ";", "\t"])
     csvfile.seek(0)
 
     data = None
@@ -140,7 +148,7 @@ def read_csv(filename):
         if data is None:
             ncols = len(row)
             data = [[] for i in range(ncols)]
-            isfloat =[None]*ncols
+            isfloat = [None] * ncols
         for i, word in enumerate(row):
             data[i].append(str2float(word))
             if isfloat[i] is None:
@@ -152,7 +160,7 @@ def read_csv(filename):
 
     out = Group(filename=filename, data=data)
     for icol in range(ncols):
-        cname = 'col_%2.2d' % (icol+1)
+        cname = "col_%2.2d" % (icol + 1)
         val = data[icol]
         if isfloat[icol]:
             val = np.array(val)
