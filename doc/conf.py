b55762f7431b8ee157b36acde35dfb311cc4aa55
Matt Newville <newville@cars.uchicago.edu>
2026-05-24 20:31:26 -0500

    wip: update theme and extensions to more modern sphinx

--- doc/conf.py
+++ doc/conf.py
@@ -2,15 +2,17 @@
 #
 # larch documentation build configuration file, created by
 
+from pathlib import Path
 import sys, os
 
-CURDIR = os.path.abspath(os.path.dirname(__file__))
+# sys.path.insert(0, Path('sphinx', 'ext'))
+sys.path.insert(0, '.')
 
-sys.path.insert(0, os.path.abspath(os.path.join('sphinx', 'ext')))
-sys.path.insert(0, CURDIR)
-sys.path.append(os.path.abspath(os.path.join('.')))
-
-from extensions import extensions
+extensions = ['sphinx.ext.mathjax', 'sphinx.ext.autodoc',
+              'sphinx.ext.napoleon', 'sphinx.ext.extlinks',
+              'sphinx.ext.intersphinx', 'sphinx.ext.ifconfig',
+              'sphinxcontrib.bibtex', 'sphinx_subfigure',
+              'sphinx_copybutton', 'numpydoc' ]
 
 import authorlist_format
 
@@ -31,7 +33,7 @@ extlinks = {
 templates_path = ['_templates']
 
 # The suffix of source filenames.
-source_suffix = {'.rst': 'restructuredtext'}
+source_suffix = '.rst'
 
 # The encoding of source files.
 #source_encoding = 'utf-8'
@@ -61,36 +63,15 @@ bibtex_bibfiles = ['larch.bib']
 try:
     import larch
     release = larch.__release_version__
-# The full version, including alpha/beta/rc tags.
 except ImportError:
-    release = 'unknown (larch import failed??)'
+    release = 'unknown'
 
 exclude_trees = ['_build']
 exclude_patterns = ['_build', 'sphinx', '_junk', 'epilog.rst']
 
-#sphinxtr
-# Ideally, we wouldn't have to do this, but sphinx seems to have trouble with
-# directives inside only directives
-if tags.has('latex'):
-    master_doc = 'index_tex'
-    exclude_patterns.append('index.rst')
-else:
-    master_doc = 'index'
-    exclude_patterns.append('index_tex.rst')
+# rst_epilog = open(Path('epilog.rst'),'r').read()# .decode('utf-8')
 
-#sphinxtr
-# A string of reStructuredText that will be included at the end of
-# every source file that is read.
-rst_epilog = open(os.path.join(CURDIR, 'epilog.rst'),'r').read()# .decode('utf-8')
-
-# The reST default role (used for this markup: `text`) to use for all documents.
-#default_role = None
-
-# If true, '()' will be appended to :func: etc. cross-reference text.
 add_function_parentheses = True
-
-# If true, the current module name will be prepended to all description
-# unit titles (such as .. function::).
 add_module_names = True
 
 # If true, sectionauthor and moduleauthor directives will be shown in the
@@ -103,50 +84,16 @@ pygments_style = 'sphinx'
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
 
-
 # -- Options for HTML output ---------------------------------------------------
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ['sphinx/theme']
+# html_theme_path = ['sphinx/theme']
 # html_theme = 'nature'
-html_theme = 'larchdoc'
+# html_theme = 'larchdoc'
 
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-
-# html_theme_options = {'collapsiblesidebar': False}
-
-# The name for this set of Sphinx documents.  If None, it defaults to
-# "<project> v<release> documentation".
-#html_title = None
-
-# A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title =
-
-# The name of an image file (relative to this directory) to place at the top
-# of the sidebar.
-html_logo = None
-
-# The name of an image file (within the static path) to use as favicon of the
-# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
-# pixels large.
-#html_favicon = None
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+html_theme = 'bizstyle'
 html_static_path = ['_static']
-
-# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
-# using the given strftime format.
-#html_last_updated_fmt = '%b %d, %Y'
-
-# If true, SmartyPants will be used to convert quotes and dashes to
-# typograpbhically correct entities.
-# html_use_smartypants = True
-
-# Custom sidebar templates, maps document names to template names.
+html_css_files = ['xraylarch_style.css']
 html_sidebars = {'index': ['indexsidebar.html','searchbox.html']}
 
 # Additional templates that should be rendered to pages, maps page names to
@@ -156,5 +103,5 @@ html_sidebars = {'index': ['indexsidebar.html','searchbox.html']}
 # If false, no module index is generated.
 html_use_modindex = False
 
-htmlhelp_basename = 'larchdoc'
-html_domain_indices = False
+# htmlhelp_basename = 'larchdoc'
+# html_domain_indices = False
